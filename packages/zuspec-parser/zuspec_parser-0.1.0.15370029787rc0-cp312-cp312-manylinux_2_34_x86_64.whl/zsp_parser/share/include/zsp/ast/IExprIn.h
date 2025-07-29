6d2c4ce7056fa03c91c6c0737d73de4b24a25c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:34 +0000

    Add package zuspec-parser/0.1.0.15370029787rc0/zuspec_parser-0.1.0.15370029787rc0-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/zuspec-parser/zuspec_parser-0.1.0.15370029787rc0-cp312-cp312-manylinux_2_34_x86_64.whl/zsp_parser/share/include/zsp/ast/IExprIn.h
@@ -0,0 +1,61 @@
+/****************************************************************************
+ * IExprIn.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+#pragma once
+#include <stdint.h>
+#include <unordered_map>
+#include <memory>
+#include <set>
+#include <string>
+#include <vector>
+#include "zsp/ast/impl/UP.h"
+#include "zsp/ast/IVisitor.h"
+#include "zsp/ast/IExpr.h"
+
+namespace zsp {
+namespace ast {
+
+class IExpr;
+using IExprUP=UP<IExpr>;
+typedef std::shared_ptr<IExpr> IExprSP;
+class IExprOpenRangeList;
+using IExprOpenRangeListUP=UP<IExprOpenRangeList>;
+typedef std::shared_ptr<IExprOpenRangeList> IExprOpenRangeListSP;
+class IExprIn;
+using IExprInUP=UP<IExprIn>;
+class IExprIn : public virtual IExpr {
+public:
+    
+    virtual ~IExprIn() { }
+    
+    
+    virtual IExpr *getLhs() const = 0;
+    
+    virtual void setLhs(IExpr *v, bool own=true) = 0;
+    
+    virtual IExprOpenRangeList *getRhs() const = 0;
+    
+    virtual void setRhs(IExprOpenRangeList *v, bool own=true) = 0;
+};
+    
+    } // namespace zsp
+    } // namespace ast
+    
