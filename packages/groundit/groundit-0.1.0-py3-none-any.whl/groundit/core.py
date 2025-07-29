905cd47e12b8f0bd1700792bbd2cb828d4cdb9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:29 +0000

    Add package groundit/0.1.0/groundit-0.1.0-py3-none-any.whl

---
+++ packages/groundit/groundit-0.1.0-py3-none-any.whl/groundit/core.py
@@ -0,0 +1,11 @@
+"""Core functionality for groundit package."""
+
+
+def greet(name: str) -> str:
+    """Greet someone with groundit."""
+    return f"Hello {name}, welcome to groundit!"
+
+
+def get_version() -> str:
+    """Get the current version of groundit."""
+    return "0.1.0" 
