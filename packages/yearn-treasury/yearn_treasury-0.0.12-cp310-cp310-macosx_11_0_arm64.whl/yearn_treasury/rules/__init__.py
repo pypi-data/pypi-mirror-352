6662fcc49a620aa5e715ccb0356ba125f20f6d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:30 +0000

    Add package yearn-treasury/0.0.12/yearn_treasury-0.0.12-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/yearn-treasury/yearn_treasury-0.0.12-cp310-cp310-macosx_11_0_arm64.whl/yearn_treasury/rules/__init__.py
@@ -0,0 +1,5 @@
+from yearn_treasury.rules.cost_of_revenue import *
+from yearn_treasury.rules.expense import *
+from yearn_treasury.rules.ignore import *
+from yearn_treasury.rules.other_income import *
+from yearn_treasury.rules.revenue import *
