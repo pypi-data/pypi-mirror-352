20b1ef3bf0109ad1b070fb2d3f780c249a84d3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:58 +0000

    Add package warframe-market.py/1.0.1/warframe_market_py-1.0.1.tar.gz

---
+++ packages/warframe-market.py/warframe_market_py-1.0.1.tar.gz/warframe_market_py-1.0.1/warframe_market/common/__init__.py
@@ -0,0 +1,17 @@
+
+BASE_URL = "https://api.warframe.market/v2"
+BASE_STATIC_ASSETS_URL = "https://warframe.market/static/assets"
+
+from .enums import (
+    Language,
+    Platform,
+)
+
+from .base import (
+    Base,
+    BaseRequest,
+)
+
+from .options import (
+    LanguageCode,
+)
