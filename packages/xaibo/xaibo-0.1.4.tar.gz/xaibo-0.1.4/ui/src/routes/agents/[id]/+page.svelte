33c25ad6d9ae8a635c555a0840da59021dc9b465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:11 +0000

    Add package xaibo/0.1.4/xaibo-0.1.4.tar.gz

---
+++ packages/xaibo/xaibo-0.1.4.tar.gz/xaibo-0.1.4/ui/src/routes/agents/[id]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import PageHeader from "$lib/components/custom/PageHeader.svelte";
+	import {page} from '$app/state';
+    import EventSequenceRenderer from "$lib/components/custom/EventSequenceRenderer.svelte";
+
+    let {data} = $props();
+    let {AgentConfig, DebugTrace} = $derived(data);
+    let {debugLog} = $derived($DebugTrace.data)
+</script>
+
+<PageHeader title={page.params.id} />
+<div class="flex h-full w-full flex-col pr-4 pl-4 overflow-auto">
+    <EventSequenceRenderer events={debugLog.events}/>
+</div>
+
+
