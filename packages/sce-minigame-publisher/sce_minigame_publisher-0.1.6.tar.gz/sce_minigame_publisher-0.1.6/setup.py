ec2dd3276e2f90d3e001b6dd388d8c40f3992027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:12 +0000

    Add package sce-minigame-publisher/0.1.6/sce_minigame_publisher-0.1.6.tar.gz

---
+++ packages/sce-minigame-publisher/sce_minigame_publisher-0.1.6.tar.gz/sce_minigame_publisher-0.1.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sce-minigame-publisher",
+    version="0.1.6",
+    author="SCE Team",
+    author_email="jl@xd.com",
+    description="TapTap SCE小游戏自动发布工具",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    py_modules=["sce_minigame_publisher"],
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "sce-minigame-publisher=sce_minigame_publisher:cli_main",
+        ],
+    },
+) 
