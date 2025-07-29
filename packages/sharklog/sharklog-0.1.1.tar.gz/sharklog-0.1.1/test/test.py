5f26e3540b0e49360df51ed89e607413a9bdee8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:35 +0000

    Add package sharklog/0.1.1/sharklog-0.1.1.tar.gz

---
+++ packages/sharklog/sharklog-0.1.1.tar.gz/sharklog-0.1.1/test/test.py
@@ -0,0 +1,9 @@
+import sharklog
+
+sharklog.init(debug=True)
+
+sharklog.debug("This is a debug message")
+sharklog.info("This is an info message")
+sharklog.warning("This is a warning message")
+sharklog.error("This is an error message")
+sharklog.critical("This is a critical message")
