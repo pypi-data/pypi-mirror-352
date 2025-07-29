c67a787fe18a51261a1df365c0c473a9b5f331cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:45 +0000

    Add package licelfile/0.1.1/licelfile-0.1.1.tar.gz

---
+++ packages/licelfile/licelfile-0.1.1.tar.gz/licelfile-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='licelfile',
+    version='0.1.1',
+    description='Package that reads and writes licel lidar files.',
+    author='Konstantin Shmirko',
+    packages=find_packages(),
+    install_requires=[
+        # здесь перечисляются зависимости, если они нужны
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
