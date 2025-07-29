d8b3f1a649c243d5a8d0e249b302a33c01aba56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:51 +0000

    Add package ripple-down-rules/0.5.92/ripple_down_rules-0.5.92.tar.gz

---
+++ packages/ripple-down-rules/ripple_down_rules-0.5.92.tar.gz/ripple_down_rules-0.5.92/test/test_results/datasets_physical_object_is_a_robot/physical_object_is_a_robot_output__scrdr.py
@@ -0,0 +1,15 @@
+from ripple_down_rules.datastructures.case import Case, create_case
+from .physical_object_is_a_robot_output__scrdr_defs import *
+
+
+attribute_name = 'output_'
+conclusion_type = (bool,)
+mutually_exclusive = True
+
+
+def classify(case: Dict, **kwargs) -> bool:
+    if not isinstance(case, Case):
+        case = create_case(case, max_recursion_idx=3)
+
+    if conditions_226969243620390858682731042391766665817(case):
+        return conclusion_226969243620390858682731042391766665817(case)
