a0e2b1a14f5d2de566cdee5bf2621114e20c1615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:10 +0000

    Add package ramifice/0.2.9/ramifice-0.2.9-py3-none-any.whl

---
+++ packages/ramifice/ramifice-0.2.9-py3-none-any.whl/ramifice/add_valid.py
@@ -0,0 +1,14 @@
+"""Additional validation of fields."""
+
+from abc import ABCMeta
+
+
+class AddValidMixin(metaclass=ABCMeta):
+    """Additional validation of fields."""
+
+    async def add_validation(self) -> dict[str, str]:
+        """For additional validation of fields.
+        Format: <"field_name", "Error message">
+        """
+        error_map: dict[str, str] = {}
+        return error_map
