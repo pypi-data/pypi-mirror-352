6197c324b0cd3dc06c396b9598fedaa9114a5208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:21:00 +0000

    Add package junjo/0.46.0/junjo-0.46.0-py3-none-any.whl

---
+++ packages/junjo/junjo-0.46.0-py3-none-any.whl/junjo/__init__.py
@@ -0,0 +1,30 @@
+"""
+Junjo: A python library for building and managing complex Graph Workflows.
+
+This library provides the building blocks and tools for wrapping python functions into
+nodes, edges, and graphs that can be executed by a workflow.
+
+This library also produces annotated Opentelemetry Spans to help make sense of
+execution telemetry.
+"""
+from .condition import Condition
+from .edge import Edge
+from .graph import Graph
+from .node import Node
+from .run_concurrent import RunConcurrent
+from .state import BaseState
+from .store import BaseStore
+from .workflow import Subflow, Workflow, _NestableWorkflow
+
+__all__ = [
+    "Condition",
+    "Graph",
+    "Workflow",
+    "Subflow",
+    "_NestableWorkflow",
+    "Node",
+    "RunConcurrent",
+    "BaseState",
+    "BaseStore",
+    "Edge",
+]
