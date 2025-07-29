4a099ec978143bbff922a34096c9140e38a9643b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:21:17 +0000

    Add package ml123/0.2/ml123-0.2.tar.gz

---
+++ packages/ml123/ml123-0.2.tar.gz/ml123-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml123',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[],
+    author="25/25 in lab",
+    python_requires='>=3.6',
+)
+#python setup.py sdist bdist_wheel
+#set TWINE_PASSWORD=
+
+
+
+#twine upload dist/*
+
