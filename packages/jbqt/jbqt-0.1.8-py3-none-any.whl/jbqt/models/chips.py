209dc9febca8a1c2652fb5eaac4b0cfd37eb1bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:36 +0000

    Add package jbqt/0.1.8/jbqt-0.1.8-py3-none-any.whl

---
+++ packages/jbqt/jbqt-0.1.8-py3-none-any.whl/jbqt/models/chips.py
@@ -0,0 +1,31 @@
+"""Model for chips widget"""
+
+from abc import abstractmethod, ABC
+
+from PyQt6.QtCore import QObject
+from PyQt6.QtWidgets import QWidget
+
+from jbqt.models.chip_button import IChipButton
+
+
+class IChipsWidget(QWidget):
+    """ChipsWidget model"""
+
+    def __init__(self, parent: QObject = None) -> None:
+        super().__init__(parent)
+        self.values: list = None
+
+    def add_chip(self):
+        pass
+
+    def remove_chip(self, button: IChipButton):
+        pass
+
+    def add_chips(self, items: list[str]) -> None:
+        pass
+
+    def remove_chips(self, items: list[str]) -> None:
+        pass
+
+    def remove_all(self, *_) -> None:
+        pass
