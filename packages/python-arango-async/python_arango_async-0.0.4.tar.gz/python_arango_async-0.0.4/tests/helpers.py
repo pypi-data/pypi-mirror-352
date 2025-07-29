2ea9ffe4acf8bce966699e2f550a7c10c138efde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:30 +0000

    Add package python-arango-async/0.0.4/python_arango_async-0.0.4.tar.gz

---
+++ packages/python-arango-async/python_arango_async-0.0.4.tar.gz/python_arango_async-0.0.4/tests/helpers.py
@@ -0,0 +1,46 @@
+from uuid import uuid4
+
+
+def generate_db_name():
+    """Generate and return a random database name.
+
+    Returns:
+        str: Random database name.
+    """
+    return f"test_database_{uuid4().hex}"
+
+
+def generate_col_name():
+    """Generate and return a random collection name.
+
+    Returns:
+        str: Random collection name.
+    """
+    return f"test_collection_{uuid4().hex}"
+
+
+def generate_graph_name():
+    """Generate and return a random graph name.
+
+    Returns:
+        str: Random graph name.
+    """
+    return f"test_graph_{uuid4().hex}"
+
+
+def generate_username():
+    """Generate and return a random username.
+
+    Returns:
+        str: Random username.
+    """
+    return f"test_user_{uuid4().hex}"
+
+
+def generate_string():
+    """Generate and return a random unique string.
+
+    Returns:
+        str: Random unique string.
+    """
+    return uuid4().hex
