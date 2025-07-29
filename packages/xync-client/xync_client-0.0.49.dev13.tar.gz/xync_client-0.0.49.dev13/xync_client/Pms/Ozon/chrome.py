8e53c2b483829ae53babd343b2164b934a9ae743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:34 +0000

    Add package xync-client/0.0.49.dev13/xync_client-0.0.49.dev13.tar.gz

---
+++ packages/xync-client/xync_client-0.0.49.dev13.tar.gz/xync_client-0.0.49.dev13/xync_client/Pms/Ozon/chrome.py
@@ -0,0 +1,29 @@
+import undetected_chromedriver as uc
+import time
+
+
+def main():
+    # Initialize Chrome options
+    chrome_options = uc.ChromeOptions()
+    # chrome_options.add_argument('--headless')  # Uncomment this to run in headless mode
+    chrome_options.add_argument("--disable-gpu")  # Disable GPU usage for compatibility
+    chrome_options.add_argument("--no-sandbox")  # Disable sandboxing for compatibility
+
+    # Initialize the undetected ChromeDriver
+    driver = uc.Chrome(options=chrome_options)
+
+    try:
+        # Navigate to a webpage
+        driver.get("https://finance.ozon.ru/lk")
+
+        # Wait for a few seconds to allow the page to load
+        time.sleep(5)
+
+        # Print the contents of the page
+        print(driver.page_source)
+    finally:
+        driver.quit()
+
+
+if __name__ == "__main__":
+    main()
