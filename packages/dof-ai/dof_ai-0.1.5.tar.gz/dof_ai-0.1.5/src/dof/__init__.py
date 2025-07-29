bcb104a92f99f56d45b862ff4cd3fd9d29fd2398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:20:50 +0000

    Add package dof-ai/0.1.5/dof_ai-0.1.5.tar.gz

---
+++ packages/dof-ai/dof_ai-0.1.5.tar.gz/dof_ai-0.1.5/src/dof/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for dof."""
+
+from .dof import robot_hello, chain_robot_actions
+
+__author__ = """Pieter Becking"""
+__email__ = "ph.becking@gmail.com"
+__version__ = "0.1.1"
+
+__all__ = ["robot_hello", "chain_robot_actions"]
