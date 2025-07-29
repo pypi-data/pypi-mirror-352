7789de6baf215d9b9279d5bc57c35d792c3d68a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:04 +0000

    Add package zrb/1.8.2/zrb-1.8.2-py3-none-any.whl

---
+++ packages/zrb/zrb-1.8.2-py3-none-any.whl/zrb/util/cmd/command.py
@@ -0,0 +1,141 @@
+import asyncio
+import os
+import re
+import sys
+from collections.abc import Callable
+
+import psutil
+
+from zrb.cmd.cmd_result import CmdResult
+
+
+def check_unrecommended_commands(cmd_script: str) -> dict[str, str]:
+    """
+    Check a command script for the use of unrecommended or non-POSIX compliant commands.
+
+    Args:
+        cmd_script (str): The command script string to check.
+
+    Returns:
+        dict[str, str]: A dictionary where keys are the violating commands/patterns
+            and values are the reasons they are unrecommended.
+    """
+    banned_commands = {
+        "<(": "Process substitution isn't POSIX compliant and causes trouble",
+        "column": "Command isn't included in Ubuntu packages and is not POSIX compliant",
+        "echo": "echo isn't consistent across OS; use printf instead",
+        "eval": "Avoid eval as it can accidentally execute arbitrary strings",
+        "realpath": "Not available by default on OSX",
+        "source": "Not POSIX compliant; use '.' instead",
+        " test": "Use '[' instead for consistency",
+        "which": "Command in not POSIX compliant, use command -v",
+    }
+    banned_commands_regex = {
+        r"grep.* -y": "grep -y does not work on Alpine; use grep -i",
+        r"grep.* -P": "grep -P is not valid on OSX",
+        r"grep[^|]+--\w{2,}": "grep long commands do not work on Alpine",
+        r'readlink.+-.*f.+["$]': "readlink -f behaves differently on OSX",
+        r"sort.*-V": "sort -V is not supported everywhere",
+        r"sort.*--sort-versions": "sort --sort-version is not supported everywhere",
+        r"\bls ": "Avoid using ls; use shell globs or find instead",
+    }
+    violations = {}
+    # Check banned commands
+    for cmd, reason in banned_commands.items():
+        if cmd in cmd_script:
+            violations[cmd] = reason
+    # Check banned regex patterns
+    for pattern, reason in banned_commands_regex.items():
+        if re.search(pattern, cmd_script):
+            violations[pattern] = reason
+    return violations
+
+
+async def run_command(
+    cmd: list[str],
+    cwd: str | None = None,
+    env_map: dict[str, str] | None = None,
+    print_method: Callable[..., None] | None = None,
+    register_pid_method: Callable[[int], None] | None = None,
+    max_output_line: int = 1000,
+    max_error_line: int = 1000,
+) -> tuple[CmdResult, int]:
+    async def __read_stream(
+        stream, print_method: Callable[..., None], max_lines: int
+    ) -> str:
+        lines = []
+        if hasattr(stream, "_limit"):
+            # The limit is set to 10 MB
+            stream._limit = 1024 * 1024 * 10
+        while True:
+            line = None
+            try:
+                line = await stream.readline()
+            except asyncio.exceptions.CancelledError:
+                pass
+            except asyncio.exceptions.LimitOverrunError as e:
+                # Recover by reading a limited chunk instead
+                await stream.read(e.consumed)
+                line = "<output line too long>"
+            except BaseException as e:
+                line = f"Error while reading stream {type(e)} {e}"
+                pass
+            if not line:
+                break
+            try:
+                line = line.decode("utf-8").rstrip()
+            except Exception:
+                pass
+            lines.append(line)
+            if len(lines) > max_lines:
+                lines.pop(0)  # Keep only the last max_lines
+            print_method(line)
+        return "\n".join(lines)
+
+    actual_print_method = print_method if print_method is not None else print
+    cmd_process = None
+    if cwd is None:
+        cwd = os.getcwd()
+    if env_map is None:
+        env_map = os.environ
+    cmd_process = await asyncio.create_subprocess_exec(
+        *cmd,
+        cwd=cwd,
+        stdin=sys.stdin if sys.stdin.isatty() else None,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        env=env_map,
+        bufsize=0,
+    )
+    if register_pid_method is not None:
+        register_pid_method(cmd_process.pid)
+    stdout_task = asyncio.create_task(
+        __read_stream(cmd_process.stdout, actual_print_method, max_output_line)
+    )
+    stderr_task = asyncio.create_task(
+        __read_stream(cmd_process.stderr, actual_print_method, max_error_line)
+    )
+    # Wait for process to complete and gather stdout/stderr
+    return_code = await cmd_process.wait()
+    stdout = await stdout_task
+    stderr = await stderr_task
+    return CmdResult(stdout, stderr), return_code
+
+
+def kill_pid(pid: int, print_method: Callable[..., None] | None = None):
+    """
+    Kill a process and its children given the parent process ID.
+
+    Args:
+        pid (int): The process ID of the parent process.
+        print_method (Callable[..., None] | None): A method to print status messages.
+            Defaults to the built-in print function.
+    """
+    actual_print_method = print_method if print_method is not None else print
+    parent = psutil.Process(pid)
+    children = parent.children(recursive=True)
+    for child in children:
+        actual_print_method(f"Killing child process {child.pid}")
+        child.terminate()
+    actual_print_method(f"Killing process {pid}")
+    parent.terminate()
