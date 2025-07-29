6487d5c148ebf776dbf08a9e9267ad4d822f6d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:09 +0000

    Add package xzero-file/0.0.8/xzero_file-0.0.8.tar.gz

---
+++ packages/xzero-file/xzero_file-0.0.8.tar.gz/xzero_file-0.0.8/README.md
@@ -0,0 +1,19 @@
+#### easy manage file by web
+
+#### feature
+
+- download file
+- upload file
+
+> NOTE: not security, used in local net.
+
+#### usage
+```
+pip install xzero-file
+python3 -m xzero_file.app
+```
+
+#### find more
+```
+python3 -m xzero_file.app --help
+```
---
+++ packages/xzero-file/xzero_file-0.0.8.tar.gz/xzero_file-0.0.8/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+def read_requirements(filename):
+    with open(os.path.join(os.path.dirname(__file__), 'xzero_file', filename), 'r') as f:
+        return [line.strip() for line in f if line.strip()]
+
+setup(
+    name='xzero_file',  # Replace with your project name
+    version='0.0.8',  # Replace with your project version
+    packages=find_packages(),
+    install_requires=read_requirements('requirements.txt'),
+    author='alick97',  # Replace with your name
+    author_email='alick97@outlook.com',  # Replace with your email
+    package_data={
+        '': ['requirements.txt', 'README.md', 'assets/**/*', 'assets/*']
+    },
+    include_package_data=True,
+    description='upload and download file by web',  # Replace with your description
+    long_description=open(os.path.join(os.path.dirname(__file__), 'xzero_file', 'README.md')).read(),  # Optional: Read from README.md
+    long_description_content_type='text/markdown',  # Optional: If you're using Markdown in README.md
+    url='https://github.com/alick97/xzero-file',  # Replace with your project's URL (e.g., GitHub)
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Replace with your project's license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Replace with your project's minimum Python version
+)
