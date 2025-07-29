b708633f020aff05b1275ca04e5bc27ac2013afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:47 +0000

    Add package spdx_checker/0.1.4/spdx_checker-0.1.4-cp313-cp313-win_amd64.whl

---
+++ packages/spdx_checker/spdx_checker-0.1.4-cp313-cp313-win_amd64.whl/builder.py
@@ -0,0 +1,39 @@
+import sysconfig
+import platform
+from setuptools.command.build_ext import build_ext
+
+
+INSTALL_BASE = sysconfig.get_config_var("installed_base")
+LIB_DIR = sysconfig.get_config_var("LIBDIR")
+INCLUDE_DIR = sysconfig.get_config_var("INCLUDEPY")
+PYTHON_VERSION = sysconfig.get_config_var("py_version_short")
+PACKAGE_NAME = "spdx_checker"
+
+
+class ZigBuilder(build_ext):
+    def build_extension(self, ext) -> None:
+        is_windows = platform.system() == "Windows"
+        is_macos = platform.system() == "Darwin"
+
+        cmd = [
+            "zig",
+            "build-lib",
+            "-lc",
+            "-dynamic",
+            "-D",
+            "ReleaseFast",
+            "-I",
+            INCLUDE_DIR,
+            "-L",
+            LIB_DIR,
+            f"-femit-bin={self.get_ext_fullpath(ext.name)}",
+        ]
+
+        if is_windows:
+            cmd += ["-l", "python3"]
+        elif is_macos:
+            cmd += ["-l", "python" + PYTHON_VERSION]
+
+        cmd.append(ext.sources[0])
+
+        self.spawn(cmd)
