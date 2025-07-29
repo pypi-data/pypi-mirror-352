b652bd0b378ba2fa3de1f61fca08822d3e14eb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:04:49 +0000

    Add package randovania/9.2.0/randovania-9.2.0-py3-none-any.whl

---
+++ packages/randovania/randovania-9.2.0-py3-none-any.whl/randovania/games/fusion/exporter/joke_hints.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from randovania.exporter.hints.joke_hints import GENERIC_JOKE_HINTS
+
+FUSION_JOKE_HINTS = GENERIC_JOKE_HINTS + [
+    "Space Pirates, strangely, dislike theft.",
+    "We've been trying to contact you about your ship's extended warranty.",
+    "Did you know Yellow X are lemon flavoured?",
+    "Did you know Green X are lime flavoured?",
+    "Did you know Red X are strawberry flavoured?",
+    "Did you know Blue X are blueberry flavoured?",
+    "I would tell you a hint, but Aran out of them.",
+    "If you do not know where you are going, you are playing METROID CONFUSION.",
+    "Someone who looked just like you already claimed this hint.",
+    "The electrified water is quite shocking.",
+    "Did Gary touch the thermostat again?",
+]
