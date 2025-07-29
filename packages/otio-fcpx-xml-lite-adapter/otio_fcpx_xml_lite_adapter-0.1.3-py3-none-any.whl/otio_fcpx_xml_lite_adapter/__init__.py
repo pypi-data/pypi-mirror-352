50e1f0370db1be3d0ac652e5109421f61ea784da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:22 +0000

    Add package otio-fcpx-xml-lite-adapter/0.1.3/otio_fcpx_xml_lite_adapter-0.1.3-py3-none-any.whl

---
+++ packages/otio-fcpx-xml-lite-adapter/otio_fcpx_xml_lite_adapter-0.1.3-py3-none-any.whl/otio_fcpx_xml_lite_adapter/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Contributors to the OpenTimelineIO project
+
+"""FCP X XML Lite Adapter for OpenTimelineIO"""
+
+# flake8: noqa
+
+from . import adapter 
