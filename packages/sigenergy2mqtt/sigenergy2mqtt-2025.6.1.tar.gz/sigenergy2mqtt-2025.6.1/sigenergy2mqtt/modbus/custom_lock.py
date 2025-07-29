19ffd701c21c26859e17cab8677bde684084df64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:38 +0000

    Add package sigenergy2mqtt/2025.6.1/sigenergy2mqtt-2025.6.1.tar.gz

---
+++ packages/sigenergy2mqtt/sigenergy2mqtt-2025.6.1.tar.gz/sigenergy2mqtt-2025.6.1/sigenergy2mqtt/modbus/custom_lock.py
@@ -0,0 +1,34 @@
+import logging
+from .client_factory import ClientFactory
+from pymodbus.client import AsyncModbusTcpClient as ModbusClient
+import asyncio
+
+
+class CustomLock:
+    def __init__(self, modbus: ModbusClient):
+        self._lock = asyncio.Lock()
+        self._waiters = 0
+        self._host = ClientFactory.get_host(modbus)
+
+    @property
+    def host(self) -> str:
+        return self._host
+
+    @property
+    def waiters(self):
+        return self._waiters
+
+    async def acquire(self):
+        self._waiters += 1
+        try:
+            await self._lock.acquire()
+        except asyncio.CancelledError:
+            logging.info(f"Attempt to acquire Modbus lock on {self._host} interrupted")
+        self._waiters -= 1
+
+    def release(self):
+        self._lock.release()
+        pass
+
+    def locked(self):
+        return self._lock.locked()
