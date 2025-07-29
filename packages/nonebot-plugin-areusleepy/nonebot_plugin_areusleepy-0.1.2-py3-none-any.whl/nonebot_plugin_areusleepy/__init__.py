80b2d4af57d0b5556d5df3c8728984a830d29efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:03 +0000

    Add package nonebot-plugin-areusleepy/0.1.2/nonebot_plugin_areusleepy-0.1.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-areusleepy/nonebot_plugin_areusleepy-0.1.2-py3-none-any.whl/nonebot_plugin_areusleepy/__init__.py
@@ -0,0 +1,19 @@
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters
+
+from . import __main__ as __main__
+
+from .config import Config
+
+__version__ = "0.1.2"
+__plugin_meta__ = PluginMetadata(
+    name="AreYouSleepy",
+    description="基于 sleepy-project/sleepy 项目的状态查询插件！",
+    usage="/areusleepy [url]",
+    type="application",
+    homepage="https://github.com/Murasame-Dev/nonebot-plugin-areusleepy",
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
+    config=Config,
+)
+
+config = get_plugin_config(Config)
