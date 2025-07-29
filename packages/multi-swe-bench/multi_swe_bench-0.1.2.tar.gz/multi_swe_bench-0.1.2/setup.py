beef2dfdef6ff5a995bac36ef5befa875a14614a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:57 +0000

    Add package multi-swe-bench/0.1.2/multi_swe_bench-0.1.2.tar.gz

---
+++ packages/multi-swe-bench/multi_swe_bench-0.1.2.tar.gz/multi_swe_bench-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="multi-swe-bench",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "dataclasses_json",
+        "docker",
+        "tqdm",
+        "gitpython",
+        "toml",
+        "pyyaml",
+        "PyGithub",
+        "unidiff",
+    ],
+    author="Daoguang Zan",
+    author_email="zandaoguang@bytedance.com",
+    description="Multi-SWE-bench: A Multilingual Benchmark for Issue Resolving",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/multi-swe-bench/multi-swe-bench",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
