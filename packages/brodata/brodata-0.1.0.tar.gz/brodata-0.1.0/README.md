1fab4e00fa9567cc64004b89ca32b8b5a12ba924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:52 +0000

    Add package brodata/0.1.0/brodata-0.1.0.tar.gz

---
+++ packages/brodata/brodata-0.1.0.tar.gz/brodata-0.1.0/README.md
@@ -0,0 +1,2 @@
+# brodata
+A Python-package to download and process subsurface data from DINO and BRO
---
+++ packages/brodata/brodata-0.1.0.tar.gz/brodata-0.1.0/brodata/__init__.py
@@ -0,0 +1,17 @@
+# ruff: noqa: F401
+from . import (
+    bhr,
+    bro,
+    cpt,
+    dino,
+    gar,
+    gld,
+    gmn,
+    gmw,
+    guf,
+    plot,
+    sfr,
+    util,
+    webservices,
+)
+from .version import __version__
