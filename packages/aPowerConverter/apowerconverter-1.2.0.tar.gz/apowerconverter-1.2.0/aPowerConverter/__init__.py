c7a22dfc6465899d5726dac3fbc8b6ca371c5124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:00 +0000

    Add package aPowerConverter/1.2.0/apowerconverter-1.2.0.tar.gz

---
+++ packages/aPowerConverter/apowerconverter-1.2.0.tar.gz/apowerconverter-1.2.0/aPowerConverter/__init__.py
@@ -0,0 +1,9 @@
+"""
+A's shady changes - A powerful DOCX to AsciiDoc converter
+"""
+
+from .converter import convert_docx_to_adoc
+
+__version__ = '1.2.0'
+__author__ = 'Attoz'
+__email__ = 'attoz@attoz.com'
