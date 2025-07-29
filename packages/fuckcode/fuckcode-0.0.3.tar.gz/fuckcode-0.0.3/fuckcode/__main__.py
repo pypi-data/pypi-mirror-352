a679d0b96f4574fe7e295ed3f9a1a238cda24aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:41 +0000

    Add package fuckcode/0.0.3/fuckcode-0.0.3.tar.gz

---
+++ packages/fuckcode/fuckcode-0.0.3.tar.gz/fuckcode-0.0.3/fuckcode/__main__.py
@@ -0,0 +1,30 @@
+"""
+入口文件
+"""
+import typer
+from . import translate
+
+app = typer.Typer()
+
+    
+@app.command()
+def translate_fc(fc_code: str, o: str = typer.Option(..., "--output", "-o")):
+    """
+    翻译 FuckCode 到 C++
+    """
+    #读取 fc_code
+    with open(fc_code, "r", encoding="utf-8") as f:
+        fc_code_text = f.read()
+    cpp_code = translate.translate_fc_to_cpp(fc_code_text)
+    with open(o, "w", encoding="utf-8") as f:
+        f.write(cpp_code)
+    typer.echo(f"已保存到 {o}")
+
+def main():
+    """
+    主函数
+    """
+    app()
+
+if __name__ == "__main__":
+    main()
