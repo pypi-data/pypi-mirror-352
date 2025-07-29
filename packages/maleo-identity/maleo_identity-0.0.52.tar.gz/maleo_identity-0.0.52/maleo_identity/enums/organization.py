d54b84c6fc2b955c9682861fb5973517888a6ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:52 +0000

    Add package maleo-identity/0.0.52/maleo_identity-0.0.52.tar.gz

---
+++ packages/maleo-identity/maleo_identity-0.0.52.tar.gz/maleo_identity-0.0.52/maleo_identity/enums/organization.py
@@ -0,0 +1,10 @@
+from enum import StrEnum
+
+class MaleoIdentityOrganizationEnums:
+    class IdentifierType(StrEnum):
+        ID = "id"
+        UUID = "uuid"
+        KEY = "key"
+
+    class ExpandableFields(StrEnum):
+        ORGANIZATION_TYPE = "organization_type"
