5bf08444832bca18dee093b8bf9b1bee5d9d4625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:17 +0000

    Add package uefi-development/0.5.3/uefi_development-0.5.3.tar.gz

---
+++ packages/uefi-development/uefi_development-0.5.3.tar.gz/uefi_development-0.5.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="uefi_development",               # Name des Pakets
+    version="0.5.3",                     # Version
+    author="hexzhen3x7",
+    author_email="hexzhen3x7@blackzspace.de",
+    description="This module is for easily creating a uefi sdk development enviroment!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/alientools-org/uefi-development",  # Optional: GitHub URL
+    packages=find_packages(where="."),   # Alle Pakete im aktuellen Verzeichnis
+    python_requires=">=3.6",
+    install_requires=[                    # Abhängigkeiten, z.B. ["requests>=2.0"]
+        "requests",
+        "tqdm"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",      # Lizenz anpassen
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+             "uefi_development=uefi_development.main:main",  # Falls CLI-Tool
+        ],
+    },
+)
