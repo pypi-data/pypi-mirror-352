b3a497d67928c5af6db5c715d91a2d25e0d4327e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:23 +0000

    Add package reactpy/2.0.0b2/reactpy-2.0.0b2.tar.gz

---
+++ packages/reactpy/reactpy-2.0.0b2.tar.gz/reactpy-2.0.0b2/src/js/README.md
@@ -0,0 +1,3 @@
+# ReactPy Client
+
+An ES6 Javascript client for ReactPy
---
+++ packages/reactpy/reactpy-2.0.0b2.tar.gz/reactpy-2.0.0b2/src/reactpy/static/pyscript/deprecations-manager-B8Tz2RBD.js
@@ -0,0 +1,2 @@
+import{notify as e}from"./error-Bd5T9j4s.js";import{e as o}from"./core-BsIVS0z4.js";function r(){const e=document.querySelectorAll("script");for(const o of e)s(o.src)}function s(o){/\/pyscript\.net\/latest/.test(o)&&e("Loading scripts from latest is deprecated and will be removed soon. Please use a specific version instead.")}o.main.onReady.add(r),o.main.onWorker.add(r);
+//# sourceMappingURL=deprecations-manager-B8Tz2RBD.js.map
