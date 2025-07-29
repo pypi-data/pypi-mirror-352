153a2ea1f8aff1a112598d9fb8a1e383a737605a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:53 +0000

    Add package 2009escape/1.0.0/2009escape-1.0.0.tar.gz

---
+++ packages/2009escape/2009escape-1.0.0.tar.gz/2009escape-1.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    name="2009escape",
+    version="1.0.0",
+    #
+    include_package_data=True,
+    packages=["2009escape"],
+    py_modules=["mymodule"],
+)
