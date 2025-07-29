e89247b47ac2a05a8c77d303991e6976476e8c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:55 +0000

    Add package oscfar/1.1.9/oscfar-1.1.9.tar.gz

---
+++ packages/oscfar/oscfar-1.1.9.tar.gz/oscfar-1.1.9/setup.py
@@ -0,0 +1,30 @@
+__version__ = "1.1.9"
+
+from setuptools import setup
+
+description = open("README.md").read()
+
+setup(
+    name="oscfar",
+    version=__version__,
+    description="Order Statistic Constant False Alarm Rate detector package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author="Chloé Legué",
+    author_email="chloe.legue@mail.mcgill.ca",
+    packages=["oscfar"],
+    package_data={"": ["README.md"]},
+    package_dir={
+        "oscfar": ".",
+    },
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "pandas",
+        "scikit-learn",
+        "statsmodels",
+    ],
+    python_requires=">=3.8",
+    include_package_data=True,
+)
