124b5ed19b48dfd42751e1d9f29301e4efa4cb09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:39 +0000

    Add package gaspype/1.1.0/gaspype-1.1.0-py3-none-any.whl

---
+++ packages/gaspype/gaspype-1.1.0-py3-none-any.whl/gaspype/membrane.py
@@ -0,0 +1,11 @@
+from . import R, F, oxygen_partial_pressure, fluid, elements, FloatArray
+import numpy as np
+
+# Each O2 molecule is transported as two ions and each ion has a charged of 2e
+z_O2 = 4
+
+
+def voltage_potential(f1: fluid | elements, f2: fluid | elements, t: float, p: float = 1e5) -> FloatArray:
+    p1 = oxygen_partial_pressure(f1, t, p)
+    p2 = oxygen_partial_pressure(f2, t, p)
+    return R * t / (z_O2 * F) * np.log(p2 / p1)
