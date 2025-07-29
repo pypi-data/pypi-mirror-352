ffe200009b6b52954adc2c7770f2f05d4050f6ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:04:56 +0000

    Add package cupyd/0.4.0/cupyd-0.4.0-py3-none-any.whl

---
+++ packages/cupyd/cupyd-0.4.0-py3-none-any.whl/cupyd/core/etl.py
@@ -0,0 +1,302 @@
+import logging
+from collections import defaultdict
+from multiprocessing import Queue, set_start_method, get_start_method
+from time import time
+from typing import List, Dict, Union, Tuple
+
+from cupyd.core.communication.connector import (
+    Connector,
+    IntraProcessConnector,
+    InterProcessConnector,
+)
+from cupyd.core.communication.counter import MPCounter
+from cupyd.core.communication.event_flag import (
+    InterProcessEventFlag,
+    IntraProcessEventFlag,
+)
+from cupyd.core.communication.interruption_handler import InterruptionHandler
+from cupyd.core.computing.etl_worker import ETLWorkerProcess, ETLWorkerThread
+from cupyd.core.constants.logging import LOGGING_FORMAT_W_NODE_NAME, LOGGING_FORMAT
+from cupyd.core.constants.sentinel_values import NO_MORE_ITEMS
+from cupyd.core.exceptions import ETLExecutionError, InterruptedETL
+from cupyd.core.graph.algorithms import (
+    get_etl_segments,
+    topological_sort,
+    assign_names_and_ids_to_nodes,
+)
+from cupyd.core.graph.classes import Node
+from cupyd.core.models.etl_segment import ETLSegment
+from cupyd.core.models.node_exception import NodeException
+from cupyd.core.nodes import Loader
+from cupyd.core.nodes.bulker import Bulker
+from cupyd.core.nodes.debulker import DeBulker
+from cupyd.core.nodes.extractor import Extractor
+from cupyd.core.nodes.filter import Filter
+from cupyd.core.nodes.transformer import Transformer
+from cupyd.core.stats.progress_thread import ProgressThread
+from cupyd.core.stats.timings_thread import TimingsThread
+from cupyd.core.utils import format_seconds, get_subdict, use_cupyd_logging_format
+
+logger = logging.getLogger("cupyd.etl")
+
+
+class ETL:
+
+    def __init__(self, extractor: Extractor):
+        if not isinstance(extractor, Extractor):
+            raise TypeError(f"Invalid Extractor: {type(extractor)}")
+        self.extractor = extractor
+
+    def run(
+        self,
+        workers: int = 1,
+        raise_exception: bool = True,
+        raise_exception_if_interrupted: bool = True,
+        monitor_performance: bool = False,
+        show_progress: bool = True,
+        progress_refresh_interval: float = 2.5,
+        verbose: bool = True,
+        include_node_name_in_logs: bool = True,
+    ):
+        logging_format = LOGGING_FORMAT_W_NODE_NAME if include_node_name_in_logs else LOGGING_FORMAT
+
+        start_method = get_start_method()
+        set_start_method("spawn", force=True)  # ETLWorkerProcesses will be spawned
+
+        with use_cupyd_logging_format(logging_format):
+            (
+                nodes,
+                segments_by_id,
+                stop_event,
+                pause_event,
+                interruption_handler,
+                node_timings,
+                finished_workers,
+                monitor_performance_event_by_node_id,
+                counter_by_node_id,
+            ) = self._build(num_workers=workers, monitor_performance=monitor_performance)
+
+            if verbose:
+                logger.info("ETL build successful, running ETL...")
+
+            start_time = time()
+            exceptions_by_node_id: Dict[str, List[NodeException]] = defaultdict(list)
+
+            # this will handle interruptions in this main process (and the ETLWorkerThreads)
+            interruption_handler.start()
+
+            if monitor_performance:
+                timings_thread = TimingsThread(
+                    nodes=nodes, node_timings=node_timings, stop_event=stop_event
+                )
+                timings_thread.start()
+            else:
+                timings_thread = None
+
+            if show_progress:
+                finalize_event = IntraProcessEventFlag()
+                progress_thread = ProgressThread(
+                    nodes=nodes,
+                    counter_by_node_id=counter_by_node_id,
+                    stop_event=stop_event,
+                    finalize_event=finalize_event,
+                    refresh_interval=progress_refresh_interval,
+                )
+                progress_thread.start()
+            else:
+                progress_thread, finalize_event = None, None
+
+            # start all ETLWorkers
+            for segment in segments_by_id.values():
+                for worker in segment.workers_by_id.values():
+                    worker.start()
+
+            # auxiliary structures to easily access segment resources
+            active_worker_ids_by_segment_id = {}
+            workers_by_id = {}
+
+            for segment_id, segment in segments_by_id.items():
+                active_worker_ids_by_segment_id[segment_id] = set(segment.workers_by_id.keys())
+
+                for worker_id, worker in segment.workers_by_id.items():
+                    workers_by_id[worker_id] = worker
+
+            if verbose:
+                logger.info(f"ETL startup time: {round(time() - start_time, 4)} seconds")
+
+            # run until all ETLSegments are finished
+            while active_worker_ids_by_segment_id:
+                try:
+                    worker_id, segment_id, exception_by_node_id = finished_workers.get()
+                    workers_by_id[worker_id].join()
+                    active_worker_ids_by_segment_id[segment_id].remove(worker_id)
+
+                    if exception_by_node_id:
+                        for node_id, exception in exception_by_node_id.items():
+                            exceptions_by_node_id[node_id].append(exception)
+
+                    # if whole segment has finished, we will trigger the finalization of other
+                    # connected nodes (from other segments if connected via InterProcessConnector)
+                    if not active_worker_ids_by_segment_id[segment_id]:
+                        active_worker_ids_by_segment_id.pop(segment_id)
+
+                        for output_segment_num_workers, connector in segments_by_id[
+                            segment_id
+                        ].output_interprocess_connectors:
+                            for _ in range(output_segment_num_workers):
+                                connector.produce(NO_MORE_ITEMS)
+                except InterruptedError:
+                    continue
+
+            # stop the TimingsThread & ProgressThread, if running
+            if monitor_performance:
+                node_timings.put(NO_MORE_ITEMS)
+                timings_thread.join()
+
+            if show_progress:
+                finalize_event.set()
+                progress_thread.join()
+
+            # close InterProcessConnectors
+            for segment in segments_by_id.values():
+                for _, connector in segment.output_interprocess_connectors:
+                    connector.close()
+
+            elapsed_time = format_seconds(time() - start_time)
+
+            # restore multiprocessing start method & the previous signal handlers
+            set_start_method(start_method, force=True)
+            interruption_handler.restore_handlers()
+
+            if exceptions_by_node_id:
+                failed_nodes_names = []
+                for node in nodes:
+                    if node.id in exceptions_by_node_id:
+                        failed_nodes_names.append(node.name)
+                failed_nodes_names.sort()
+                logger.error(
+                    f'ETL finished with errors in nodes: {", ".join(failed_nodes_names)} | '
+                    f"Elapsed time: {elapsed_time}"
+                )
+            else:
+                if interruption_handler.interrupted():
+                    if raise_exception_if_interrupted:
+                        raise InterruptedETL(f"Elapsed time: {elapsed_time}")
+                    else:
+                        logger.warning(f"ETL interrupted! | {elapsed_time}")
+                else:
+                    logger.info(f"ETL finished | Elapsed time: {elapsed_time}")
+
+            if exceptions_by_node_id and raise_exception:
+                # we will only raise one exception although maybe multiple occurred
+                for exceptions in exceptions_by_node_id.values():
+                    raise ETLExecutionError(exceptions[0].traceback_formatted)
+
+    def _build(self, num_workers: int, monitor_performance: bool):
+        """Build the ETL."""
+
+        # 1. List all nodes & edges & ensure the ETL is a DAG
+        nodes, edges = topological_sort(root_node=self.extractor)
+
+        # 2. Each node will have its unique ID + an autogenerated name (if any wasn't provided)
+        assign_names_and_ids_to_nodes(nodes=nodes)
+
+        # 3. Determine the ETLSegments of the ETL. Each ETLSegment will consist in a group of
+        # consecutive nodes that will run on the same ETLWorker
+        segments = get_etl_segments(nodes=nodes, num_workers=num_workers)
+
+        # 4. Create the ETL connectors
+        input_connector_by_node_id: Dict[str, Connector] = {}
+        output_connectors_by_node_id: Dict[str, List[Connector]] = defaultdict(list)
+
+        for edge in edges:
+            origin: Node = edge.origin
+            target: Node = edge.target
+
+            origin_segment = None
+            target_segment = None
+
+            for segment in segments:
+                if origin.id in segment.node_ids:
+                    origin_segment = segment
+                if target.id in segment.node_ids:
+                    target_segment = segment
+
+            connector: Union[IntraProcessConnector, InterProcessConnector]
+
+            if isinstance(target, (Transformer, Loader, Filter, Bulker, DeBulker)):
+                queue_max_size = target.configuration.queue_max_size
+            else:
+                raise AttributeError('No "queue_max_size" attr in connected Node!')
+
+            if origin_segment.id == target_segment.id:
+                connector = IntraProcessConnector(maxsize=queue_max_size)
+            else:
+                connector = InterProcessConnector(maxsize=queue_max_size)
+                connector.start()
+
+            input_connector_by_node_id[target.id] = connector
+            output_connectors_by_node_id[origin.id].append(connector)
+
+            if isinstance(connector, InterProcessConnector):
+                origin_segment.output_interprocess_connectors.append(
+                    (target_segment.num_workers, connector)
+                )
+
+        # 5. Create necessary structures & ETL Workers
+        finished_workers: Queue[Tuple[str, str, Dict[str, NodeException]]] = Queue()
+        node_timings: Queue[Tuple[str, float]] = Queue(maxsize=25_000)
+        stop_event = InterProcessEventFlag()
+        pause_event = InterProcessEventFlag()
+        interruption_handler = InterruptionHandler(stop_event=stop_event)
+        counter_by_node_id: Dict[str, MPCounter] = {
+            node.id: MPCounter() for node in nodes if isinstance(node, Loader) and not node.outputs
+        }
+        monitor_performance_event_by_node_id = {
+            node.id: InterProcessEventFlag(start_set=monitor_performance) for node in nodes
+        }
+        segments_by_id: Dict[str, ETLSegment] = {}
+        etl_worker_num = 1
+
+        for segment in segments:
+            worker_class = ETLWorkerThread if segment.run_in_main_process else ETLWorkerProcess
+
+            for _ in range(segment.num_workers):
+                etl_worker_id = f"etl_worker_{etl_worker_num}"
+                etl_worker = worker_class(
+                    worker_id=etl_worker_id,
+                    segment_id=segment.id,
+                    nodes=segment.nodes,
+                    counters=get_subdict(dictionary=counter_by_node_id, keys=segment.node_ids),
+                    input_connector_by_node_id=get_subdict(
+                        dictionary=input_connector_by_node_id, keys=segment.node_ids
+                    ),
+                    output_connectors_by_node_id=get_subdict(
+                        dictionary=output_connectors_by_node_id, keys=segment.node_ids
+                    ),
+                    monitor_performance_event_by_node_id=get_subdict(
+                        dictionary=monitor_performance_event_by_node_id,
+                        keys=segment.node_ids,
+                    ),
+                    stop_event=stop_event,
+                    pause_event=pause_event,
+                    interruption_handler=interruption_handler,
+                    node_timings=node_timings,
+                    finished_workers=finished_workers,
+                )
+                segment.workers_by_id[etl_worker_id] = etl_worker  # type: ignore
+                etl_worker_num += 1
+
+            segments_by_id[segment.id] = segment
+
+        return (
+            nodes,
+            segments_by_id,
+            stop_event,
+            pause_event,
+            interruption_handler,
+            node_timings,
+            finished_workers,
+            monitor_performance_event_by_node_id,
+            counter_by_node_id,
+        )
---
+++ packages/cupyd/cupyd-0.4.0-py3-none-any.whl/cupyd/core/nodes/debulker.py
@@ -0,0 +1,15 @@
+from typing import final
+
+from cupyd.core.graph.classes import Node
+from cupyd.core.models.node_configuration import DeBulkerConfiguration
+
+
+class DeBulker(Node):
+    def __init__(self):
+        super().__init__()
+        self._configuration = DeBulkerConfiguration()
+
+    @final
+    @property
+    def configuration(self):
+        return self._configuration
