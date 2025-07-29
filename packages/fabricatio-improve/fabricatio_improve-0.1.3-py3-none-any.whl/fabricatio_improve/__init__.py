7c09f7df92a213d390129c067e4c1bb311698db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:42 +0000

    Add package fabricatio-improve/0.1.3/fabricatio_improve-0.1.3-py3-none-any.whl

---
+++ packages/fabricatio-improve/fabricatio_improve-0.1.3-py3-none-any.whl/fabricatio_improve/__init__.py
@@ -0,0 +1,8 @@
+"""Fabricatio is a Python library for building llm app using event-based agent structure."""
+
+from fabricatio_improve import capabilities, models
+
+__all__ = [
+    "capabilities",
+    "models",
+]
