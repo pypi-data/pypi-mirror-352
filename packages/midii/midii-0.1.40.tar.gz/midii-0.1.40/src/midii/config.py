a0386c7c09b11733d5b0096dc77ff72cc3c0caff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:54 +0000

    Add package midii/0.1.40/midii-0.1.40.tar.gz

---
+++ packages/midii/midii-0.1.40.tar.gz/midii-0.1.40/src/midii/config.py
@@ -0,0 +1,62 @@
+__all__ = [
+    "DEFAULT_TICKS_PER_BEAT",
+    "DEFAULT_TIME_SIGNATURE",
+    "DEFAULT_TEMPO",
+    "DEFAULT_BPM",
+    "DEFAULT_SAMPLING_RATE",
+    "DEFAULT_HOP_LENGTH",
+]
+
+DEFAULT_TICKS_PER_BEAT = 480
+DEFAULT_TIME_SIGNATURE = (4, 4)
+DEFAULT_TEMPO = 500000
+DEFAULT_BPM = 120
+DEFAULT_SAMPLING_RATE = 22050
+DEFAULT_HOP_LENGTH = 512
+
+COLOR = (
+    15,
+    165,
+    47,
+    87,
+    121,
+    9,
+    27,
+    190,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    9,
+    11,
+    30,
+    33,
+    37,
+    39,
+    51,
+    46,
+    63,
+    57,
+    87,
+    121,
+    91,
+    104,
+    123,
+    118,
+    129,
+    159,
+    124,
+    127,
+    157,
+    159,
+    135,
+    162,
+    194,
+    201,
+    226,
+    230,
+    217,
+)
