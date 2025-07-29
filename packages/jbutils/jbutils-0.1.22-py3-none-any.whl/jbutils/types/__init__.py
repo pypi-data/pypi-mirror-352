18ade31f3eb0a291eda042b098214f77d7c88565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:09 +0000

    Add package jbutils/0.1.22/jbutils-0.1.22-py3-none-any.whl

---
+++ packages/jbutils/jbutils-0.1.22-py3-none-any.whl/jbutils/types/__init__.py
@@ -0,0 +1,37 @@
+"""Common types for the jbutils package"""
+
+from collections.abc import Callable
+from typing import TypeVar, Optional, Pattern, Sequence
+
+# General Typing
+T = TypeVar("T")
+OptStr = Optional[str]
+OptInt = Optional[int]
+OptFloat = Optional[float]
+OptDict = Optional[dict]
+OptList = Optional[list]
+Opt = Optional[T]
+
+# Function Types
+Predicate = Callable[[T], bool]
+Function = Callable[..., None]
+TFunction = Callable[..., T]
+
+# Other
+Patterns = Sequence[str | Pattern[str]]
+DataPathList = list[str] | list[str | int] | list[int]
+DataPath = DataPathList | str | int
+
+__all__ = [
+    "OptStr",
+    "OptInt",
+    "OptFloat",
+    "OptDict",
+    "OptList",
+    "Opt",
+    "Patterns",
+    "Predicate",
+    "Function",
+    "TFunction",
+    "T",
+]
