611986f497d5f2e67a8f59545a04a6b994aea3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:45 +0000

    Add package scrutinydebugger/0.4.3/scrutinydebugger-0.4.3.tar.gz

---
+++ packages/scrutinydebugger/scrutinydebugger-0.4.3.tar.gz/scrutinydebugger-0.4.3/scrutiny/core/typehints.py
@@ -0,0 +1,12 @@
+#    typehints.py
+#        Contains some definition for type hints that are used across all project
+#
+#   - License : MIT - See LICENSE file.
+#   - Project :  Scrutiny Debugger (github.com/scrutinydebugger/scrutiny-main)
+#
+#   Copyright (c) 2021 Scrutiny Debugger
+
+from typing import TypedDict
+
+class EmptyDict(TypedDict):
+    pass
