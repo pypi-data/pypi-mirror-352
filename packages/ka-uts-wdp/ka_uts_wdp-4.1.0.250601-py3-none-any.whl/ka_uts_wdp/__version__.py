dfa1a12ecd05dbf0ec54bf86e4288f2944251267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:17 +0000

    Add package ka-uts-wdp/4.1.0.250601/ka_uts_wdp-4.1.0.250601-py3-none-any.whl

---
+++ packages/ka-uts-wdp/ka_uts_wdp-4.1.0.250601-py3-none-any.whl/ka_uts_wdp/__version__.py
@@ -0,0 +1,10 @@
+__title__ = 'ka_uts_wdp'
+__description__ = 'Watch Dog Processors Utilities.'
+__url__ = 'https://kosakya.de'
+__version__ = '4.1.0.250601'
+__build__ = 0x024200
+__author__ = 'Bernd Stroehle'
+__author_email__ = 'bernd.stroehle@gmail.com'
+__license__ = 'GPL-3.0-only WITH Classpath-Exception-2.0 OR BSD-3-Clause'
+__copyright__ = 'Copyright 2025 Bernd Stroehle'
+__cake__ = u'\u2728 \U0001f370 \u2728'
