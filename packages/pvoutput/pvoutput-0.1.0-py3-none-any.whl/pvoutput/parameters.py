6a201f8131deed5a19a19017c2581569091e14f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:52 +0000

    Add package pvoutput/0.1.0/pvoutput-0.1.0-py3-none-any.whl

---
+++ packages/pvoutput/pvoutput-0.1.0-py3-none-any.whl/pvoutput/parameters.py
@@ -0,0 +1,316 @@
+"""Standard parameter things."""
+
+from copy import copy
+from datetime import date, datetime, time
+
+
+from .utils import ALERT_TYPES, validate_delete_status_date
+
+__all__ = [
+    "ADDOUTPUT_PARAMETERS",
+    "ADDSTATUS_PARAMETERS",
+    "CALL_PARAMETERS",
+    "DELETESTATUS_PARAMETERS",
+    "DELETE_NOTIFICATION_PARAMETERS",
+    "REGISTER_NOTIFICATION_PARAMETERS",
+    "DEFAULT_REQUEST_TIMEOUT",
+]
+
+DEFAULT_REQUEST_TIMEOUT = 30
+
+standard_parameters = {
+    "d": {
+        "required": True,
+        "description": "Date",
+        "format": r"^(20\d{2})(\d{2})(\d{2})$",
+        "type": str,
+        "donation_required": False,
+        "default": lambda: datetime.today().strftime("%Y%m%d"),
+    },
+    "t": {
+        "required": True,
+        "description": "Time",
+        "format": r"^([0-1][0-9]|2[0-3]):[0-5][0-9]$",
+        "type": str,
+        "donation_required": False,
+        "default": lambda: datetime.now().strftime("%H:%M"),
+    },
+}
+
+CALL_PARAMETERS = {
+    "data": {
+        "required": False,
+        "type": dict,
+        "donation_required": False,
+        "default": None,
+    },
+    "params": {
+        "required": False,
+        "type": dict,
+        "donation_required": False,
+    },
+    "headers": {
+        "required": False,
+        "type": dict,
+        "donation_required": False,
+    },
+}
+
+DELETESTATUS_PARAMETERS = {
+    "date_val": {
+        "required": True,
+        "description": "Date",
+        "type": date,
+        "donation_required": False,
+        "additional_validators": [validate_delete_status_date],
+    },
+    "time_val": {
+        "required": False,
+        "description": "Time",
+        "type": time,
+        "donation_required": False,
+    },
+}
+
+# <https://pvoutput.org/help/api_specification.html#add-batch-status-service>
+ADDBATCHSTATUS_PARAMETERS = {
+    "data": {
+        "description": "see https://pvoutput.org/help/api_specification.html#api-add-batch-status-data-structure",
+        "required": True,
+        "type": str,  # this isn't the real format but you'll have to deal until someone patches it
+    },
+    "c1": {
+        "required": False,
+        "type": bool,
+    },
+    "n": {
+        "required": False,
+        "type": bool,
+    },
+}
+
+ADDSTATUS_PARAMETERS = {
+    "d": copy(standard_parameters["d"]),
+    "t": copy(standard_parameters["t"]),
+    "v1": {
+        "required": False,
+        "description": "Energy Generation (Wh)",
+        "donation_required": False,
+        "type": int,
+    },
+    "v2": {
+        "required": False,
+        "description": "Power Exporting (W)",
+        "donation_required": False,
+    },
+    "v3": {
+        "required": False,
+        "description": "Energy Consumption (Wh)",
+        "donation_required": False,
+        "maxval": 200000,
+    },
+    "v4": {
+        "required": False,
+        "description": "Power Importing (W)",
+        "donation_required": False,
+    },
+    "v5": {
+        "required": False,
+        "description": "Temperature (C)",
+        "type": float,
+        "donation_required": False,
+    },
+    "v6": {
+        "required": False,
+        "description": "Voltage",
+        "type": float,
+        "donation_required": False,
+    },
+    "n": {
+        "required": False,
+        "description": "Net Flag",
+        "donation_required": False,
+        "format": r"^1$",
+    },
+    "v7": {
+        "required": False,
+        "description": "Extended Value 1",
+        "donation_required": True,
+    },
+    "v8": {
+        "required": False,
+        "description": "Extended Value 2",
+        "donation_required": True,
+    },
+    "v9": {
+        "required": False,
+        "description": "Extended Value 3",
+        "donation_required": True,
+    },
+    "v10": {
+        "required": False,
+        "description": "Extended Value 4",
+        "donation_required": True,
+    },
+    "v11": {
+        "required": False,
+        "description": "Extended Value 5",
+        "donation_required": True,
+    },
+    "v12": {
+        "required": False,
+        "description": "Extended Value 6",
+        "donation_required": True,
+    },
+    "c1": {
+        "required": False,
+        "description": "Cumulative Flag",
+        "type": int,
+        "format": r"[123]{1}",
+        "donation_required": False,
+    },
+    "m1": {
+        "required": False,
+        "description": "Text Message 1",
+        "maxlen": 30,
+        "donation_required": True,
+    },
+    "required_oneof": {"keys": ["v1", "v2", "v3", "v4"]},
+}
+"""
+Cumulative Energy
+The following values are valid for the c1 flag.
+    1 Both v1 and v3 values are lifetime energy values. Consumption and generation energy is reset to 0 at the start of the day.
+    2 Only v1 generation is a lifetime energy value.
+    3 Only v3 consumption is a lifetime energy value.
+"""
+
+ADDOUTPUT_PARAMETERS = {
+    "d": copy(standard_parameters["d"]),
+    "g": {
+        "required": False,
+        "description": "Generated (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+    "e": {
+        "required": False,
+        "description": "Exported (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+    "pp": {
+        "required": False,
+        "description": "Peak Power (W)",
+        "type": int,
+        "donation_required": False,
+    },
+    "pt": {
+        "required": False,
+        "description": "Peak Time",
+        "format": r"^([0-1][0-9]|2[0-3]):[0-5][0-9]$",
+        "type": str,
+        "donation_required": False,
+    },
+    "cd": {
+        "required": False,
+        "description": "Condition",
+        "format": "Fine|Partly Cloudy|Mostly Cloudy|Cloudy|Showers|Snow|Hazy|Fog|Dusty|Frost|Storm",
+        "type": str,
+        "donation_required": False,
+    },
+    "tm": {
+        "required": False,
+        "description": "Min Temp (C)",
+        "type": float,
+        "donation_required": False,
+    },
+    "tx": {
+        "required": False,
+        "description": "Max Temp (C)",
+        "type": float,
+        "donation_required": False,
+    },
+    "cm": {
+        "required": False,
+        "description": "Comments",
+        "type": str,
+        "donation_required": False,
+    },
+    "ip": {
+        "required": False,
+        "description": "Import Peak (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+    "io": {
+        "required": False,
+        "description": "Import Off Peak (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+    "is": {
+        "required": False,
+        "description": "Import Shoulder (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+    "ih": {
+        "required": False,
+        "description": "Import High Shoulder (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+    "c": {
+        "required": False,
+        "description": "Consumption (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+    "ep": {
+        "required": False,
+        "description": "Export Peak (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+    "eo": {
+        "required": False,
+        "description": "Export Off-Peak (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+    "es": {
+        "required": False,
+        "description": "Export Shoulder (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+    "eh": {
+        "required": False,
+        "description": "Export High Shoulder (Wh)",
+        "type": int,
+        "donation_required": False,
+    },
+}
+
+
+DELETE_NOTIFICATION_PARAMETERS = {
+    "appid": {
+        "required": True,
+        "type": str,
+        "maxlen": 100,
+    },
+    "alerttype": {
+        "required": True,
+        "type": int,
+        "choices": ALERT_TYPES,
+    },
+}
+
+REGISTER_NOTIFICATION_PARAMETERS = copy(DELETE_NOTIFICATION_PARAMETERS)
+# registering notifications requires a URL
+REGISTER_NOTIFICATION_PARAMETERS["url"] = {
+    "required": True,
+    "type": str,
+    "maxlen": 150,
+}
---
+++ packages/pvoutput/pvoutput-0.1.0-py3-none-any.whl/pvoutput/utils.py
@@ -0,0 +1,109 @@
+"""Utilities"""
+
+from datetime import date, datetime, timedelta
+from typing import Any, Dict, List, Tuple
+
+__all__ = [
+    "URLS",
+    "validate_delete_status_date",
+    "ALERT_TYPES",
+]
+
+BASE_URL = "https://pvoutput.org/service/r2/"
+
+URLS = {
+    "addbatchstatus": {
+        "url": BASE_URL + "addbatchstatus.jsp",
+        "method": "POST",
+    },
+    "addstatus": (
+        BASE_URL + "addstatus.jsp",
+        "POST",
+    ),
+    "addoutput": (
+        BASE_URL + "addoutput.jsp",
+        "POST",
+    ),
+    "deletestatus": (
+        BASE_URL + "deletestatus.jsp",
+        "POST",
+    ),
+    "getstatus": (
+        BASE_URL + "getstatus.jsp",
+        "GET",
+    ),
+    "getsystem": (
+        BASE_URL + "getsystem.jsp",
+        "GET",
+    ),
+    "registernotification": (
+        BASE_URL + "registernotification.jsp",
+        "GET",
+    ),
+    "deregisternotification": (
+        BASE_URL + "deregisternotification.jsp",
+        "GET",
+    ),
+}
+
+ALERT_TYPES = {
+    0: "All Notifications",
+    1: "Private Message",
+    3: "Joined Team",
+    4: "Added Favourite",
+    5: "High Consumption Alert",
+    6: "System Idle Alert",
+    8: "Low Generation Alert",
+    11: "Performance Alert",
+    14: "Standby Cost Alert",
+    15: "Extended Data V7 Alert",
+    16: "Extended Data V8 Alert",
+    17: "Extended Data V9 Alert",
+    18: "Extended Data V10 Alert",
+    19: "Extended Data V11 Alert",
+    20: "Extended Data V12 Alert",
+    23: "High Net Power Alert",
+    24: "Low Net Power Alert",
+}
+
+
+def responsedata_to_response(input_data: List[str]) -> Tuple[Dict[str, Any], List[str]]:
+    """Turns the status output into a dict"""
+    # pylint: disable=invalid-name
+    d, t, v1, v2, v3, v4, v5, v6, normalised_output, *extras = input_data
+
+    # if there's no data, you get "NaN". Here we change that to NoneType
+    responsedata = {
+        "d": d,
+        "t": t,
+        "timestamp": datetime.strptime(f"{d} {t}", "%Y%m%d %H:%M"),
+        "v1": None if v1 == "NaN" else float(v1),
+        "v2": None if v2 == "NaN" else float(v2),
+        "v3": None if v3 == "NaN" else float(v3),
+        "v4": None if v4 == "NaN" else float(v4),
+        "v5": None if v5 == "NaN" else float(v5),
+        "v6": None if v6 == "NaN" else float(v6),
+        "normalised_output": float(normalised_output),
+    }
+    return responsedata, extras
+
+
+def get_rate_limit_header(response_object: Any) -> Dict[str, str]:
+    """gets the rate limit header from the returned headers"""
+    retval = {}
+    for key in response_object.headers.keys():
+        if key.startswith("X-Rate-Limit"):
+            retval[key] = str(response_object.headers[key])
+    return retval
+
+
+def validate_delete_status_date(date_val: date) -> None:
+    """runs the validation for the date_val option of delete_status"""
+    yesterday = date.today() - timedelta(days=1)
+    tomorrow = date.today() + timedelta(days=1)
+    # you can't delete back past yesterday
+    if date_val < yesterday:
+        raise ValueError(f"date_val can only be yesterday or today, you provided {date_val}")
+    # you can't delete forward of today
+    if date_val >= tomorrow:
+        raise ValueError(f"date_val can only be yesterday or today, you provided {date_val}")
