572c16919ac12e3a151d9b2c3586250e60faa21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:42 +0000

    Add package webeasyml/0.1.3/webeasyml-0.1.3.tar.gz

---
+++ packages/webeasyml/webeasyml-0.1.3.tar.gz/webeasyml-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='webeasyml',
+    version='0.1.3',
+    description='wzai ; A simple ML web server,from wenzhou S&T High school',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='linmy',
+    packages=find_packages(),
+    package_data={
+        'webeasyml': [
+            '*.py', '*.md', '*.txt', '*.js', '*.css', '*.html'
+        ],
+    },
+    install_requires=[
+        'flask',
+        'werkzeug',
+        # 其他依赖
+    ],
+    include_package_data=True,
+)
