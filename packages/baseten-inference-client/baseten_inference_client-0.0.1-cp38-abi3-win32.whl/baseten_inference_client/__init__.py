4ec6c5b64e363093100e6808f90a6fec4a42da6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:57 +0000

    Add package baseten-inference-client/0.0.1/baseten_inference_client-0.0.1-cp38-abi3-win32.whl

---
+++ packages/baseten-inference-client/baseten_inference_client-0.0.1-cp38-abi3-win32.whl/baseten_inference_client/__init__.py
@@ -0,0 +1,5 @@
+from .baseten_inference_client import *
+
+__doc__ = baseten_inference_client.__doc__
+if hasattr(baseten_inference_client, "__all__"):
+    __all__ = baseten_inference_client.__all__
