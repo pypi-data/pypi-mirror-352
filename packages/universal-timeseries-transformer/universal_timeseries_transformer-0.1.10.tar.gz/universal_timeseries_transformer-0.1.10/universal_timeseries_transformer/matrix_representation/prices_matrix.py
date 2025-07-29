d0eb7ca48962b0bc6f95fe8a403caef605d0a48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:19 +0000

    Add package universal-timeseries-transformer/0.1.10/universal_timeseries_transformer-0.1.10.tar.gz

---
+++ packages/universal-timeseries-transformer/universal_timeseries_transformer-0.1.10.tar.gz/universal_timeseries_transformer-0.1.10/universal_timeseries_transformer/matrix_representation/prices_matrix.py
@@ -0,0 +1,15 @@
+from string_date_controller import get_all_data_historical_dates
+from .timeseries_matrix import TimeseriesMatrix
+
+class PricesMatrix(TimeseriesMatrix):
+    def __init__(self, prices, date_ref=None):
+        super().__init__(df=prices, index_ref=date_ref)
+        self._historical_dates = None
+
+    @property
+    def historical_dates(self):
+        if self._historical_dates is None:
+            self._historical_dates = get_all_data_historical_dates(dates=self.dates, date_ref=self.date_ref)
+        return self._historical_dates
+
+    
