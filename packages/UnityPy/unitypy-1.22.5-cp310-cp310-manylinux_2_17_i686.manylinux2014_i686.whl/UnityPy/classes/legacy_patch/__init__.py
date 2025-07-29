7635700bd1ee5716790bc5a572b95b5b62ba24e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:24 +0000

    Add package UnityPy/1.22.5/unitypy-1.22.5-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/UnityPy/unitypy-1.22.5-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl/UnityPy/classes/legacy_patch/__init__.py
@@ -0,0 +1,8 @@
+from .AudioClip import AudioClip as AudioClip
+from .GameObject import GameObject as GameObject
+from .Mesh import Mesh as Mesh
+from .Renderer import Renderer as Renderer
+from .Shader import Shader as Shader
+from .Sprite import Sprite as Sprite
+from .Texture2D import Texture2D as Texture2D
+from .Texture2DArray import Texture2DArray as Texture2DArray
