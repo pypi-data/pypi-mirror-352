78b7c398c8c0021d26c37f7cc43dc2b6e3633715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:11 +0000

    Add package unofficial-tabdeal-api/0.2.0/unofficial_tabdeal_api-0.2.0.tar.gz

---
+++ packages/unofficial-tabdeal-api/unofficial_tabdeal_api-0.2.0.tar.gz/unofficial_tabdeal_api-0.2.0/src/unofficial_tabdeal_api/__init__.py
@@ -0,0 +1,15 @@
+"""Unofficial Tabdeal API.
+--------------------------
+
+a Package to communicate with Tabdeal platform
+
+:copyright: (c) 2025-present MohsenHNSJ
+:license: MIT, see LICENSE for more details
+
+"""  # noqa: D205
+
+__title__ = "unofficial-tabdeal-api"
+__author__ = "MohsenHNSJ"
+__license__ = "MIT"
+__copyright__ = "Copyright 2025-present MohsenHNSJ"
+__version__ = "0.2.0"
