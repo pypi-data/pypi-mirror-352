e77c8e1bf393e8110cfe3b5a44f28ba1a685a3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:09:36 +0000

    Add package path2dream-processors/0.1.16/path2dream_processors-0.1.16.tar.gz

---
+++ packages/path2dream-processors/path2dream_processors-0.1.16.tar.gz/path2dream_processors-0.1.16/src/path2dream_processors/summary_processor.py
@@ -0,0 +1,22 @@
+from typing import List, Tuple
+from pydantic import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from langchain import hub
+
+
+class TitleAndSummary(BaseModel):
+    title: str = Field(description="The title of the document")
+    summary: str = Field(description="The short summarization of all insights of this document in relevance to the purpose of the project.")
+
+
+class LangChainSummaryProcessor:
+    def __init__(self):
+        llm = ChatOpenAI(model="gpt-4.1", temperature=0)
+        llm = llm.with_structured_output(TitleAndSummary)
+        prompt = hub.pull("path2dream_context_summarizer")
+        self.chain = prompt | llm
+
+    async def generate_summary(self, context: str, text_blocks: List[str]) -> Tuple[str, str]:
+        aggregated_text = "\n".join(text_blocks)
+        result = await self.chain.ainvoke({"context": context, "new_document": aggregated_text})
+        return result.title, result.summary
