bf906c0147c215524447a48e91b073a1de42fb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:38 +0000

    Add package smc-lammps/0.1.0/smc_lammps-0.1.0-py3-none-any.whl

---
+++ packages/smc-lammps/smc_lammps-0.1.0-py3-none-any.whl/smc_lammps/console.py
@@ -0,0 +1,19 @@
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
+
+console = Console()
+
+
+def warn(message):
+    """Prints the warning message in a panel."""
+    console = Console()
+    console.print(
+        Panel(
+            Text(message, style="bold yellow"),
+            title="⚠️ WARNING",
+            title_align="left",
+            border_style="yellow",
+            expand=False,
+        )
+    )
