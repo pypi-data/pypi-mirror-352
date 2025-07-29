9a4db726262e0aac368476d64b65c5ade7c55ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:17 +0000

    Add package yubin-mcp/0.0.3/yubin_mcp-0.0.3.tar.gz

---
+++ packages/yubin-mcp/yubin_mcp-0.0.3.tar.gz/yubin_mcp-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yubin_mcp",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=[
+        "posuto",
+        "geopy",
+        "mcp[cli]"
+    ],
+    entry_points={
+        "console_scripts": [
+            "yubin-mcp=yubin_mcp.server:main"
+        ]
+    },
+    author="Masashi Morita",
+    author_email="masashi.morita.mm@gmail.com",
+    description="MCP server that retrieves addresses and map links from Japanese postal codes and latitude/longitude coordinates",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/masashimorita/yubin-mcp.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
