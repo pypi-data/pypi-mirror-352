4bcfdbc15017d1cf47a6db1806f891d7961ca512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:09 +0000

    Add package apexnova/1.2.387/apexnova-1.2.387-py3-none-any.whl

---
+++ packages/apexnova/apexnova-1.2.387-py3-none-any.whl/apexnova/stub/authorization/rule/fail_if_not_owner_rule.py
@@ -0,0 +1,29 @@
+"""Fail if not owner authorization rule."""
+
+from typing import TypeVar, Callable
+
+from apexnova.authorization_context_pb2 import AuthorizationContext
+from ..authorization_rule import AuthorizationRule
+from ..authorization_status import AuthorizationStatus
+
+T = TypeVar("T")
+
+
+class FailIfNotOwnerRule(AuthorizationRule[T]):
+    """Authorization rule that fails if user is not the owner."""
+
+    def __init__(self, owner_id_extractor: Callable[[T], str]):
+        """
+        Initialize with owner ID extractor function.
+
+        Args:
+            owner_id_extractor: Function to extract owner ID from entity
+        """
+        self.owner_id_extractor = owner_id_extractor
+
+    def evaluate(self, context: AuthorizationContext, entity: T) -> AuthorizationStatus:
+        """Returns NEXT if user is owner, FAIL otherwise."""
+        if self.owner_id_extractor(entity) == context.id:
+            return AuthorizationStatus.NEXT
+        else:
+            return AuthorizationStatus.FAIL
