d9259f2be13a0195af1c2a38a93eb65466690072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:12 +0000

    Add package nixt/322/nixt-322.tar.gz

---
+++ packages/nixt/nixt-322.tar.gz/nixt-322/nixt/disk.py
@@ -0,0 +1,89 @@
+# This file is placed in the Public Domain.
+
+
+"persistence"
+
+
+import datetime
+import json.decoder
+import os
+import pathlib
+import threading
+
+
+from .object import fqn, update
+from .serial import dump, load
+from .store  import store
+
+
+lock = threading.RLock()
+j    = os.path.join
+
+
+class Error(Exception):
+
+    pass
+
+
+class Cache:
+
+    objs = {}
+
+    @staticmethod
+    def add(path, obj):
+        Cache.objs[path] = obj
+
+    @staticmethod
+    def get(path):
+        return Cache.objs.get(path, None)
+
+    @staticmethod
+    def typed(matcher):
+        for key in Cache.objs:
+            if matcher not in key:
+                continue
+            yield Cache.objs.get(key)
+
+
+def cdir(path):
+    pth = pathlib.Path(path)
+    pth.parent.mkdir(parents=True, exist_ok=True)
+
+
+def getpath(obj):
+    return j(store(ident(obj)))
+
+
+def ident(obj):
+    return j(fqn(obj),*str(datetime.datetime.now()).split())
+
+
+def read(obj, path):
+    with lock:
+        with open(path, "r", encoding="utf-8") as fpt:
+            try:
+                update(obj, load(fpt))
+            except json.decoder.JSONDecodeError as ex:
+                raise Error(path) from ex
+
+
+def write(obj, path=""):
+    with lock:
+        if path == "":
+            path = getpath(obj)
+        cdir(path)
+        with open(path, "w", encoding="utf-8") as fpt:
+            dump(obj, fpt, indent=4)
+        return path
+
+
+def __dir__():
+    return (
+        'Cache',
+        'Error',
+        'cdir',
+        'getpath',
+        'ident',
+        'read',
+        'write'
+    )
---
+++ packages/nixt/nixt-322.tar.gz/nixt-322/nixt/modules/tbl.py
@@ -0,0 +1,47 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dne": "tdo",
+    "dpl": "rss",
+    "err": "err",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "mod": "mod",
+    "mre": "irc",
+    "nme": "rss",
+    "pwd": "irc",
+    "rem": "rss",
+    "res": "rss",
+    "rss": "rss",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "upt": "upt",
+    "ver": "ver"
+}
+
+
+MD5 = {
+    "err": "2aca6bd937ceb5aa2b4233f1700804b1",
+    "flt": "9fbbbeb19efe8f50f01831418fdc94a7",
+    "fnd": "00aadbabe428d635365e13c3b417e1a8",
+    "irc": "ad932ee797de66e9df56c06552abad95",
+    "log": "84000a16134000642aa926da942d6f67",
+    "mod": "82d9b50b3a2a446fbb857de7192b8f28",
+    "rss": "ea5a5c4c19b38fe1cf8ac3eaa0466e73",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "339e409ecfee7b8b943a26e27b617042",
+    "thr": "b87c29de413fa248279282dd064b4609",
+    "tmr": "99cd051efe338764b927bd4236a5891e",
+    "upt": "6f6fdb1397b57db6e68498e03b793d7a",
+    "ver": "ba6cfe6e26cbaff110fb7afa356c5a7f",
+}
