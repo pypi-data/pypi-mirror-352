0789ecbbacbbaaf599ea5da4c6847cb800861000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:49 +0000

    Add package fdtdx/0.4.0/fdtdx-0.4.0-py3-none-any.whl

---
+++ packages/fdtdx/fdtdx-0.4.0-py3-none-any.whl/fdtdx/objects/boundaries/boundary.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+
+import jax
+
+from fdtdx.core.jax.pytrees import ExtendedTreeClass, extended_autoinit
+from fdtdx.objects.object import SimulationObject
+from fdtdx.typing import GridShape3D
+
+
+@extended_autoinit
+class BaseBoundaryState(ExtendedTreeClass):
+    pass
+
+
+@extended_autoinit
+class BaseBoundary(SimulationObject, ABC):
+    @abstractmethod
+    def init_state(
+        self,
+    ) -> BaseBoundaryState:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def reset_state(self, state: BaseBoundaryState) -> BaseBoundaryState:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def update_E_boundary_state(
+        self,
+        boundary_state: BaseBoundaryState,
+        H: jax.Array,
+    ) -> BaseBoundaryState:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def update_H_boundary_state(
+        self,
+        boundary_state: BaseBoundaryState,
+        E: jax.Array,
+    ) -> BaseBoundaryState:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def update_E(
+        self,
+        E: jax.Array,
+        boundary_state: BaseBoundaryState,
+        inverse_permittivity: jax.Array,
+    ) -> jax.Array:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def update_H(
+        self,
+        H: jax.Array,
+        boundary_state: BaseBoundaryState,
+        inverse_permeability: jax.Array | float,
+    ) -> jax.Array:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def boundary_interface_grid_shape(self) -> GridShape3D:
+        raise NotImplementedError()
