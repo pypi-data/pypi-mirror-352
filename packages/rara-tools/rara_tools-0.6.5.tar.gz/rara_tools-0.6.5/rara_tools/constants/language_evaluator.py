55f15415ecc8314286f899f35a7dad077c3557aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:48 +0000

    Add package rara-tools/0.6.5/rara_tools-0.6.5.tar.gz

---
+++ packages/rara-tools/rara_tools-0.6.5.tar.gz/rara_tools-0.6.5/rara_tools/constants/language_evaluator.py
@@ -0,0 +1,9 @@
+COMPONENT_KEY = "language_evaluator"
+
+
+class Tasks:
+    EVALUATE = "text_evaluator"
+
+
+class Queue:
+    EVALUATE = "text_evaluator"
---
+++ packages/rara-tools/rara_tools-0.6.5.tar.gz/rara_tools-0.6.5/rara_tools/constants/meta_extractor.py
@@ -0,0 +1,10 @@
+COMPONENT_KEY = "meta_extractor"
+
+
+class Tasks:
+    SINGLE = "extract_meta_from_text"
+    PIPELINE = "run_meta_extractor_as_part_of_core_pipeline"
+
+
+class Queue:
+    MAIN = "meta_extractor"
