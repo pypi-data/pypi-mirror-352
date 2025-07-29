12f023e8be6e52d7f8ca6155056f81c57840af2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:08 +0000

    Add package pdns-zone-search/1.0.25/pdns_zone_search-1.0.25-py3-none-any.whl

---
+++ packages/pdns-zone-search/pdns_zone_search-1.0.25-py3-none-any.whl/pdns_zone_search/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2025 Captain Wulfstar <9593988-wulf-star@users.noreply.gitlab.com>
+# This code is licensed under the MIT License - see the LICENSE file in the root directory.
+
+"""PowerDNS zone search tool."""
+__version__ = "1.0.25"
