25e73f4f15e74cc5f2501bbfc3260b2770c0fdb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:47 +0000

    Add package foundationallm/0.9.7rc117/foundationallm-0.9.7rc117.tar.gz

---
+++ packages/foundationallm/foundationallm-0.9.7rc117.tar.gz/foundationallm-0.9.7rc117/foundationallm/models/constants/resource_object_id_property_values.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class ResourceObjectIdPropertyValues(str, Enum):
+    """Allowed values for resource object id properties dictionary entries."""
+    MAIN_MODEL = 'main_model'
+    MAIN_PROMPT = 'main_prompt'
+    MAIN_INDEXING_PROFILE = 'main_indexing_profile'
+    INDEXING_PROFILE = 'indexing_profile'
+    EMBEDDING_PROFILE = 'embedding_profile'
+    AI_PROJECT = 'ai_project'
