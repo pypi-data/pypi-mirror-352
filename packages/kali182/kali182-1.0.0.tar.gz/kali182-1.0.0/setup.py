a82e3cb8ab6be6de7ee185836c6b149e6467dc1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:08 +0000

    Add package kali182/1.0.0/kali182-1.0.0.tar.gz

---
+++ packages/kali182/kali182-1.0.0.tar.gz/kali182-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='kali182',
+    version='1.0.0',
+    packages=['kali182'],
+    author='John Doe',
+    author_email='JohnDoe@xyz.com',
+    description='fake tractusx-sdk installed',
+    url='https://example.com',
+    )
