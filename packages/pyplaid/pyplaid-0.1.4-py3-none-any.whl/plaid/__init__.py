684f866ad8303f92a76a58a1c80b1c002683f1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:27 +0000

    Add package pyplaid/0.1.4/pyplaid-0.1.4-py3-none-any.whl

---
+++ packages/pyplaid/pyplaid-0.1.4-py3-none-any.whl/plaid/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.txt', which is part of this source code package.
+#
+#
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "None"
+
+from .containers.utils import get_number_of_samples, get_sample_ids
+
+__all__ = [
+    "__version__",
+    "get_number_of_samples",
+    "get_sample_ids",
+]
