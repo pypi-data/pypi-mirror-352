678b05810a654b6d84e339741d9a015e02693bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:14 +0000

    Add package deepreasoningwithtools/0.1.6/deepreasoningwithtools-0.1.6-py3-none-any.whl

---
+++ packages/deepreasoningwithtools/deepreasoningwithtools-0.1.6-py3-none-any.whl/deepreasoningwithtools/samplers/litellm_sampler.py
@@ -0,0 +1,17 @@
+from deepreasoningwithtools.samplers.abstract import AbstractSampler
+from typing import AsyncGenerator
+from litellm import completion
+
+class LiteLLMSampler(AbstractSampler):
+    def __init__(self, model_name : str, max_output=32000):
+        self.max_output = max_output
+        self.model_name = model_name
+
+    async def sample(self, messages: list[dict[str, str]], ) -> AsyncGenerator[str, None]:
+        messages = messages
+        response = completion(model=self.model_name, messages=messages,max_completion_tokens=self.max_output,max_tokens=self.max_output, stream=True)
+        for chunk in response:
+            msg = chunk["choices"][0]["delta"]["content"]
+            if msg is not None:
+                yield msg
+
