ba9800618a6402ad9bfe130db76dd72236c3020a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:06 +0000

    Add package canadian-address-parser/0.0.6/canadian_address_parser-0.0.6.tar.gz

---
+++ packages/canadian-address-parser/canadian_address_parser-0.0.6.tar.gz/canadian_address_parser-0.0.6/src/canadian_address_parser/address_parser.py
@@ -0,0 +1,39 @@
+import json
+from dataclasses import asdict
+
+from .models.canadian_address_model import AddressParserModel
+from .data.clean_address import CleanAddress
+from .data.raw_address import RawAddress
+from .errors.unable_parse_model_output import UnableToParseModelOutputError
+
+
+class AddressParser:
+    def __init__(self, hf_token: str) -> None:
+        self.__address_model = AddressParserModel('PixelPulse64/canadian_address_parser', hf_token)
+        self.__model_loaded = False
+
+    @staticmethod
+    def __create_raw_address_text(raw_address: RawAddress) -> str:
+        return f'{asdict(raw_address)}'
+
+    def parse_address(self, raw_address: RawAddress) -> CleanAddress:
+        if not self.__model_loaded:
+            self.__address_model.load()
+            self.__model_loaded = True
+
+        raw_address_text = self.__create_raw_address_text(raw_address)
+        model_output_text = self.__address_model.parse_address(raw_address_text)
+
+        try:
+            json_parsed = json.loads(model_output_text)
+        except json.decoder.JSONDecodeError:
+            raise UnableToParseModelOutputError()
+
+        clean_address = CleanAddress(
+            postal_code=json_parsed['POSTAL_CODE'],
+            city=json_parsed['CITY'],
+            province_code=json_parsed['PROVINCE_CODE'],
+            address_line=json_parsed['ADDRESS_LINE'],
+        )
+
+        return clean_address
