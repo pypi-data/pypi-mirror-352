92120dcad8eeed1c8b75e896cb331e1d5eb41442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:17 +0000

    Add package fabricatio/0.4.1.dev1/fabricatio-0.4.1.dev1-cp313-cp313-win_amd64.whl

---
+++ packages/fabricatio/fabricatio-0.4.1.dev1-cp313-cp313-win_amd64.whl/fabricatio/capabilities/__init__.py
@@ -0,0 +1,43 @@
+"""A module containing all the capabilities of the Fabricatio framework."""
+
+from importlib.util import find_spec
+
+__all__ = []
+
+if find_spec("fabricatio_capabilities"):
+    from fabricatio_capabilities.capabilities.extract import Extract
+    from fabricatio_capabilities.capabilities.propose import Propose
+    from fabricatio_capabilities.capabilities.rating import Rating
+    from fabricatio_capabilities.capabilities.task import ProposeTask
+
+    __all__ += ["Extract", "Propose", "ProposeTask", "Rating"]
+
+if find_spec("fabricatio_rag"):
+    from fabricatio_rag.capabilities.rag import RAG
+
+    __all__ += ["RAG"]
+    if find_spec("fabricatio_write"):
+        from fabricatio_typst.capabilities.citation_rag import CitationRAG
+
+    __all__ += [
+        "CitationRAG",
+    ]
+
+if find_spec("fabricatio_rule"):
+    from fabricatio_rule.capabilities.censor import Censor
+    from fabricatio_rule.capabilities.check import Check
+
+    __all__ += ["Censor", "Check"]
+
+if find_spec("fabricatio_improve"):
+    from fabricatio_improve.capabilities.correct import Correct
+    from fabricatio_improve.capabilities.review import Review
+
+    __all__ += ["Correct",
+                "Review",
+                ]
+
+if find_spec():
+    from fabricatio_judge.capabilities.advanced_judge import AdvancedJudge
+
+    __all__ += ["AdvancedJudge"]
---
+++ packages/fabricatio/fabricatio-0.4.1.dev1-cp313-cp313-win_amd64.whl/fabricatio/models/__init__.py
@@ -0,0 +1,31 @@
+"""A module for the usage of the fabricatio package."""
+
+from importlib.util import find_spec
+
+from fabricatio_core.models.usages import EmbeddingUsage, LLMUsage, ToolBoxUsage
+
+__all__ = [
+    "EmbeddingUsage",
+    "LLMUsage",
+    "ToolBoxUsage",
+]
+
+if find_spec("fabricatio_typst"):
+    from fabricatio_typst.models.article_essence import ArticleEssence
+    from fabricatio_typst.models.article_main import Article, ArticleOutline
+    from fabricatio_typst.models.article_proposal import ArticleProposal
+
+    __all__ += [
+        "Article",
+        "ArticleEssence",
+        "ArticleOutline",
+        "ArticleProposal",
+    ]
+
+    if find_spec("fabricatio_typst"):
+        from fabricatio_typst.models.aricle_rag import ArticleChunk
+
+        __all__ += [
+            "ArticleChunk"
+
+        ]
