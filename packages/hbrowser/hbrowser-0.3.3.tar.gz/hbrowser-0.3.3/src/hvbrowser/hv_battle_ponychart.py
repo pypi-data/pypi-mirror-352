dbe2113b874a1341bda3048c79318512c8d44aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:04 +0000

    Add package hbrowser/0.3.3/hbrowser-0.3.3.tar.gz

---
+++ packages/hbrowser/hbrowser-0.3.3.tar.gz/hbrowser-0.3.3/src/hvbrowser/hv_battle_ponychart.py
@@ -0,0 +1,35 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from hbrowser.beep import beep_os_independent
+
+from .hv import HVDriver
+
+
+class PonyChart:
+    def __init__(self, driver: HVDriver) -> None:
+        self.hvdriver: HVDriver = driver
+
+    @property
+    def driver(self) -> WebDriver:
+        return self.hvdriver.driver
+
+    def _check(self) -> bool:
+        return self.driver.find_elements(By.ID, "ponychart") != []
+
+    def check(self) -> bool:
+        isponychart: bool = self._check()
+        if not isponychart:
+            return isponychart
+
+        beep_os_independent()
+
+        waitlimit: float = 100
+        while waitlimit > 0 and self._check():
+            time.sleep(0.1)
+            waitlimit -= 0.1
+
+        time.sleep(1)
+
+        return isponychart
