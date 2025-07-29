28113aee37401b694ec8b1ba1dc4e4d0f5c95a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:42 +0000

    Add package jreadability/1.1.4rc1/jreadability-1.1.4rc1.tar.gz

---
+++ packages/jreadability/jreadability-1.1.4rc1.tar.gz/jreadability-1.1.4rc1/src/jreadability/__init__.py
@@ -0,0 +1,12 @@
+"""
+jreadability
+------------
+
+jReadability is a python package that can be used to score the readability of a given Japanese text.
+
+This package is an unofficial python implementation of the readability model developed by Jae-ho Lee and Yoichiro Hasebe in "Readability measurement of Japanese texts based on levelled corpora."
+"""
+
+from .jreadability import compute_readability
+
+__all__ = ["compute_readability"]
