cca9cd8e8484124f1316e371b975ba5c9de3e0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:13 +0000

    Add package django-auto-model-loader/1.0.1/django_auto_model_loader-1.0.1-py3-none-any.whl

---
+++ packages/django-auto-model-loader/django_auto_model_loader-1.0.1-py3-none-any.whl/django_auto_model_loader/aliases.py
@@ -0,0 +1,16 @@
+﻿_model_alias_registry = {}
+_model_reverse_registry = {}
+
+def register_model_alias(model, *aliases):
+    module_name = model.__module__.split('.')[0]
+    for alias in aliases:
+        _model_alias_registry[(module_name, alias)] = model
+        _model_reverse_registry.setdefault(model, set()).add((module_name, alias))
+
+def get_model_by_alias(module_name, alias):
+    return _model_alias_registry.get((module_name, alias))
+
+def get_aliases_for_model(model):
+    return _model_reverse_registry.get(model, set())
+
+
---
+++ packages/django-auto-model-loader/django_auto_model_loader-1.0.1-py3-none-any.whl/django_auto_model_loader/decorators.py
@@ -0,0 +1,11 @@
+﻿import inflection
+from .aliases import register_model_alias
+
+def model_alias(*aliases):
+    def decorator(model_cls):
+        all_aliases = set(aliases)
+        all_aliases.add(inflection.camelize(model_cls.__name__, False))
+        all_aliases.add(inflection.underscore(model_cls.__name__))
+        register_model_alias(model_cls, *all_aliases)
+        return model_cls
+    return decorator
