a30b98f5a68728c7fb53d57917148c608d2509b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:33 +0000

    Add package MetaRagTool/0.3.13/metaragtool-0.3.13.tar.gz

---
+++ packages/MetaRagTool/metaragtool-0.3.13.tar.gz/metaragtool-0.3.13/MetaRagTool/Encoders/SentenceTransformerEncoder.py
@@ -0,0 +1,52 @@
+﻿from sentence_transformers import SentenceTransformer
+from MetaRagTool.Encoders.Encoder import Encoder
+import MetaRagTool.Constants as Constants
+
+good_encoder_names = [
+    'sentence-transformers/LaBSE',
+    'codersan/FaLaBSE-v3',
+    'intfloat/multilingual-e5-base',
+    'sentence-transformers/use-cmlm-multilingual',
+    'HIT-TMG/KaLM-embedding-multilingual-mini-instruct-v1',
+    'myrkur/sentence-transformer-parsbert-fa',
+    'BAAI/bge-m3',
+
+
+
+
+    #  from https://huggingface.co/spaces/PartAI/pteb-leaderboard
+    'jinaai/jina-embeddings-v3',
+    'intfloat/multilingual-e5-large',
+    'intfloat/multilingual-e5-base',
+    'Alibaba-NLP/gte-multilingual-base',
+    'PartAI/Tooka-SBERT',
+    'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2',
+    'sentence-transformers/LaBSE',
+    'm3hrdadfi/bert-zwnj-wnli-mean-tokens',
+    'm3hrdadfi/roberta-zwnj-wnli-mean-tokens',
+    'myrkur/sentence-transformer-parsbert-fa',
+    'WhereIsAI/UAE-Large-V1',
+    'intfloat/e5-large-v2',
+    'intfloat/e5-base-v2',
+    'thenlper/gte-large',
+]
+
+
+class SentenceTransformerEncoder(Encoder):
+    MODEL_NAME_LABSE = "sentence-transformers/LaBSE"
+    def __init__(self, model_name: str, verbose=False):
+        super().__init__(model_name, verbose)
+        if Constants.trust_remote_code:
+            self.model = SentenceTransformer(model_name,trust_remote_code=True)
+        else:
+            self.model = SentenceTransformer(model_name)
+        print("Model loaded successfully")
+
+    def encode(self, sentences, isQuery=True):
+        embeddings = self.model.encode(sentences,
+                                       # normalize_embeddings=True,
+                                       # batch_size=256,
+                                       show_progress_bar=not isQuery,
+
+                                       convert_to_tensor=False)
+        return embeddings
---
+++ packages/MetaRagTool/metaragtool-0.3.13.tar.gz/metaragtool-0.3.13/setup.py
@@ -0,0 +1,48 @@
+﻿from setuptools import setup, find_packages
+setup(
+    name='MetaRagTool',
+    packages=find_packages(),
+    version='0.3.13',
+    license='MIT',
+    description='A comprehensive RAG (Retrieval-Augmented Generation) toolkit for AI applications',
+    author='Ali Mobarekati',
+    author_email='ali7919mb@yahoo.com',
+    url='https://github.com/ali7919/MetaRAG',
+    download_url='https://github.com/ali7919/MetaRAG/archive/refs/tags/v0.3.13.tar.gz',
+    keywords=['AI', 'RAG', 'LLM', 'NLP', 'Machine Learning'],
+    install_requires=[
+        'faiss-cpu',
+        'hazm',
+        'PyPDF2',
+        'datasets>=3',
+        'huggingface_hub',
+        'gradio',
+        'weave',
+
+        # you can remove these and it will still work on google Colab
+        'transformers',
+        'sentence-transformers',
+        'langchain-text-splitters',
+        'matplotlib',
+        'wandb==0.19.2',
+        'google-genai',
+        'openai',
+        'tqdm',
+        'nltk',
+        'pandas',
+        # 'numpy'
+
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
