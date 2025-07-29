cc4deae5663a9f5ed55abb46dbe19d0bb099cb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:56 +0000

    Add package odoo14-addon-sale-order-lot-selection/14.0.1.2.1/odoo14_addon_sale_order_lot_selection-14.0.1.2.1-py3-none-any.whl

---
+++ packages/odoo14-addon-sale-order-lot-selection/odoo14_addon_sale_order_lot_selection-14.0.1.2.1-py3-none-any.whl/odoo/addons/sale_order_lot_selection/models/sale_order_line.py
@@ -0,0 +1,19 @@
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    lot_id = fields.Many2one("stock.production.lot", "Lot", copy=False, index=True)
+
+    def _prepare_procurement_values(self, group_id=False):
+        vals = super()._prepare_procurement_values(group_id=group_id)
+        if self.lot_id:
+            vals["restrict_lot_id"] = self.lot_id.id
+        return vals
+
+    @api.onchange("product_id")
+    def product_id_change(self):
+        res = super().product_id_change()
+        self.lot_id = False
+        return res
