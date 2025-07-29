17f9e261ee93e0d13378229f4b8d22d565fec441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:22 +0000

    Add package UnityPy/1.22.5/UnityPy-1.22.5-cp37-cp37m-win32.whl

---
+++ packages/UnityPy/UnityPy-1.22.5-cp37-cp37m-win32.whl/UnityPy/classes/__init__.py
@@ -0,0 +1,5 @@
+from .generated import *
+from .legacy_patch import *
+from .Object import Object as Object
+from .PPtr import PPtr as PPtr
+from .UnknownObject import UnknownObject as UnknownObject
