5e536d41e1285a4dad17ebbe49fcfd84430ecdb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:41 +0000

    Add package esmerald/3.8.1/esmerald-3.8.1-py3-none-any.whl

---
+++ packages/esmerald/esmerald-3.8.1-py3-none-any.whl/esmerald/conf/__init__.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import os
+from typing import TYPE_CHECKING, Any, cast
+
+from monkay import Monkay
+
+if TYPE_CHECKING:
+    from esmerald.conf.global_settings import EsmeraldAPISettings
+
+ENVIRONMENT_VARIABLE = "ESMERALD_SETTINGS_MODULE"
+
+monkay: Monkay[None, EsmeraldAPISettings] = Monkay(
+    globals(),
+    settings_path=lambda: os.environ.get(
+        ENVIRONMENT_VARIABLE, "esmerald.conf.global_settings.EsmeraldAPISettings"
+    ),
+)
+
+
+class SettingsForward:
+    def __getattribute__(self, name: str) -> Any:
+        return getattr(monkay.settings, name)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        return setattr(monkay.settings, name, value)
+
+
+settings: EsmeraldAPISettings = cast("EsmeraldAPISettings", SettingsForward())
+
+
+def reload_settings() -> None:
+    """
+    Reloads the global settings.
+    """
+    monkay.settings = os.environ.get(
+        ENVIRONMENT_VARIABLE, "esmerald.conf.global_settings.EsmeraldAPISettings"
+    )
