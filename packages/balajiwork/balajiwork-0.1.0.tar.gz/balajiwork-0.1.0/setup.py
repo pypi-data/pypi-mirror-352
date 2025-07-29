0c29760c43713df45ae69ba4964a8120a591653a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:25 +0000

    Add package balajiwork/0.1.0/balajiwork-0.1.0.tar.gz

---
+++ packages/balajiwork/balajiwork-0.1.0.tar.gz/balajiwork-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='balajiwork',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Balaji',
+    author_email='',
+    description='Minimal Python library with info() function.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://balaji.work',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+)
