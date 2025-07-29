79c21221d47094c43ac1c751edf2f2b8cba02ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:47 +0000

    Add package ldap-server-lib/0.8.4/ldap_server_lib-0.8.4.tar.gz

---
+++ packages/ldap-server-lib/ldap_server_lib-0.8.4.tar.gz/ldap_server_lib-0.8.4/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='ldap-server-lib',
+	version='0.8.4',
+	author='idk_wdym_i_needa_un',
+	author_email='',
+	description='.',
+	packages=['ldap-server-lib'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.11',
+)
