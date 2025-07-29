32a35e2bfaf441a1b818321a832eae1e2a424a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:17 +0000

    Add package philips-hue-midi/2.0.0/philips_hue_midi-2.0.0-py3-none-any.whl

---
+++ packages/philips-hue-midi/philips_hue_midi-2.0.0-py3-none-any.whl/philips_hue_midi/controllers/note_controller.py
@@ -0,0 +1,25 @@
+"""
+note_controller.py
+"""
+
+from typing import List, Union
+
+from ..core.base_controller import BaseController
+from ..event import Event
+
+
+class NoteController(BaseController):
+    """
+    Note controller maps
+    """
+
+    @classmethod
+    def __call__(
+        cls, events: List[Union[None, Event]], **kwargs
+    ) -> List[Union[None, Event]]:
+        res = [None] * 12
+        for event in sorted(filter(None, events), key=lambda e: e.note):
+            # returns 0 - 11 (representing the pitch class of note)
+            pitch_class = (event.note - 12) % 12
+            res[pitch_class] = event
+        return res
