2084b69a1134c58f114a083632b4a7a3d39281ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:35 +0000

    Add package cproxchecker/0.1.0/cproxchecker-0.1.0.tar.gz

---
+++ packages/cproxchecker/cproxchecker-0.1.0.tar.gz/cproxchecker-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='cproxchecker',
+    version='0.1.0',
+    description='cproxchecker',
+    url='https://github.com/abrincookie/abrincookie',
+    author='Abrincookie',
+    author_email='aaa@gmail.com',
+    license='BSD 2-clause',
+    packages=['cproxchecker'],
+    install_requires=["requests"
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
