ab9870d45ad186339f9e4b65f1901dc2f9ccc28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:45 +0000

    Add package x-mlps-pytorch/0.0.9/x_mlps_pytorch-0.0.9.tar.gz

---
+++ packages/x-mlps-pytorch/x_mlps_pytorch-0.0.9.tar.gz/x_mlps_pytorch-0.0.9/x_mlps_pytorch/activations.py
@@ -0,0 +1,67 @@
+from functools import partial
+
+import torch
+from torch.nn import Module
+
+# relu squared with optional signing
+
+class ReluSquared(Module):
+    def __init__(self, signed = False):
+        super().__init__()
+        self.signed = signed
+
+    def forward(self, x):
+        out = x.relu().square()
+
+        if not self.signed:
+            return out
+
+        return out * x.sign()
+
+# sugar-(bsilu | nelu)
+
+class BSiLU(Module):
+    # eq (7) in paper
+
+    def __init__(self, alpha = 1.67):
+        super().__init__()
+        self.alpha = alpha
+
+    def forward(self, x):
+        α = self.alpha
+        return (x + α) * x.sigmoid() - α / 2
+
+class NeLU(Module):
+    def __init__(self, alpha = 0.05):
+        super().__init__()
+        self.alpha = alpha
+
+    def forward(self, x):
+        α = self.alpha
+        return -α / (1. + x.square())
+
+class Sugar(Module):
+    def __init__(
+        self,
+        forward_fn: Module,
+        backward_fn: Module
+    ):
+        super().__init__()
+        self.forward_fn = forward_fn
+        self.backward_fn = backward_fn
+
+    def forward(self, x):
+        forward_out = self.forward_fn(x)
+
+        if not self.training:
+            return forward_out
+
+        backward_out = self.backward_fn(x)
+
+        # only neg region for backward function gradients
+
+        soft = torch.where(x > 0, forward_out, backward_out)
+        
+        # straight-through during training
+
+        return soft + (forward_out - soft).detach()
