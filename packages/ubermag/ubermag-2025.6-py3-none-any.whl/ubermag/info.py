ff48219fbc87fdc5c75a713ef0dcb540918ef298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:18 +0000

    Add package ubermag/2025.6/ubermag-2025.6-py3-none-any.whl

---
+++ packages/ubermag/ubermag-2025.6-py3-none-any.whl/ubermag/info.py
@@ -0,0 +1,36 @@
+import importlib
+import sys
+
+
+def debug_info():
+    """Collect inforamtion about ubermag subpackages and available calculators."""
+    info_str = f"Platform: {sys.platform}\nPython: {sys.version}\n\n"
+
+    for pkg in [
+        "ubermagutil",
+        "discretisedfield",
+        "ubermagtable",
+        "micromagneticmodel",
+        "micromagneticdata",
+        "micromagnetictests",
+        "mumax3c",
+        "oommfc",
+        "mag2exp",
+        "ubermag",
+    ]:
+        module = importlib.import_module(pkg)
+        version = module.__version__
+        info_str += f"{pkg}: {version}\n"
+
+    info_str += "\n"
+
+    for pkg, calculator in [("oommfc", "OOMMF"), ("mumax3c", "Mumax3")]:
+        try:
+            module = importlib.import_module(pkg)
+            runner = module.runner.runner
+        except OSError as e:
+            info_str += f"{calculator}: {e}\n"
+        else:
+            info_str += f"{calculator}: {runner}, {runner.version}\n"
+
+    return info_str
