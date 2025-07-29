ff52ea739258255a518b612ff4ebb8fecee2c6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:10 +0000

    Add package exa-py/1.13.2/exa_py-1.13.2-py3-none-any.whl

---
+++ packages/exa-py/exa_py-1.13.2-py3-none-any.whl/exa_py/research/__init__.py
@@ -0,0 +1,9 @@
+from .client import ResearchClient, AsyncResearchClient
+from .models import ResearchTask
+
+__all__ = [
+    "ResearchClient",
+    "AsyncResearchClient",
+    "ResearchTaskId",
+    "ResearchTask",
+]
