31043c63c096d57170183b8962f51ee5097993a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:46 +0000

    Add package universal-mcp-braze/0.1.4/universal_mcp_braze-0.1.4.tar.gz

---
+++ packages/universal-mcp-braze/universal_mcp_braze-0.1.4.tar.gz/universal_mcp_braze-0.1.4/src/universal_mcp_braze/server.py
@@ -0,0 +1,19 @@
+
+from universal_mcp.servers import SingleMCPServer
+from universal_mcp.integrations import ApiKeyIntegration
+from universal_mcp.stores import EnvironmentStore
+
+from universal_mcp_braze.app import BrazeApp
+
+env_store = EnvironmentStore()
+integration_instance = ApiKeyIntegration(name="BRAZE_API_KEY", store=env_store)
+app_instance = BrazeApp(integration=integration_instance)
+
+mcp = SingleMCPServer(
+    app_instance=app_instance,
+)
+
+if __name__ == "__main__":
+    mcp.run()
+
+
