6f8a1b0fb8414aaf833941617473e22aa12de238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:17 +0000

    Add package types-peewee/3.18.1.20250601/types_peewee-3.18.1.20250601.tar.gz

---
+++ packages/types-peewee/types_peewee-3.18.1.20250601.tar.gz/types_peewee-3.18.1.20250601/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'peewee-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed'], 'playhouse-stubs': ['__init__.pyi', 'flask_utils.pyi', 'METADATA.toml', 'py.typed']})
