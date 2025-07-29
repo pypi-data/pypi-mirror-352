46aaa4d8aadd60c9e06f3f9b7fb37cdb313b7939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:30 +0000

    Add package swarms/7.8.0/swarms-7.8.0-py3-none-any.whl

---
+++ packages/swarms/swarms-7.8.0-py3-none-any.whl/swarms/structs/output_types.py
@@ -0,0 +1,6 @@
+from swarms.utils.history_output_formatter import (
+    HistoryOutputType as OutputType,
+)
+
+# Use the OutputType for type annotations
+output_type: OutputType  # OutputType now includes 'xml'
