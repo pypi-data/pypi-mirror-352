73d1f97f3e57217417b8d5d2c96417d1dab4eaaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:59 +0000

    Add package openbankproject-client/0.1.0/openbankproject_client-0.1.0.tar.gz

---
+++ packages/openbankproject-client/openbankproject_client-0.1.0.tar.gz/openbankproject_client-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="openbankproject-client",
+    version="0.1.0",
+    description="A comprehensive Python client for the OpenBankProject API",
+    author="Joseph Pollack",
+    author_email="joseph.pollack@emle.eu",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
