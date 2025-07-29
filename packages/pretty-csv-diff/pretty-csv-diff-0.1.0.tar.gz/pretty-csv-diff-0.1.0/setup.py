5e3c7ce17cf4a060b624d15201675dabe179d8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:49 +0000

    Add package pretty-csv-diff/0.1.0/pretty-csv-diff-0.1.0.tar.gz

---
+++ packages/pretty-csv-diff/pretty-csv-diff-0.1.0.tar.gz/pretty-csv-diff-0.1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Importing necessary functions from setuptools package.
+from setuptools import setup
+
+# Importing path manipulation utilities.
+from os.path import dirname, abspath, join
+
+# Setting the path to the README file which is used as the long description for the package.
+long_description_path = join(dirname(abspath(__file__)), "README.md")
+# Reading the content of README.md to use as the long description.
+long_description = open(long_description_path, encoding="utf-8").read()
+
+# Calling the setup function to handle the packaging of the module.
+setup(
+    name="pretty-csv-diff",  # Name of the package.
+    long_description=long_description,  # Long description read from README.md.
+    long_description_content_type="text/markdown",  # Specifying the format of the long description.
+    url="https://github.com/eternity8/pretty-csv-diff",  # URL of the project's homepage.
+    license="Apache License 2.0",  # License type for the package.
+    version="0.1.0",
+    packages=[
+        "pretty_csv_diff",  # List of all packages that are part of this distribution.
+    ],
+    entry_points={
+        "console_scripts": [
+            "pretty-csv-diff = pretty_csv_diff.__main__:main",  # Entry point for the console script.
+        ],
+    },
+    classifiers=[
+        # Metadata classifiers to categorize the project.
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
