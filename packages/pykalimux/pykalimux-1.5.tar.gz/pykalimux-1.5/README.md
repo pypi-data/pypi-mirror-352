fe01ccde7830e9a23b5874540f685341fc798991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:06 +0000

    Add package pykalimux/1.5/pykalimux-1.5.tar.gz

---
+++ packages/pykalimux/pykalimux-1.5.tar.gz/pykalimux-1.5/README.md
@@ -0,0 +1,34 @@
+# pykalimux
+This is a python script by which you can install Kali Nethunter (Kali Linux) in your termux application without rooted phone.
+
+## Download Counter
+<p align="left">
+<a href="https://github.com/rc-chuah/pykalimux/releases/latest"><img alt="GitHub all releases" src="https://img.shields.io/github/downloads/rc-chuah/pykalimux/total?label=GITHUB%20DOWNLOADS&style=for-the-badge&color=brightgreen"></a>
+<a href="https://pypi.org/project/pykalimux"><img alt="PyPI - Downloads" src="https://img.shields.io/pypi/dm/pykalimux?label=PYPI%20DOWNLOADS&style=for-the-badge&color=brightgreen"></a>
+</p>
+
+## Usage
+### How To Use pykalimux
+1. Install Dependencies In Termux `pkg update -y && pkg install git python -y`
+2. Install pykalimux `pip install git+https://github.com/rc-chuah/pykalimux`
+3. Run It On Termux Terminal `pykalimux`
+#### Update pykalimux
+- Update To The Latest pykalimux Run This Command Again `pip install git+https://github.com/rc-chuah/pykalimux`
+### How To Use pykalimux Using PyPI (Python Package Index)
+1. Install Dependencies In Termux `pkg update -y && pkg install python -y`
+2. Install pykalimux `pip install pykalimux`
+3. Run It On Termux Terminal `pykalimux`
+#### Update pykalimux Using PyPI (Python Package Index)
+- Update To The Latest pykalimux Using PyPI (Python Package Index) `pip install --upgrade pykalimux`
+
+## Star History
+<a href="https://www.star-history.com/#rc-chuah/pykalimux&Date">
+ <picture>
+   <source media="(prefers-color-scheme: dark)" srcset="https://api.star-history.com/svg?repos=rc-chuah/pykalimux&type=Date&theme=dark" />
+   <source media="(prefers-color-scheme: light)" srcset="https://api.star-history.com/svg?repos=rc-chuah/pykalimux&type=Date" />
+   <img alt="Star History Chart" src="https://api.star-history.com/svg?repos=rc-chuah/pykalimux&type=Date" />
+ </picture>
+</a>
+
+## Stargazers Over Time
+[![Stargazers Over Time](https://starchart.cc/rc-chuah/pykalimux.svg?variant=adaptive)](https://starchart.cc/rc-chuah/pykalimux)
---
+++ packages/pykalimux/pykalimux-1.5.tar.gz/pykalimux-1.5/pykalimux/pykalimux.py
@@ -0,0 +1,84 @@
+#!/bin/env python
+# -*- coding: utf-8 -*- #
+# Created And Coded by RC Chuah-(RaynerSec)
+
+# Import Modules
+import os
+import sys
+import time
+import datetime
+
+# Install Kali Nethunter In Termux Full Version Function
+def full():
+    os.system("cd ${HOME} && curl -fsSL https://bit.ly/install-nethunter-full-termux | bash && rm -rf kali-nethunter-daily-dev-rootfs-full-armhf.tar.xz && rm -rf kali-nethunter-daily-dev-rootfs-full-arm64.tar.xz && rm -rf kali-nethunter-daily-dev-rootfs-full-armhf.tar.xz.sha512sum && rm -rf kali-nethunter-daily-dev-rootfs-full-arm64.tar.xz.sha512sum")
+
+# Install Kali Nethunter In Termux Minimal Version Function
+def minimal():
+    os.system("cd ${HOME} && curl -fsSL https://bit.ly/install-nethunter-minimal-termux | bash && rm -rf kali-nethunter-daily-dev-rootfs-minimal-armhf.tar.xz && rm -rf kali-nethunter-daily-dev-rootfs-minimal-arm64.tar.xz && rm -rf kali-nethunter-daily-dev-rootfs-minimal-armhf.tar.xz.sha512sum && rm -rf kali-nethunter-daily-dev-rootfs-minimal-arm64.tar.xz.sha512sum")
+
+# Install Kali Nethunter In Termux Nano Version Function
+def nano():
+    os.system("cd ${HOME} && curl -fsSL https://bit.ly/install-nethunter-nano-termux | bash && rm -rf kali-nethunter-daily-dev-rootfs-nano-armhf.tar.xz && rm -rf kali-nethunter-daily-dev-rootfs-nano-arm64.tar.xz && rm -rf kali-nethunter-daily-dev-rootfs-nano-armhf.tar.xz.sha512sum && rm -rf kali-nethunter-daily-dev-rootfs-nano-arm64.tar.xz.sha512sum")
+
+# Uninstall Kali Nethunter In Termux Function
+def uninstall():
+    os.system("rm -rf ${HOME}/kali-arm64 && rm -rf ${HOME}/kali-armhf && rm -rf ${PREFIX}/bin/nh && rm -rf ${PREFIX}/bin/nethunter && sleep 1 && echo [+] Successfully Uninstalled ...")
+
+# Press Enter To Continue Function
+def press_enter():
+    print("")
+    input("Press Enter To Continue...")
+    os.system("clear")
+
+# Incorrect Selection Function
+def incorrect_selection():
+    print("Incorrect Selection! Try Again.")
+
+# Main Function
+def main():
+  while True:
+    # Usage Menu
+    os.system("clear")
+    print("Kali-Nethunter-In-Termux Installer")
+    print("")
+    print("Script By RC Chuah-(RaynerSec)")
+    print("")
+    now = datetime.datetime.now()
+    print("Current Date And Time:")
+    print(now.strftime("%d-%m-%y %H:%M:%S"))
+    print("")
+    print("[1]-Install Kali Nethunter In Termux Full Version")
+    print("[2]-Install Kali Nethunter In Termux Minimal Version")
+    print("[3]-Install Kali Nethunter In Termux Nano Version")
+    print("[4]-Uninstall Kali Nethunter In Termux")
+    print("[5]-Exit Menu")
+    print("")
+    choice = input("Enter Choice: ")
+    # Menu Choices
+    if choice == "1":
+      os.system("clear")
+      full()
+      press_enter()
+    elif choice == "2":
+      os.system("clear")
+      minimal()
+      press_enter()
+    elif choice == "3":
+      os.system("clear")
+      nano()
+      press_enter()
+    elif choice == "4":
+      os.system("clear")
+      uninstall()
+      press_enter()
+    elif choice == "5":
+      os.system("clear")
+      sys.exit()
+    else:
+      os.system("clear")
+      incorrect_selection()
+      press_enter()
+
+# Driver Code
+if __name__ == "__main__":
+    main()
