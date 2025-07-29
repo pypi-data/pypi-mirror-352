1b5d04e8d0dc08c95b4a95097737ca0191443275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:15 +0000

    Add package aws-annoying/0.7.0/aws_annoying-0.7.0-py3-none-any.whl

---
+++ packages/aws-annoying/aws_annoying-0.7.0-py3-none-any.whl/aws_annoying/cli/session_manager/install.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import logging
+
+import typer
+
+from aws_annoying.utils.downloader import TQDMDownloader
+
+from ._app import session_manager_app
+from ._common import SessionManager
+
+logger = logging.getLogger(__name__)
+
+
+# https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html
+@session_manager_app.command()
+def install(
+    yes: bool = typer.Option(  # noqa: FBT001
+        False,  # noqa: FBT003
+        help="Do not ask confirmation for installation.",
+    ),
+) -> None:
+    """Install AWS Session Manager plugin."""
+    session_manager = SessionManager()
+
+    # Check session-manager-plugin already installed
+    is_installed, binary_path, version = session_manager.verify_installation()
+    if is_installed:
+        logger.info("Session Manager plugin is already installed at %s (version: %s)", binary_path, version)
+        return
+
+    # Install session-manager-plugin
+    logger.warning("Installing AWS Session Manager plugin. You could be prompted for admin privileges request.")
+    session_manager.install(confirm=yes, downloader=TQDMDownloader())
+
+    # Verify installation
+    is_installed, binary_path, version = session_manager.verify_installation()
+    if not is_installed:
+        logger.error("Installation failed. Session Manager plugin not found.")
+        raise typer.Exit(1)
+
+    logger.info("Session Manager plugin successfully installed at %s (version: %s)", binary_path, version)
