c0f949fb3d47cd134035fe090c68498d2bed9ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:33 +0000

    Add package reolink-aio/0.13.5/reolink_aio-0.13.5-py3-none-any.whl

---
+++ packages/reolink-aio/reolink_aio-0.13.5-py3-none-any.whl/reolink_aio/const.py
@@ -0,0 +1,20 @@
+"""Constants for Reolink"""
+
+WAKING_COMMANDS = {
+    "GetWhiteLed",
+    "GetZoomFocus",
+    "GetAudioCfg",
+    "GetPtzGuard",
+    "GetAutoReply",
+    "GetPtzTraceSection",
+    "GetAiCfg",
+    "GetAiAlarm",
+    "GetPtzCurPos",
+    "GetAudioAlarm",
+    "GetDingDongList",
+    "GetDingDongCfg",
+    "DingDongOpt",
+    "GetPerformance",
+    "296",
+    "483",
+}
