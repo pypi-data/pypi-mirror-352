28c05c817c255fd9e656c74d98d4de0753d536db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:22 +0000

    Add package yaccounts-wd/0.1.0/yaccounts_wd-0.1.0.tar.gz

---
+++ packages/yaccounts-wd/yaccounts_wd-0.1.0.tar.gz/yaccounts_wd-0.1.0/src/yaccounts_wd/__init__.py
@@ -0,0 +1,2 @@
+from .account import Account
+from .payroll import PayrollData
---
+++ packages/yaccounts-wd/yaccounts_wd-0.1.0.tar.gz/yaccounts_wd-0.1.0/src/yaccounts_wd/yutils.py
@@ -0,0 +1,64 @@
+from decimal import Decimal
+import pathlib
+import appdirs
+
+
+class TermColors:
+    NONE = ""
+    PURPLE = "\033[95m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    END = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+def error(
+    *msg,
+):
+    print_color(
+        TermColors.RED, "ERROR:", " ".join(str(item) for item in msg), TermColors.END
+    )
+    assert False, "Exiting due to error"
+
+
+def warn(*msg):
+    print_color(
+        TermColors.YELLOW,
+        "WARNING:",
+        " ".join(str(item) for item in msg),
+        TermColors.END,
+    )
+
+
+def print_color(color, msg, end="\n"):
+    print(f"{color}{msg}{TermColors.END}", end=end, flush=True)
+
+
+def retry(func, retries=3, exception=Exception, *args, **kwargs):
+    for attempt in range(1, retries + 1):
+        try:
+            return func(*args, **kwargs)  # Pass arguments to the function
+        except exception as e:
+            warn(f"Warning: Attempt {attempt} failed")
+            if attempt == retries:
+                raise  # Re-raise the last exception after all retries
+
+
+def workday_str_amount_to_decimal(val):
+    """Clean financial amounts."""
+    if isinstance(val, str):
+        val = val.strip()
+        if val.startswith("(") and val.endswith(")"):
+            val = "-" + val[1:-1]  # Convert (292.53) -> -292.53
+        val = val.replace(",", "")  # Remove thousands separator
+    return Decimal(val)
+
+
+def get_app_data_dir(app_name="yaccounts-wd"):
+    data_dir = appdirs.user_data_dir(appname=app_name)
+    path = pathlib.Path(data_dir)
+    path.mkdir(parents=True, exist_ok=True)
+    return path
