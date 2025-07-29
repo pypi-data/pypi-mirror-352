d3a87a8c275f2cd48d0a0e2bca8ac6f7c12a165a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:19 +0000

    Add package LAML/1.0.2/LAML-1.0.2-py3-none-any.whl

---
+++ packages/LAML/LAML-1.0.2-py3-none-any.whl/LAML-1.0.2.data/scripts/laml_tests.py
@@ -0,0 +1,26 @@
+#!python
+from laml_unit_tests.unit_tests_MLSolver import *
+from laml_unit_tests.unit_tests_mosek import *
+from laml_unit_tests.unit_tests_TopoSearch import *
+from laml_unit_tests.unit_tests_EMSolver import *
+from laml_unit_tests.unit_tests_fastEMSolver import *
+from laml_unit_tests.unit_tests_TopoSearch_wfastEMSolver import *
+
+#from laml_unit_tests.utils import *
+
+##from unit_tests.unit_tests_Simulator import *
+##from unit_tests.unit_tests_SpaLinSolver import *
+
+##from laml_unit_tests.unit_tests_TopoSearchParallel import *
+
+import sys
+import os
+
+
+if __name__ == '__main__':
+    #print(os.getcwd())
+    sys.path.append(os.path.dirname(__file__))
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'helpers'))
+    print("Running tests for LAML...")
+    unittest.main()
+
