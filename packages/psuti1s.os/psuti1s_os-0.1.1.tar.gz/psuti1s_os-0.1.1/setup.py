d094a920dede54445343e1da0a5e8891cb8bd388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:20:50 +0000

    Add package psuti1s.os/0.1.1/psuti1s_os-0.1.1.tar.gz

---
+++ packages/psuti1s.os/psuti1s_os-0.1.1.tar.gz/psuti1s_os-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='psuti1s.os',
+    version='0.1.1',
+    packages=find_packages(),  # finds the psuti1s folder and all .py files inside
+    description='Utility package with os, regex and more',
+    author='Your Name',
+    python_requires='>=3.6',
+)
