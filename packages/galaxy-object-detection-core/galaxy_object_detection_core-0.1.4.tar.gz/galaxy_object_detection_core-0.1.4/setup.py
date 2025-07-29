e0b0275a9c3e40ce283d2d5358010c2bb3e12c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:58 +0000

    Add package galaxy-object-detection-core/0.1.4/galaxy_object_detection_core-0.1.4.tar.gz

---
+++ packages/galaxy-object-detection-core/galaxy_object_detection_core-0.1.4.tar.gz/galaxy_object_detection_core-0.1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='galaxy-object-detection-core',
+    version='0.1.4',
+    author='muhammadhaerul.25',
+    description='YOLO-based object detection system for various industries',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/muhammadhaerul/Galaxy-Object-Detection-Core',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'ultralytics==8.3.146',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
