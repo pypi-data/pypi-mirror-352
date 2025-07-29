cf41d329b81dc09a1a66c508c7045305c8e824bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:30 +0000

    Add package symbolicai/0.12.0/symbolicai-0.12.0.tar.gz

---
+++ packages/symbolicai/symbolicai-0.12.0.tar.gz/symbolicai-0.12.0/symai/extended/interfaces/flux.py
@@ -0,0 +1,15 @@
+from ... import core
+from ...symbol import Expression, Symbol
+from ...backend.engines.drawing.engine_bfl import FluxResult
+
+
+class flux(Expression):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, prompt: str, operation: str = 'create', **kwargs) -> FluxResult:
+        prompt = self._to_symbol(prompt)
+        @core.draw(operation=operation, **kwargs)
+        def _func(_) -> FluxResult:
+            pass
+        return _func(prompt)
