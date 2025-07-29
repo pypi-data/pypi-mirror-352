087a1fe3760000750b58ab661e4b044477be5e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:10 +0000

    Add package huntflow-webhook-models/0.1.15/huntflow_webhook_models-0.1.15-py3-none-any.whl

---
+++ packages/huntflow-webhook-models/huntflow_webhook_models-0.1.15-py3-none-any.whl/huntflow_webhook_models/common_models/vacancy_request.py
@@ -0,0 +1,50 @@
+import datetime
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel, Field
+
+from huntflow_webhook_models.consts import VacancyRequestLogAction
+
+
+class VacancyRequestLegacy(BaseModel):
+    """
+    Legacy vacancy request structure.
+    It should be replaced with VacancyRequest in a next webhook version
+    """
+
+    id: int = Field(..., description="Vacancy request ID", examples=[1])
+    account_vacancy_request: int = Field(
+        ...,
+        description="Account vacancy request ID",
+        examples=[1],
+    )
+    created: datetime.datetime = Field(
+        ...,
+        description="Date time vacancy request created",
+        examples=[datetime.datetime(1970, 1, 1, 1, 1, 1)],
+    )
+    position: str = Field(..., description="Vacancy position", examples=["Developer"])
+    values: Dict[str, Any] = Field(
+        ...,
+        description="Vacancy request fields",
+        examples=[{"company": "test_company"}],
+    )
+
+
+class VacancyRequestLog(BaseModel):
+    id: int = Field(..., description="Vacancy request log ID", examples=[1])
+    action: VacancyRequestLogAction = Field(
+        ...,
+        description="Vacancy request log action",
+        examples=[VacancyRequestLogAction.CREATE],
+    )
+    created: datetime.datetime = Field(
+        ...,
+        description="Date time vacancy request log created",
+        examples=[datetime.datetime(1970, 1, 1, 1, 1, 1)],
+    )
+    reason: Optional[str] = Field(
+        None,
+        description="Vacancy close/hold reason",
+        examples=["All hired"],
+    )
