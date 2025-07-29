73fd9657ede39182f5fc6edbad1aa237a47a078f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:26 +0000

    Add package restcodegen/1.0.3/restcodegen-1.0.3-py3-none-any.whl

---
+++ packages/restcodegen/restcodegen-1.0.3-py3-none-any.whl/restcodegen/restclient/__init__.py
@@ -0,0 +1,9 @@
+from restcodegen.restclient.configuration import Configuration
+from restcodegen.restclient.client import Client, AsyncClient
+
+
+__all__ = [
+    "Configuration",
+    "Client",
+    "AsyncClient",
+]
