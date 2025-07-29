0e9e250611b14276819b69ce7dc9e56e3fb8d181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:24 +0000

    Add package UnityPy/1.22.5/unitypy-1.22.5-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/UnityPy/unitypy-1.22.5-cp310-cp310-macosx_11_0_arm64.whl/UnityPy/files/__init__.py
@@ -0,0 +1,5 @@
+from .BundleFile import BundleFile
+from .File import DirectoryInfo, File
+from .ObjectReader import ObjectReader
+from .SerializedFile import SerializedFile
+from .WebFile import WebFile
