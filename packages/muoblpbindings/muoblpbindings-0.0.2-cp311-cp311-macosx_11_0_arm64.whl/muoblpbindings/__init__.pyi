4f3f16248e72321d935cacfa08c75ecb0bf513f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:51 +0000

    Add package muoblpbindings/0.0.2/muoblpbindings-0.0.2-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/muoblpbindings/muoblpbindings-0.0.2-cp311-cp311-macosx_11_0_arm64.whl/muoblpbindings/__init__.pyi
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def add(i: int, j: int) -> int:
+    """
+    Add two numbers
+    """
+
+
+def subtract(i: int, j: int) -> int:
+    """
+    Subtract two numbers
+    """
+
+
+def equal_shares(voters: list[str],
+                 projects: list[str],
+                 cost: dict[str, float],
+                 approvers_utilities: dict[str, list[tuple[str, int]]],
+                 total_budget: float) -> list[str]:
+    """
+    Basic equal shares implementation
+    """
