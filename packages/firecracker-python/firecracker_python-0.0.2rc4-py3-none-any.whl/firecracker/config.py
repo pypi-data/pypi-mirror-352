c74fed56fa16daf99dd4ac34dcbbf3195f8c18d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:20:29 +0000

    Add package firecracker-python/0.0.2rc4/firecracker_python-0.0.2rc4-py3-none-any.whl

---
+++ packages/firecracker-python/firecracker_python-0.0.2rc4-py3-none-any.whl/firecracker/config.py
@@ -0,0 +1,26 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class MicroVMConfig:
+    """Configuration defaults for Firecracker microVMs."""
+    data_path: str = "/var/lib/firecracker"
+    binary_path: str = "/usr/local/bin/firecracker"
+    kernel_file: str = os.path.join(data_path, "vmlinux-5.10.225")
+    base_rootfs: str = os.path.join(data_path, "rootfs.img")
+    ip_addr: str = "172.16.0.2"
+    bridge: bool = False
+    bridge_name: str = "docker0"
+    mmds_enabled: bool = False
+    mmds_ip: str = "169.254.169.254"
+    user_data: str = None   # cloud-init must be installed in the rootfs
+    vcpu_count: int = 1
+    mem_size_mib: int = 512
+    hostname: str = "fc-vm"
+    verbose: bool = False
+    level: str = "INFO"
+    ssh_user: str = "root"
+    expose_ports: bool = False
+    host_port: int = None
+    dest_port: int = None
