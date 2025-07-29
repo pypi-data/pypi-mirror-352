afadfb14eb0792b024f4c372e8e379a4ad387e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:01 +0000

    Add package kasturing-testing-lib/1.0.97/kasturing_testing_lib-1.0.97.tar.gz

---
+++ packages/kasturing-testing-lib/kasturing_testing_lib-1.0.97.tar.gz/kasturing_testing_lib-1.0.97/algowalk/sorting/sort_context.py
@@ -0,0 +1,29 @@
+from algowalk.sorting.tracker.sort_algorithm_tracker import StepTracker
+
+
+class SortContext:
+    def __init__(self, strategy, visualizer=None, pseudocode_builder=None):
+        self.strategy = strategy
+        self.visualizer = visualizer
+        self.pseudocode_builder = pseudocode_builder
+
+    def execute_sort(self, data: list):
+        # Initialize Step Tracker
+        tracker = StepTracker(visualizer=self.visualizer, pseudocode_builder=self.pseudocode_builder)
+
+        # Perform Sort
+        sorted_data = self.strategy.sort(data, tracker)
+
+        # Build Pseudocode
+        self.strategy.pseudocode(tracker)
+
+        # Print Summary and Steps
+        tracker.print_steps()
+        tracker.print_summary()
+
+        # Print Pseudocode and Visualize
+        tracker.print_pseudocode()
+        tracker.visualize()
+
+        print(f"\n✅ Final Sorted Output: {sorted_data}")
+        return sorted_data
