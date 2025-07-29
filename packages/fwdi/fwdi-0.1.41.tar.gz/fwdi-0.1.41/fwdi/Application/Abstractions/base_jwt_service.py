44910c9e6a6b436144ca69aa37c5f5bfc4d586e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:31 +0000

    Add package fwdi/0.1.41/fwdi-0.1.41.tar.gz

---
+++ packages/fwdi/fwdi-0.1.41.tar.gz/fwdi-0.1.41/fwdi/Application/Abstractions/base_jwt_service.py
@@ -0,0 +1,41 @@
+from abc import ABCMeta, abstractmethod
+from typing import Annotated
+
+from fastapi import Depends, Security
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
+
+from ...Application.DTO.Auth.user_in_db import UserInDB
+from ...Application.Abstractions.base_user_repository import BaseUserRepositoryFWDI
+from ...Application.DTO.Auth.model_user import User
+from ...Utilites.jwt_tools import JwtToolsFWDI
+
+oauth2_scheme:OAuth2PasswordBearer = OAuth2PasswordBearer(tokenUrl="token")
+
+class BaseAuthServiceFWDI(metaclass=ABCMeta):
+
+    @staticmethod
+    @abstractmethod
+    def get_user(username: str, 
+                    db_context:BaseUserRepositoryFWDI, 
+                    jwt_tools:JwtToolsFWDI)->UserInDB|None:    
+        ...
+
+    @staticmethod
+    @abstractmethod
+    def authenticate_user(db_context:BaseUserRepositoryFWDI, 
+                          username: str, 
+                          password: str, 
+                          jwt_tools:JwtToolsFWDI)->UserInDB|None:
+        ...
+
+    @staticmethod
+    @abstractmethod
+    def get_current_user(security_scopes: SecurityScopes, 
+                         token: Annotated[str, Depends(oauth2_scheme)], 
+                         jwt_tools:JwtToolsFWDI=Depends())->UserInDB|None:
+        ...
+    
+    @staticmethod
+    @abstractmethod
+    def get_current_active_user(current_user:User = Security(get_current_user),)->UserInDB|None:
+        ...
