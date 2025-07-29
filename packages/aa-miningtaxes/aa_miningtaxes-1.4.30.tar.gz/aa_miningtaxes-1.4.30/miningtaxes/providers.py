d4f2d026bf65ccce14cbc3b0300e7b6d456e4f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:23 +0000

    Add package aa-miningtaxes/1.4.30/aa_miningtaxes-1.4.30.tar.gz

---
+++ packages/aa-miningtaxes/aa_miningtaxes-1.4.30.tar.gz/aa_miningtaxes-1.4.30/miningtaxes/providers.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from esi.clients import EsiClientProvider
+
+from . import __version__
+
+spec_file = Path(__file__).parent / "swagger.json"
+esi = EsiClientProvider(
+    app_info_text=f"aa-miningtaxes v{__version__}", spec_file=spec_file
+)
