97f203aefde7d0c2ada3e52ffe51ae881b13258f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:32 +0000

    Add package dangotestplayer/0.0.1/dangotestplayer-0.0.1-py3-none-any.whl

---
+++ packages/dangotestplayer/dangotestplayer-0.0.1-py3-none-any.whl/dangotestplayer/player.py
@@ -0,0 +1,24 @@
+"""
+Esta es el modulo que incluye la clase de reproductor de musica
+"""
+
+class Player:
+    """
+    Esta clase crea un reproducto
+    de musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la canción que recibio como parametro
+
+        Parameters:
+        song (str): este es un string con el path de la canción
+
+        Returns:
+        int: devuelve 1 si reproduce con exito, en caso de fracaso devuelve 0
+        """
+        print("reproduciendo canción")
+
+    def stop(self, stopping):
+        print("stopping")
