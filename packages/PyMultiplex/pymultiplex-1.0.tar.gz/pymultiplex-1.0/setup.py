4f6c2dfce8d31dd429dda949269835c21a172302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:05 +0000

    Add package PyMultiplex/1.0/pymultiplex-1.0.tar.gz

---
+++ packages/PyMultiplex/pymultiplex-1.0.tar.gz/pymultiplex-1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='PyMultiplex',
+    version='1.0',
+    author='Gaffner',
+    author_email='gefen102@gmail.com',
+    url="https://github.com/GefenAltshuler/PyMultiplex",
+    packages=['PyMultiplex'],
+    entry_points={
+        'console_scripts': [
+            'multiplex = PyMultiplex.__main__:main'
+        ]
+    },
+    description='Transfer data across multiple virtual channels over a single socket connection using multiplexing',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
