a62f53de1610d0befbf428b887784493a9444dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:57 +0000

    Add package treescriptify/0.2.1/treescriptify-0.2.1-py3-none-any.whl

---
+++ packages/treescriptify/treescriptify-0.2.1-py3-none-any.whl/treescriptify/__init__.py
@@ -0,0 +1,26 @@
+"""TreeScriptify Package
+"""
+from .input_data import InputData
+from .tree_runner import get_tree_json, is_tree_v2_available
+from .tree_reader import generate_from_json
+from .script_writer import generate_script
+from .windows_tree import win_tree
+
+
+def tsfy(data: InputData) -> str:
+    """Run Treescriptify on the given Inputs.
+    """
+    return '\n'.join(
+        generate_script(
+            generate_from_json(get_tree_json(data))
+            if is_tree_v2_available() else win_tree(data)
+        )
+    )
+
+
+def tsfy_windows(data: InputData) -> str:
+    """Run Treescriptify on the given Inputs, on a Windows OS.
+    """
+    return '\n'.join(
+        generate_script(win_tree(data))
+    )
