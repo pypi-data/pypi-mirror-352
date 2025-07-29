8d368986f664f4ee6bc719c267bcf41c2a9e0df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:30 +0000

    Add package photolink/1.0.2/photolink-1.0.2.tar.gz

---
+++ packages/photolink/photolink-1.0.2.tar.gz/photolink-1.0.2/photolink/utils.py
@@ -0,0 +1,17 @@
+mime_types = {
+    "jpg": "image/jpeg",
+    "jpeg": "image/jpeg",
+    "png": "image/png",
+    "webp": "image/webp"
+}
+
+errors = {
+    "InvalidClientID": "\"Invalid Client ID\"\n\nPlease provide a valid client ID. If you don't have one, create a new client ID by visiting photolink.uz.",
+    "INVALID_CONTENT_TYPE": "The file type is not supported. Please check the image format.",
+    "FILE_SIZE_ERROR": "The file size should not be larger than 1 MB. Please upload a smaller file.",
+    "FileNotFoundError": "File not found. Please check if the file exists.",
+    "NoCredentialsError": "The credential for the database does not exist. Please set the credentials correctly.",
+    "UNEXPECTED_ERROR": "An unexpected error occurred. Please contact your system admin.\nMail: gayratov@xudoberdi.uz",
+    "InvalidUrlException": "The provided URL is invalid. Please ensure the URL is correct and try again.",
+
+}
