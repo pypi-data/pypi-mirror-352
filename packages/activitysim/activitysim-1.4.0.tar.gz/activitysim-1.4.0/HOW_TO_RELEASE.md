38a6b3cae89a30dad5a17fe18cdfaeecfa909012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:18 +0000

    Add package activitysim/1.4.0/activitysim-1.4.0.tar.gz

---
+++ packages/activitysim/activitysim-1.4.0.tar.gz/activitysim-1.4.0/HOW_TO_RELEASE.md
@@ -0,0 +1,134 @@
+# How to issue an ActivitySim release
+
+00. Check that the main branch is passing tests, especially the "core tests" on
+    [GitHub Actions](https://github.com/ActivitySim/activitysim/actions/workflows/core_tests.yml). 
+    It is generally the policy that the main branch should always be passing tests, 
+    becuase PRs must pass tests before they can be merged.  However, it is 
+    possible that tests may fail after a PR is merged, so it is important to
+    double-check that the main branch is passing tests before issuing a release.
+
+00. Start from a completely clean conda environment
+    and git repository.  Assuming you have `conda` installed, you can do so
+    by starting where ActivitySim is not yet cloned (e.g. in an empty
+    directory) and running:
+    ```sh
+    conda create -p ./TEMP-ASIM-DEV python=3.10 git gh -c conda-forge --override-channels
+    conda activate ./TEMP-ASIM-DEV
+    gh auth login   # <--- (only needed if gh is not logged in)
+    gh repo clone ActivitySim/activitysim
+    cd activitysim
+    ```
+
+00. Update your Conda environment for testing.  We do not want to use an
+    existing environment on your machine, as it may be out-of-sync
+    and we want to make sure everything passes muster using the
+    dependencies as they are available today.  The following command
+    will update the active environment (we made this to be `TEMP-ASIM-DEV`
+    if you followed the directions above).
+    ```sh
+    conda env update --file=conda-environments/activitysim-dev.yml
+    ```
+    If you add to the ActivitySim dependencies, make sure to also update
+    the environments in `conda-environments`, which are used for testing
+    and development.  If they are not updated, these environments will end
+    up with dependencies loaded from *pip* instead of *conda-forge*.
+
+00. Run black to ensure that the codebase passes all style checks.
+    This check should only take a few seconds.  These checks are also done on
+    GitHub Actions and are platform independent, so they should not be necessary to
+    replicate locally, but are listed here for completeness.
+    ```sh
+    black --check --diff .
+    ```
+
+00. Run the regular test suite on Windows. Most GitHub Actions tests are done on
+    Linux (it's faster to start up and run a new clean VM for testing) but most
+    users are on Windows, and the test suite should also be run on Windows to
+    ensure that it works on that platform as well.  If you
+    are not preparing this release on Windows, you should be sure to run
+    at least through this step on a Windows machine before finalizing a
+    release.
+
+    A few of the tests require pre-created data that is not included in the
+    repository directly, but rather recreated on the fly before testing. The
+    regular test suite takes some time to run, between about half an hour and
+    two hours depending on the specs of your machine.
+    ```sh
+    python activitysim/examples/placeholder_multiple_zone/scripts/two_zone_example_data.py
+    python activitysim/examples/placeholder_multiple_zone/scripts/three_zone_example_data.py
+    pytest .
+    ```
+
+00. Test the full-scale regional examples. These examples are big, too
+    large to run on GitHub Actions, and will take a lot of time (many hours) to
+    download and run.
+    ```sh
+    mkdir tmp-asim
+    cd activitysim/examples
+    python create_run_all_examples.py > ../../tmp-asim/run_all_examples.bat
+    cd ../../tmp-asim
+    call run_all_examples.bat
+    ```
+    These tests will run through the gamut even if some of them crash, so
+    if you don't sit and watch them go (please don't do this) you'll need
+    to scan through the results to make sure there are no errors after the
+    fact.
+    ```sh
+    python ../activitysim/examples/scan_examples_for_errors.py .
+    ```
+
+00. Test the notebooks in `activitysim/examples/prototype_mtc/notebooks`.
+    There are also demo notebooks for estimation, but their functionality
+    is completely tested in the unit tests run previously.
+
+00. Tag the release commit with the new version number.  ActivitySim uses
+    dynamic versioning, so the version number is not stored in a file but
+    is instead read from the most recent git tag, so it is important to tag
+    the repository with the correct version.  The following command will 
+    generate a new tag with the version number "1.2.3" (for example):
+    ```sh
+    git -a v1.2.3 -m "Release v1.2.3"
+    ```
+
+00. Push the tagged commit to GitHub.
+    ```sh
+    git push --tags
+    ```
+
+00. Create a "release" on GitHub.  You can do this from the command line using
+    the `gh` command line tool:
+    ```sh
+    gh release create v1.2.3
+    ```
+    But it may be easier to do this through the 
+    [GitHub web interface](https://github.com/ActivitySim/activitysim/releases/new),
+    where you can select the tag you just created and add a title and description.
+    Both the interactive command line tool shown above and the web interface include
+    the ability to create release notes automatically from the commit messages of
+    all accepted PRs since the last release, but you may want to add additional
+    notes to the release to highlight important changes or new features.
+
+    The process of creating and tagging a release will automatically
+    trigger various GitHub Actions scripts to build, test, and publish the
+    new release to PyPI and conda forge, assuming there are no errors.
+
+00. If the dependencies of ActivitySim have changed, also be sure to update the
+    dependencies of the conda-forge [recipe](https://github.com/conda-forge/activitysim-feedstock/tree/main/recipe).
+    If the dependencies of ActivitySim have not changed, the conda-forge version
+    of ActivitySim should auto-update within a few hours of making the release on
+    Github.
+
+00. Build the ActivitySim Standalone Windows Installer.  This is done using 
+    GitHub Actions, but it is not done automatically when a release is created, 
+    instead it requires a manual workflow dispatch trigger.  You can do this by 
+    going to the [build_installer workflow page](https://github.com/ActivitySim/activitysim/actions/workflows/build_installer.yml)
+    and clicking on the "Run workflow" button.  You will need to provide the 
+    version number and choose to add the built installer to the release.
+
+00. Clean up your workspace, including removing the Conda environment used for
+    testing (which will prevent you from accidentally using an old
+    environment when you should have a fresh up-to-date one next time).
+    ```sh
+    conda deactivate
+    conda env remove -p ./TEMP-ASIM-DEV
+    ```
---
+++ packages/activitysim/activitysim-1.4.0.tar.gz/activitysim-1.4.0/activitysim/estimation/larch/location_choice.py
@@ -0,0 +1,467 @@
+from __future__ import annotations
+
+import os
+import pickle
+from datetime import datetime
+from pathlib import Path
+from typing import Collection
+
+import numpy as np
+import pandas as pd
+import yaml
+
+from .general import (
+    apply_coefficients,
+    construct_nesting_tree,
+    cv_to_ca,
+    explicit_value_parameters,
+    linear_utility_from_spec,
+    remove_apostrophes,
+    str_repr,
+)
+
+try:
+    import larch
+except ImportError:
+    larch = None
+else:
+    from larch import DataFrames, Model, P, X
+    from larch.util import Dict
+
+
+def size_coefficients_from_spec(size_spec):
+    size_coef = size_spec.stack().reset_index()
+    size_coef.index = size_coef.iloc[:, 0] + "_" + size_coef.iloc[:, 1]
+    size_coef = size_coef.loc[size_coef.iloc[:, 2] > 0]
+    size_coef["constrain"] = "F"
+    one_each = size_coef.groupby("segment").first().reset_index()
+    size_coef.loc[one_each.iloc[:, 0] + "_" + one_each.iloc[:, 1], "constrain"] = "T"
+    size_coef = size_coef.iloc[:, 2:]
+    size_coef.columns = ["value", "constrain"]
+    size_coef.index.name = "coefficient_name"
+    size_coef["value"] = np.log(size_coef["value"])
+    return size_coef
+
+
+def location_choice_model(
+    name="workplace_location",
+    edb_directory="output/estimation_data_bundle/{name}/",
+    coefficients_file="{name}_coefficients.csv",
+    spec_file="{name}_SPEC.csv",
+    size_spec_file="{name}_size_terms.csv",
+    alt_values_file="{name}_alternatives_combined.csv",
+    chooser_file="{name}_choosers_combined.csv",
+    settings_file="{name}_model_settings.yaml",
+    landuse_file="{name}_landuse.csv",
+    return_data=False,
+    alt_values_to_feather=False,
+    chunking_size=None,
+):
+    model_selector = name.replace("_location", "")
+    model_selector = model_selector.replace("_destination", "")
+    model_selector = model_selector.replace("_subtour", "")
+    model_selector = model_selector.replace("_tour", "")
+    if model_selector == "joint":
+        model_selector = "non_mandatory"
+    edb_directory = edb_directory.format(name=name)
+
+    def _read_csv(filename, **kwargs):
+        filename = filename.format(name=name)
+        return pd.read_csv(os.path.join(edb_directory, filename), **kwargs)
+
+    def _read_feather(filename, **kwargs):
+        filename = filename.format(name=name)
+        return pd.read_feather(os.path.join(edb_directory, filename), **kwargs)
+
+    def _to_feather(df, filename, **kwargs):
+        filename = filename.format(name=name)
+        return df.to_feather(os.path.join(edb_directory, filename), **kwargs)
+
+    def _read_pickle(filename, **kwargs):
+        filename = filename.format(name=name)
+        return pd.read_pickle(os.path.join(edb_directory, filename))
+
+    def _to_pickle(df, filename, **kwargs):
+        filename = filename.format(name=name)
+        return df.to_pickle(os.path.join(edb_directory, filename))
+
+    def _file_exists(filename):
+        filename = filename.format(name=name)
+        return os.path.exists(os.path.join(edb_directory, filename))
+
+    coefficients = _read_csv(
+        coefficients_file,
+        index_col="coefficient_name",
+    )
+    spec = _read_csv(spec_file, comment="#")
+
+    # read alternative values either as csv or feather file
+    alt_values_fea_file = alt_values_file.replace(".csv", ".fea")
+    if os.path.exists(
+        os.path.join(edb_directory, alt_values_fea_file.format(name=name))
+    ):
+        alt_values = _read_feather(alt_values_fea_file)
+    else:
+        alt_values = _read_csv(alt_values_file)
+        if alt_values_to_feather:
+            _to_feather(df=alt_values, filename=alt_values_fea_file)
+    chooser_data = _read_csv(chooser_file)
+    landuse = _read_csv(landuse_file, index_col="zone_id")
+    master_size_spec = _read_csv(size_spec_file)
+
+    # remove temp rows from spec, ASim uses them to calculate the other values written
+    # to the EDB, but they are not actually part of the utility function themselves.
+    spec = spec.loc[~spec.Expression.isna()]
+    spec = spec.loc[~spec.Expression.str.startswith("_")].copy()
+
+    settings_file = settings_file.format(name=name)
+    with open(os.path.join(edb_directory, settings_file), "r") as yf:
+        settings = yaml.load(
+            yf,
+            Loader=yaml.SafeLoader,
+        )
+
+    include_settings = settings.get("include_settings")
+    if include_settings:
+        include_settings = os.path.join(edb_directory, include_settings)
+    if include_settings and os.path.exists(include_settings):
+        with open(include_settings, "r") as yf:
+            more_settings = yaml.load(
+                yf,
+                Loader=yaml.SafeLoader,
+            )
+        settings.update(more_settings)
+
+    CHOOSER_SEGMENT_COLUMN_NAME = settings.get("CHOOSER_SEGMENT_COLUMN_NAME")
+    SEGMENT_IDS = settings.get("SEGMENT_IDS")
+    if SEGMENT_IDS is None:
+        SEGMENTS = settings.get("SEGMENTS")
+        if SEGMENTS is not None:
+            SEGMENT_IDS = {i: i for i in SEGMENTS}
+
+    SIZE_TERM_SELECTOR = (
+        settings.get("SIZE_TERM_SELECTOR", model_selector) or model_selector
+    )
+
+    # filter size spec for this location choice only
+    size_spec = (
+        master_size_spec.query(f"model_selector == '{SIZE_TERM_SELECTOR}'")
+        .drop(columns="model_selector")
+        .set_index("segment")
+    )
+    size_spec = size_spec.loc[:, size_spec.max() > 0]
+    assert (
+        len(size_spec) > 0
+    ), f"Empty size_spec, is model_selector {SIZE_TERM_SELECTOR} in your size term file?"
+
+    size_coef = size_coefficients_from_spec(size_spec)
+
+    indexes_to_drop = [
+        "util_size_variable",  # pre-computed size (will be re-estimated)
+        "util_size_variable_atwork",  # pre-computed size (will be re-estimated)
+        "util_utility_adjustment",  # shadow pricing (ignored in estimation)
+        "@df['size_term'].apply(np.log1p)",  # pre-computed size (will be re-estimated)
+    ]
+    if "Label" in spec.columns:
+        indexes_to_drop = [i for i in indexes_to_drop if i in spec.Label.to_numpy()]
+        label_column_name = "Label"
+    elif "Expression" in spec.columns:
+        indexes_to_drop = [
+            i for i in indexes_to_drop if i in spec.Expression.to_numpy()
+        ]
+        label_column_name = "Expression"
+    else:
+        raise ValueError("cannot find Label or Expression in spec file")
+
+    expression_labels = None
+    if label_column_name == "Expression":
+        expression_labels = {
+            expr: f"variable_label{n:04d}"
+            for n, expr in enumerate(spec.Expression.to_numpy())
+        }
+
+    # Remove shadow pricing and pre-existing size expression for re-estimation
+    spec = spec.set_index(label_column_name).drop(index=indexes_to_drop).reset_index()
+
+    if label_column_name == "Expression":
+        spec.insert(0, "Label", spec["Expression"].map(expression_labels))
+        alt_values["variable"] = alt_values["variable"].map(expression_labels)
+        label_column_name = "Label"
+
+    if name == "trip_destination":
+        CHOOSER_SEGMENT_COLUMN_NAME = "primary_purpose"
+        primary_purposes = spec.columns[3:]
+        SEGMENT_IDS = {pp: pp for pp in primary_purposes}
+
+    chooser_index_name = chooser_data.columns[0]
+    x_co = chooser_data.set_index(chooser_index_name)
+
+    def split(a, n):
+        k, m = divmod(len(a), n)
+        return (a[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n))
+
+    # process x_ca with cv_to_ca with or without chunking
+    x_ca_pickle_file = "{name}_x_ca.pkl"
+    if chunking_size == None:
+        x_ca = cv_to_ca(
+            alt_values.set_index([chooser_index_name, alt_values.columns[1]])
+        )
+    elif _file_exists(x_ca_pickle_file):
+        # if pickle file from previous x_ca processing exist, load it to save time
+        time_start = datetime.now()
+        x_ca = _read_pickle(x_ca_pickle_file)
+        print(
+            f"x_ca data loaded from {name}_x_ca.fea - time elapsed {(datetime.now() - time_start).total_seconds()}"
+        )
+    else:
+        time_start = datetime.now()
+        # calculate num_chunks based on chunking_size (or max number of rows per chunk)
+        num_chunks = int(len(alt_values) / chunking_size)
+        id_col_name = alt_values.columns[0]
+        all_ids = list(alt_values[id_col_name].unique())
+        split_ids = list(split(all_ids, num_chunks))
+        x_ca_list = []
+        i = 0
+        for chunk_ids in split_ids:
+            alt_values_i = alt_values[alt_values[id_col_name].isin(chunk_ids)]
+            x_ca_i = cv_to_ca(
+                alt_values_i.set_index([chooser_index_name, alt_values_i.columns[1]])
+            )
+            x_ca_list.append(x_ca_i)
+            print(
+                f"\rx_ca_i compute done for chunk {i}/{num_chunks} - time elapsed {(datetime.now() - time_start).total_seconds()}"
+            )
+            i = i + 1
+        x_ca = pd.concat(x_ca_list, axis=0)
+        # save final x_ca result as pickle file to save time for future data loading
+        _to_pickle(df=x_ca, filename=x_ca_pickle_file)
+        print(
+            f"x_ca compute done - time elapsed {(datetime.now() - time_start).total_seconds()}"
+        )
+
+    if CHOOSER_SEGMENT_COLUMN_NAME is not None:
+        # label segments with names
+        SEGMENT_IDS_REVERSE = {v: k for k, v in SEGMENT_IDS.items()}
+        x_co["_segment_label"] = x_co[CHOOSER_SEGMENT_COLUMN_NAME].apply(
+            lambda x: SEGMENT_IDS_REVERSE[x]
+        )
+    else:
+        x_co["_segment_label"] = size_spec.index[0]
+
+    # compute total size values by segment
+    for segment in size_spec.index:
+        total_size_segment = pd.Series(0, index=landuse.index)
+        x_co["total_size_" + segment] = 0
+        for land_use_field in size_spec.loc[segment].index:
+            total_size_segment += (
+                landuse[land_use_field] * size_spec.loc[segment, land_use_field]
+            )
+        x_co["total_size_" + segment] = total_size_segment.loc[
+            x_co["override_choice"]
+        ].to_numpy()
+
+    # for each chooser, collate the appropriate total size value
+    x_co["total_size_segment"] = 0
+    for segment in size_spec.index:
+        labels = "total_size_" + segment
+        rows = x_co["_segment_label"] == segment
+        x_co.loc[rows, "total_size_segment"] = x_co[labels][rows]
+
+    # Remove choosers with invalid observed choice (appropriate total size value = 0)
+    valid_observed_zone = x_co["total_size_segment"] > 0
+    x_co = x_co[valid_observed_zone]
+    x_ca = x_ca[x_ca.index.get_level_values(chooser_index_name).isin(x_co.index)]
+
+    # Merge land use characteristics into CA data
+    try:
+        x_ca_1 = pd.merge(x_ca, landuse, on="zone_id", how="left")
+    except KeyError:
+        # Missing the zone_id variable?
+        # Use the alternative id's instead, which assumes no sampling of alternatives
+        x_ca_1 = pd.merge(
+            x_ca,
+            landuse,
+            left_on=x_ca.index.get_level_values(1),
+            right_index=True,
+            how="left",
+        )
+    x_ca_1.index = x_ca.index
+
+    # Availability of choice zones
+    if "util_no_attractions" in x_ca_1:
+        av = (
+            x_ca_1["util_no_attractions"]
+            .apply(lambda x: False if x == 1 else True)
+            .astype(np.int8)
+        )
+    elif "@df['size_term']==0" in x_ca_1:
+        av = (
+            x_ca_1["@df['size_term']==0"]
+            .apply(lambda x: False if x == 1 else True)
+            .astype(np.int8)
+        )
+    else:
+        av = 1
+
+    assert len(x_co) > 0, "Empty chooser dataframe"
+    assert len(x_ca_1) > 0, "Empty alternatives dataframe"
+
+    d = DataFrames(co=x_co, ca=x_ca_1, av=av)
+
+    m = Model(dataservice=d)
+    if len(spec.columns) == 4 and all(
+        spec.columns == ["Label", "Description", "Expression", "coefficient"]
+    ):
+        m.utility_ca = linear_utility_from_spec(
+            spec,
+            x_col="Label",
+            p_col=spec.columns[-1],
+            ignore_x=("local_dist",),
+        )
+    elif (
+        len(spec.columns) == 4
+        and all(spec.columns[:3] == ["Label", "Description", "Expression"])
+        and len(SEGMENT_IDS) == 1
+        and spec.columns[3] == list(SEGMENT_IDS.values())[0]
+    ):
+        m.utility_ca = linear_utility_from_spec(
+            spec,
+            x_col="Label",
+            p_col=spec.columns[-1],
+            ignore_x=("local_dist",),
+        )
+    else:
+        m.utility_ca = linear_utility_from_spec(
+            spec,
+            x_col=label_column_name,
+            p_col=SEGMENT_IDS,
+            ignore_x=("local_dist",),
+            segment_id=CHOOSER_SEGMENT_COLUMN_NAME,
+        )
+
+    if CHOOSER_SEGMENT_COLUMN_NAME is None:
+        assert len(size_spec) == 1
+        m.quantity_ca = sum(
+            P(f"{i}_{q}") * X(q)
+            for i in size_spec.index
+            for q in size_spec.columns
+            if size_spec.loc[i, q] != 0
+        )
+    else:
+        m.quantity_ca = sum(
+            P(f"{i}_{q}")
+            * X(q)
+            * X(f"{CHOOSER_SEGMENT_COLUMN_NAME}=={str_repr(SEGMENT_IDS[i])}")
+            for i in size_spec.index
+            for q in size_spec.columns
+            if size_spec.loc[i, q] != 0
+        )
+
+    apply_coefficients(coefficients, m, minimum=-25, maximum=25)
+    apply_coefficients(size_coef, m, minimum=-6, maximum=6)
+
+    m.choice_co_code = "override_choice"
+
+    if return_data:
+        return (
+            m,
+            Dict(
+                edb_directory=Path(edb_directory),
+                alt_values=alt_values,
+                chooser_data=chooser_data,
+                coefficients=coefficients,
+                landuse=landuse,
+                spec=spec,
+                size_spec=size_spec,
+                master_size_spec=master_size_spec,
+                model_selector=model_selector,
+                settings=settings,
+            ),
+        )
+
+    return m
+
+
+def update_size_spec(model, data, result_dir=Path("."), output_file=None):
+    master_size_spec = data.master_size_spec
+    size_spec = data.size_spec
+    model_selector = data.model_selector
+
+    # Write size coefficients into size_spec
+    for c in size_spec.columns:
+        for i in size_spec.index:
+            param_name = f"{i}_{c}"
+            j = (master_size_spec["segment"] == i) & (
+                master_size_spec["model_selector"] == model_selector
+            )
+            try:
+                master_size_spec.loc[j, c] = np.exp(model.get_value(param_name))
+            except KeyError:
+                pass
+
+    # Rescale each row to total 1, not mathematically needed
+    # but to maintain a consistent approach from existing ASim
+    master_size_spec.iloc[:, 2:] = master_size_spec.iloc[:, 2:].div(
+        master_size_spec.iloc[:, 2:].sum(1), axis=0
+    )
+
+    if output_file is not None:
+        os.makedirs(result_dir, exist_ok=True)
+        master_size_spec.reset_index().to_csv(
+            result_dir / output_file,
+            index=False,
+        )
+
+    return master_size_spec
+
+
+def workplace_location_model(**kwargs):
+    unused = kwargs.pop("name", None)
+    return location_choice_model(
+        name="workplace_location",
+        **kwargs,
+    )
+
+
+def school_location_model(**kwargs):
+    unused = kwargs.pop("name", None)
+    return location_choice_model(
+        name="school_location",
+        **kwargs,
+    )
+
+
+def atwork_subtour_destination_model(**kwargs):
+    unused = kwargs.pop("name", None)
+    return location_choice_model(
+        name="atwork_subtour_destination",
+        **kwargs,
+    )
+
+
+def joint_tour_destination_model(**kwargs):
+    # goes with non_mandatory_tour_destination
+    unused = kwargs.pop("name", None)
+    if "coefficients_file" not in kwargs:
+        kwargs["coefficients_file"] = "non_mandatory_tour_destination_coefficients.csv"
+    return location_choice_model(
+        name="joint_tour_destination",
+        **kwargs,
+    )
+
+
+def non_mandatory_tour_destination_model(**kwargs):
+    # goes with joint_tour_destination
+    unused = kwargs.pop("name", None)
+    return location_choice_model(
+        name="non_mandatory_tour_destination",
+        **kwargs,
+    )
+
+
+def trip_destination_model(**kwargs):
+    unused = kwargs.pop("name", None)
+    return location_choice_model(
+        name="trip_destination",
+        **kwargs,
+    )
