df83869dd99c90022d153c0f4a556e1de7fc504f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:37 +0000

    Add package pydiaid/0.0.38/pydiaid-0.0.38.tar.gz

---
+++ packages/pydiaid/pydiaid-0.0.38.tar.gz/pydiaid-0.0.38/tests/test_gui.py
@@ -0,0 +1,11 @@
+#!python -m unittest tests.test_utils
+"""This module provides unit tests for pydiaid.gui."""
+
+# builtin
+import unittest
+
+# local
+import pydiaid.gui
+
+if __name__ == "__main__":
+    unittest.main()
