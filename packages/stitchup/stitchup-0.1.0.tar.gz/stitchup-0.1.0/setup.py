4141284ff424d6c8834e9144677a4d1344ec25f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:18 +0000

    Add package stitchup/0.1.0/stitchup-0.1.0.tar.gz

---
+++ packages/stitchup/stitchup-0.1.0.tar.gz/stitchup-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stitchup',
+    version='0.1.0',
+    packages=find_packages(),
+    author="Amit Malaker",
+    description="A tool to stitch up together multiple python files into a single file (just like Rollup JS)",
+)
