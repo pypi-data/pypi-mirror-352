862b6a7d6921b5ab84863093a021dc63ece3bbed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:59 +0000

    Add package PromptTasker/0.1.7/prompttasker-0.1.7.tar.gz

---
+++ packages/PromptTasker/prompttasker-0.1.7.tar.gz/prompttasker-0.1.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="PromptTasker",
+    version="0.1.7",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "numpy",
+        "pandas",
+        "pillow",
+        "duckdb",
+        "mdformat"
+    ],
+    entry_points={
+        'console_scripts': [
+            'PromptTasker=PromptTasker.cli:main',  
+        ],
+    },
+    author="Suneha Datta",
+    description="A prompt-powered multi-task CLI utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.7',
+)
+
