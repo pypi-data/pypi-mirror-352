3cf74c62e66f5bd4b06fea27e9c88d1e8fc057c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:09 +0000

    Add package ai-pr-reviewer/0.0.8/ai_pr_reviewer-0.0.8-py3-none-any.whl

---
+++ packages/ai-pr-reviewer/ai_pr_reviewer-0.0.8-py3-none-any.whl/ai_pr_review/clients/__init__.py
@@ -0,0 +1,4 @@
+from .azure import AzureDevOpsClient as AzureDevOpsClient
+from .base import PullRequestClient as PullRequestClient
+from .github import GitHubClient as GitHubClient
+from .gitlab import GitLabClient as GitLabClient
