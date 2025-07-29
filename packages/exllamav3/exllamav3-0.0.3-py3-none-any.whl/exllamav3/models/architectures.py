b83833821d35fd499c9795d53a303472910e2248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:06 +0000

    Add package exllamav3/0.0.3/exllamav3-0.0.3-py3-none-any.whl

---
+++ packages/exllamav3/exllamav3-0.0.3-py3-none-any.whl/exllamav3/models/architectures.py
@@ -0,0 +1,38 @@
+from .llama import LlamaModel
+from .mistral import MistralModel
+from .mixtral import MixtralModel
+from .qwen2 import Qwen2Model
+from .qwen3 import Qwen3Model
+from .qwen3_moe import Qwen3MoeModel
+from .phi3 import Phi3Model
+from .gemma2 import Gemma2Model
+from .gemma3 import Gemma3Model
+from .decilm import DeciLMModel
+from .glm4 import Glm4Model
+from .cohere import CohereModel
+from .cohere2 import Cohere2Model
+
+ARCHITECTURES = {
+    m.config_class.arch_string: {
+        "architecture": m.config_class.arch_string,
+        "config_class": m.config_class,
+        "model_class": m,
+    } for m in [
+        LlamaModel,
+        MistralModel,
+        MixtralModel,
+        Qwen2Model,
+        Qwen3Model,
+        Qwen3MoeModel,
+        Phi3Model,
+        Gemma2Model,
+        Gemma3Model,
+        DeciLMModel,
+        Glm4Model,
+        CohereModel,
+        Cohere2Model,
+    ]
+}
+
+def get_architectures():
+    return ARCHITECTURES
