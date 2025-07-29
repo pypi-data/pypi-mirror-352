add13fcb4ffed62c380f5b28390411797dd36d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:20:31 +0000

    Add package utilsps/0.1.0/utilsps-0.1.0.tar.gz

---
+++ packages/utilsps/utilsps-0.1.0.tar.gz/utilsps-0.1.0/README.md
@@ -0,0 +1,3 @@
+# utilsps
+
+A simple Python package with file utility functions.
---
+++ packages/utilsps/utilsps-0.1.0.tar.gz/utilsps-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utilsps',
+    version='0.1.0',
+    description='Simple file utilities package',
+    author='Your Name',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
