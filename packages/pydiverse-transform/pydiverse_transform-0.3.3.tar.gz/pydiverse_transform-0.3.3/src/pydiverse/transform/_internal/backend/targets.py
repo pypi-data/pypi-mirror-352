10aa5b7c9ae211f12d9cacf476b425f78374025a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:34 +0000

    Add package pydiverse-transform/0.3.3/pydiverse_transform-0.3.3.tar.gz

---
+++ packages/pydiverse-transform/pydiverse_transform-0.3.3.tar.gz/pydiverse_transform-0.3.3/src/pydiverse/transform/_internal/backend/targets.py
@@ -0,0 +1,39 @@
+# This module defines the config classes provided to the user to configure
+# the backend on import / export.
+
+
+from __future__ import annotations
+
+import sqlalchemy as sqa
+
+
+class Target: ...
+
+
+class Polars(Target):
+    def __init__(self, *, lazy: bool = False) -> None:
+        self.lazy = lazy
+
+
+class Pandas(Target): ...
+
+
+class DuckDb(Target): ...
+
+
+class SqlAlchemy(Target):
+    def __init__(self, engine: sqa.Engine, *, schema: str | None = None):
+        self.engine = engine
+        self.schema = schema
+
+
+class Scalar(Target): ...
+
+
+class Dict(Target): ...
+
+
+class DictOfLists(Target): ...
+
+
+class ListOfDicts(Target): ...
---
+++ packages/pydiverse-transform/pydiverse_transform-0.3.3.tar.gz/pydiverse_transform-0.3.3/src/pydiverse/transform/_internal/ops/ops/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from .aggregation import *
+from .arithmetic import *
+from .comparison import *
+from .datetime import *
+from .horizontal import *
+from .list import *
+from .logical import *
+from .markers import *
+from .numeric import *
+from .string import *
+from .window import *
