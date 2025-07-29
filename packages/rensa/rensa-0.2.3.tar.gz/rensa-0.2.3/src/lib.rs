53823b16bbc65dc3ee0ae6181f895a8783de9c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:51 +0000

    Add package rensa/0.2.3/rensa-0.2.3.tar.gz

---
+++ packages/rensa/rensa-0.2.3.tar.gz/rensa-0.2.3/src/lib.rs
@@ -0,0 +1,32 @@
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![allow(clippy::unsafe_derive_deserialize)]
+#![allow(clippy::cast_precision_loss)]
+#![allow(clippy::needless_pass_by_value)]
+
+mod cminhash;
+mod lsh;
+mod opt_dens_minhash;
+mod rminhash;
+mod utils;
+
+pub use cminhash::CMinHash;
+pub use lsh::RMinHashLSH;
+pub use opt_dens_minhash::OptDensMinHash;
+pub use rminhash::RMinHash;
+
+use pyo3::prelude::*;
+
+/// Python module for MinHash and LSH implementations
+///
+/// # Errors
+/// Returns an error if the module initialization fails or classes cannot be added
+#[pymodule]
+pub fn rensa(m: &Bound<'_, PyModule>) -> PyResult<()> {
+  m.add_class::<RMinHash>()?;
+  m.add_class::<OptDensMinHash>()?;
+  m.add_class::<CMinHash>()?;
+  m.add_class::<RMinHashLSH>()?;
+  Ok(())
+}
