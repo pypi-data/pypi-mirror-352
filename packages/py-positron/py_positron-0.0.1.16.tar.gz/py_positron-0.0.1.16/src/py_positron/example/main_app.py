b0dd2adada3220ec9c84371ebb24cd87d7e43af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:01 +0000

    Add package py-positron/0.0.1.16/py_positron-0.0.1.16.tar.gz

---
+++ packages/py-positron/py_positron-0.0.1.16.tar.gz/py_positron-0.0.1.16/src/py_positron/example/main_app.py
@@ -0,0 +1,4 @@
+import py_positron as main
+# Run this file by typing positron start on the terminal while at the project root directory.
+main.openUI("views/index.html",None,900,700,"Example app")
+# Go to views to see and edit the HTML file.
