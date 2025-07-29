3edd127888fa5a3e753c34674362fa15ddb0bcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:45 +0000

    Add package scrutinydebugger/0.4.3/scrutinydebugger-0.4.3-py3-none-any.whl

---
+++ packages/scrutinydebugger/scrutinydebugger-0.4.3-py3-none-any.whl/scrutiny/core/bintools/elftools_stubs.py
@@ -0,0 +1,8 @@
+#    elftools_stubs.py
+#        Stubfile for pyelftools
+#
+#   - License : MIT - See LICENSE file.
+#   - Project :  Scrutiny Debugger (github.com/scrutinydebugger/scrutiny-main)
+#
+#   Copyright (c) 2021 Scrutiny Debugger
+
---
+++ packages/scrutinydebugger/scrutinydebugger-0.4.3-py3-none-any.whl/scrutiny/exceptions.py
@@ -0,0 +1,10 @@
+#    exceptions.py
+#        Some exceptions specific to this module
+#
+#   - License : MIT - See LICENSE file.
+#   - Project :  Scrutiny Debugger (github.com/scrutinydebugger/scrutiny-main)
+#
+#   Copyright (c) 2021 Scrutiny Debugger
+
+class EnvionmentNotSetUpException(Exception):
+    pass
