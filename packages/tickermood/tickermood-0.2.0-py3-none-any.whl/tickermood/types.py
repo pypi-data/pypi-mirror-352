bd4119dd72439f8e1f27fb57f907625c41b35049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:49 +0000

    Add package tickermood/0.2.0/tickermood-0.2.0-py3-none-any.whl

---
+++ packages/tickermood/tickermood-0.2.0-py3-none-any.whl/tickermood/types.py
@@ -0,0 +1,6 @@
+from typing import Literal
+
+SourceName = Literal["Investing", "Marketwatch"]
+ConsensusType = Literal[
+    "Strong Buy", "Buy", "Cautious Buy", "Hold", "Cautious Sell", "Sell", "Strong Sell"
+]
