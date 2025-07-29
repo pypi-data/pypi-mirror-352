84ba58ffa6ca2800f891deb75c3c50e3d92745fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:10 +0000

    Add package anglefadez/0.1.0/anglefadez-0.1.0.tar.gz

---
+++ packages/anglefadez/anglefadez-0.1.0.tar.gz/anglefadez-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="anglefadez", 
+    version="0.1.0",
+    description="Cool terminal text fade.",
+    author="Angle Facts",
+    author_email="your@email.com",
+    packages=["anglefadez"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
