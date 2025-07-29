abfed97ff9a4a1efa2aed1ad122bad6407cfddd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:32 +0000

    Add package agentstr-sdk/0.2.4/agentstr_sdk-0.2.4.tar.gz

---
+++ packages/agentstr-sdk/agentstr_sdk-0.2.4.tar.gz/agentstr_sdk-0.2.4/src/agentstr/__init__.py
@@ -0,0 +1,9 @@
+from agentstr.nostr_client import NostrClient
+from agentstr.nostr_agent_server import NostrAgentServer, NoteFilters
+from agentstr.nostr_mcp_server import NostrMCPServer
+from agentstr.nostr_mcp_client import NostrMCPClient
+from agentstr.nostr_rag import NostrRAG
+from agentstr.nwc_relay import NWCRelay
+from agentstr.a2a import AgentCard, Skill, ChatInput
+from agentstr.logger import get_logger
+from pynostr.key import PrivateKey, PublicKey
---
+++ packages/agentstr-sdk/agentstr_sdk-0.2.4.tar.gz/agentstr_sdk-0.2.4/tests/test_relay_manager.py
@@ -0,0 +1,47 @@
+import asyncio
+import os
+import time
+from dotenv import load_dotenv
+load_dotenv()
+
+from agentstr.relay_manager import RelayManager
+from pynostr.key import PrivateKey
+
+
+
+async def test_relay_manager():
+    relays = os.getenv('NOSTR_RELAYS').split(',')
+
+    private_key1 = PrivateKey()
+    private_key2 = PrivateKey()
+
+    manager = RelayManager(relays, private_key1)
+    manager2 = RelayManager(relays, private_key2)
+
+    timestamp = int(time.time())
+    event = await manager.send_message("hello", private_key2.public_key.hex())
+    print(event)
+    
+    dm_event = await manager2.receive_message(private_key1.public_key.hex(), timestamp)
+    print(dm_event)
+
+    assert dm_event.message == "hello"
+
+
+async def test_get_following():
+    relays = os.getenv('NOSTR_RELAYS').split(',')
+    private_key = PrivateKey.from_nsec(os.getenv('AGENT_PRIVATE_KEY'))
+    manager = RelayManager(relays, private_key)
+    following = await manager.get_following()
+    print('Following')
+    print(following)
+
+
+async def test_suite():
+    await test_relay_manager()
+    await test_get_following()
+
+
+if __name__ == '__main__':
+    asyncio.run(test_suite())
+    
