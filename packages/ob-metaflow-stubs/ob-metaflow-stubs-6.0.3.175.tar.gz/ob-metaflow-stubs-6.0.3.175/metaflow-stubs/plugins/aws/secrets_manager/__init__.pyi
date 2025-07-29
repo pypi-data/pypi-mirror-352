28ac9bdc4051ff093297cfdc0f1d06e066774e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:51 +0000

    Add package ob-metaflow-stubs/6.0.3.175/ob-metaflow-stubs-6.0.3.175.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.175.tar.gz/ob-metaflow-stubs-6.0.3.175/metaflow-stubs/plugins/aws/secrets_manager/__init__.pyi
@@ -0,0 +1,11 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.14.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-02T15:33:51.350119                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import aws_secrets_manager_secrets_provider as aws_secrets_manager_secrets_provider
+
