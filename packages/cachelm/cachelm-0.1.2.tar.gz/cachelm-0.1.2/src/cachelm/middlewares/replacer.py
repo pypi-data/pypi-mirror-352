039a9d5504e71f8138858b9e47970316fbbfc17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:18 +0000

    Add package cachelm/0.1.2/cachelm-0.1.2.tar.gz

---
+++ packages/cachelm/cachelm-0.1.2.tar.gz/cachelm-0.1.2/src/cachelm/middlewares/replacer.py
@@ -0,0 +1,44 @@
+from cachelm.middlewares.middleware import Middleware
+
+
+class Replacement:
+    """
+    A class representing a replacement operation.
+    """
+
+    def __init__(
+        self,
+        key: str,
+        value: str,
+    ):
+        """
+        Initialize the Replacement object.
+
+        Args:
+            key (str): The string to be replaced.
+            value (str): The string to replace with.
+        """
+        self.key = key
+        self.value = value
+
+
+class Replacer(Middleware):
+
+    def __init__(self, replacements: list[Replacement]):
+        """
+        Initialize the Replacer middleware.
+
+        Args:
+            replacements: list[Replacement]: A list of Replacement objects.
+        """
+        self.replacements = replacements
+
+    def pre_cache_save(self, message, history):
+        return message
+
+    def post_cache_retrieval(self, message, history):
+        for replacement in self.replacements:
+            message.content = message.content.replace(
+                replacement.key, replacement.value
+            )
+        return message
