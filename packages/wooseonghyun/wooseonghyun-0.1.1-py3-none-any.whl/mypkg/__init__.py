6f3cc4139260dd1ddf77319110df0c682aa90926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:02 +0000

    Add package wooseonghyun/0.1.1/wooseonghyun-0.1.1-py3-none-any.whl

---
+++ packages/wooseonghyun/wooseonghyun-0.1.1-py3-none-any.whl/mypkg/__init__.py
@@ -0,0 +1,4 @@
+from .cleaner import clean_text
+from .detector import contains_keyword
+
+__all__ = ["clean_text", "contains_keyword"]
---
+++ packages/wooseonghyun/wooseonghyun-0.1.1-py3-none-any.whl/mypkg/detector.py
@@ -0,0 +1,2 @@
+def contains_keyword(text, keyword):
+    return keyword.lower() in text.lower()
