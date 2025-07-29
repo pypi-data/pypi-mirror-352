3c9c446b47b6f77ad1313e9265c74d48afc4a84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:23 +0000

    Add package spforge/0.5.5/spforge-0.5.5-py3-none-any.whl

---
+++ packages/spforge/spforge-0.5.5-py3-none-any.whl/spforge/predictor/__init__.py
@@ -0,0 +1,9 @@
+from .predictor import (
+    SklearnPredictor,
+    GroupByPredictor,
+    GranularityPredictor,
+    SklearnPredictor,
+)
+from ._base import BasePredictor
+from .sklearn_estimator import OrdinalClassifier, SkLearnWrapper
+from ._distribution import NegativeBinomialPredictor, DistributionManagerPredictor
