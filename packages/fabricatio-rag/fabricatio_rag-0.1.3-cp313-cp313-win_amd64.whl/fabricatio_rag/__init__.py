2ead62f14e0b4ce1750a02118410d87e37e34104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:17 +0000

    Add package fabricatio-rag/0.1.3/fabricatio_rag-0.1.3-cp313-cp313-win_amd64.whl

---
+++ packages/fabricatio-rag/fabricatio_rag-0.1.3-cp313-cp313-win_amd64.whl/fabricatio_rag/__init__.py
@@ -0,0 +1,5 @@
+"""Fabricatio is a Python library for building llm app using event-based agent structure."""
+
+from fabricatio_rag import capabilities, models
+
+__all__ = ["capabilities", "models"]
