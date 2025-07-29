c5a49bc38774cc5e6872eb6c974406969c0ace88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:35 +0000

    Add package fed-rag/0.0.24/fed_rag-0.0.24.tar.gz

---
+++ packages/fed-rag/fed_rag-0.0.24.tar.gz/fed_rag-0.0.24/tests/generators/test_base.py
@@ -0,0 +1,13 @@
+from fed_rag.base.generator import BaseGenerator
+
+
+def test_generate(mock_generator: BaseGenerator) -> None:
+    output = mock_generator.generate(query="hello", context="again")
+    assert output == "mock output from 'hello' and 'again'."
+
+
+def test_compute_target_sequence_proba(mock_generator: BaseGenerator) -> None:
+    proba = mock_generator.compute_target_sequence_proba(
+        prompt="mock prompt", target="mock target"
+    )
+    assert proba == 0.42
