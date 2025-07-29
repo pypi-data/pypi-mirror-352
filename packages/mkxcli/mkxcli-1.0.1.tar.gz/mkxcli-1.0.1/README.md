9bf4a9ce2fbf4e24dedfa8e092e63d16baf32a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:57 +0000

    Add package mkxcli/1.0.1/mkxcli-1.0.1.tar.gz

---
+++ packages/mkxcli/mkxcli-1.0.1.tar.gz/mkxcli-1.0.1/README.md
@@ -0,0 +1,30 @@
+
+# Make
+
+This CLI (command line interface) is designed to make any file with custom contents.
+
+
+## Documentation
+
+This makes any file with any extension easily
+
+eg. ```make hello.txt``` or ```make hello.bat```
+
+There are parameters, those being
+ ```-c``` eg. ```make hello.txt -c hi```
+
+```-c``` = the contents of the file
+
+```-encode``` eg. ```encode hi.txt```
+
+```-decode``` does the opposite
+
+Also the file is always made in the desktop folder
+
+
+## Deployment
+
+To deploy this project run
+
+```pip install mkxcli```
+
---
+++ packages/mkxcli/mkxcli-1.0.1.tar.gz/mkxcli-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkxcli',
+    version='1.0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'make = mkxcli.make:main',
+            'encode = mkxcli.encode:main',
+            'decode = mkxcli.decode:main',
+        ],
+    },
+    install_requires=[],
+    author='Martin V.',
+    description='Simple CLI tools for make, encode, decode',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+)
