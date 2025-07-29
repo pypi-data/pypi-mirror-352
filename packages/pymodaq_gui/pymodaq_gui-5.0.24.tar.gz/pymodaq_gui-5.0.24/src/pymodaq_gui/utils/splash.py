3a1dc8587d4549efccb91334e248b154188b163d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:09 +0000

    Add package pymodaq_gui/5.0.24/pymodaq_gui-5.0.24.tar.gz

---
+++ packages/pymodaq_gui/pymodaq_gui-5.0.24.tar.gz/pymodaq_gui-5.0.24/src/pymodaq_gui/utils/splash.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from qtpy import QtWidgets, QtGui, QtCore
+from qtpy.QtCore import Qt
+
+
+class MySplash(QtWidgets.QSplashScreen):
+
+    def __init__(self, *args, **kwargs):
+        here = Path(__file__)
+        pixmap = QtGui.QPixmap(str(here.parent.parent.joinpath('splash.png')))
+        super().__init__(pixmap, Qt.WindowStaysOnTopHint, *args, **kwargs)
+
+        font = self.font()
+        font.setPixelSize(18)
+        self.setFont(font)
+
+    def showMessage(self, message, *args, **kwargs):
+        """ force any message to be printed in white in the right/top corner """
+        super().showMessage(message, QtCore.Qt.AlignRight, QtCore.Qt.white,
+                            )
+
+def get_splash_sc() -> MySplash:
+    return MySplash()
