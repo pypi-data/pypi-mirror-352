06591b07c26a07dd5b0f608268063551151cd0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:55 +0000

    Add package intvalpy/1.6.6/intvalpy-1.6.6.tar.gz

---
+++ packages/intvalpy/intvalpy-1.6.6.tar.gz/intvalpy-1.6.6/setup.py
@@ -0,0 +1,38 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+os.environ['CVXOPT_BUILD_GLPK'] = '1'
+
+readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+with io.open(readme_file, mode='r', encoding='utf-8') as f:
+    README = f.read()
+
+INSTALL_REQUIRES = [
+    'matplotlib',
+    'numpy',
+    'cvxopt',
+    'mpmath',
+    'pandas'
+]
+
+setup(
+    name='intvalpy',
+    version='1.6.6',
+    description='IntvalPy - a Python interval computation library',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    keywords=[
+        'interval',
+        'inequality visualization',
+        'optimal solutions',
+        'math',
+        'machine learning'
+    ],
+    author='Андросов Артем Станиславович, Шарый Сергей Петрович',
+    author_email='artem.androsov@gmail.com, shary@ict.nsc.ru',
+    url='https://github.com/AndrosovAS/intvalpy',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES
+)
