1bf6b4e540492d27c6725c3119a8a561e5e48b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:21:29 +0000

    Add package PyGuymer3/0.0.8/pyguymer3-0.0.8.tar.gz

---
+++ packages/PyGuymer3/pyguymer3-0.0.8.tar.gz/pyguymer3-0.0.8/pyguymer3/image/image2gif.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+# Define function ...
+def image2gif(
+    img,
+    gif,
+    /,
+    *,
+       chunksize = 1048576,
+           debug = __debug__,
+    exiftoolPath = None,
+    gifsiclePath = None,
+    jpegtranPath = None,
+            mode = "RGB",
+        optimize = True,
+     optipngPath = None,
+    screenHeight = -1,
+     screenWidth = -1,
+           strip = False,
+         timeout = 60.0,
+):
+    """Save an image as a GIF
+
+    This function accepts either a PIL Image or a file path and saves the image
+    as a GIF.
+
+    Parameters
+    ----------
+    img : PIL.Image.Image or str
+        the input PIL Image or path to the input image
+    gif : str
+        the path to the output GIF
+    debug : bool, optional
+        print debug messages (default False)
+    exiftoolPath : str, optional
+        the path to the "exiftool" binary (if not provided then Python will attempt to
+        find the binary itself)
+    gifsiclePath : str, optional
+        the path to the "gifsicle" binary (if not provided then Python will attempt to
+        find the binary itself)
+    jpegtranPath : str, optional
+        the path to the "jpegtran" binary (if not provided then Python will attempt to
+        find the binary itself)
+    mode : str, optional
+        the mode of the outout GIF (default "RGB")
+    optimize : bool, optional
+        optimize the output GIF (default True)
+    optipngPath : str, optional
+        the path to the "optipng" binary (if not provided then Python will attempt to
+        find the binary itself)
+    screenHeight : int, optional
+        the height of the screen to downscale the input image to fit within,
+        currently only implemented if "img" is a str (default -1; integers less
+        than 100 imply no downscaling)
+    screenWidth : int, optional
+        the width of the screen to downscale the input image to fit within,
+        currently only implemented if "img" is a str (default -1; integers less
+        than 100 imply no downscaling)
+    strip : bool, optional
+        strip metadata from the output GIF (default False)
+    timeout : float, optional
+        the timeout for any requests/subprocess calls
+
+    Notes
+    -----
+    Copyright 2017 Thomas Guymer [1]_
+
+    References
+    ----------
+    .. [1] PyGuymer3, https://github.com/Guymer/PyGuymer3
+    """
+
+    # Import special modules ...
+    try:
+        import PIL
+        import PIL.Image
+        PIL.Image.MAX_IMAGE_PIXELS = 1024 * 1024 * 1024                         # [px]
+    except:
+        raise Exception("\"PIL\" is not installed; run \"pip install --user Pillow\"") from None
+
+    # Import sub-functions ...
+    from .optimize_image import optimize_image
+
+    # Find out what the user supplied ...
+    match img:
+        case str():
+            # Open image as RGB (even if it is paletted) ...
+            with PIL.Image.open(img) as iObj:
+                tmpImg = iObj.convert("RGB")
+
+            # Check if the user wants to scale the image down to fit within a
+            # screen size ...
+            if screenWidth >= 100 and screenHeight >= 100:
+                # Resize image in place ...
+                tmpImg.thumbnail(
+                    (screenWidth, screenHeight),
+                    resample = PIL.Image.Resampling.LANCZOS,
+                )
+
+            # Convert it to whatever mode the user asked for ...
+            tmpImg = tmpImg.convert(mode)
+        case PIL.Image.Image():
+            # Convert image to whatever mode the user asked for ...
+            tmpImg = img.convert(mode)
+        case _:
+            # Crash ...
+            raise TypeError(f"\"img\" is an unexpected type ({repr(type(img))})") from None
+
+    # Save it as a GIF ...
+    # NOTE: See https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+    tmpImg.save(
+        gif,
+        optimize = optimize,
+    )
+
+    # Optimize GIF ...
+    if optimize or strip:
+        optimize_image(
+            gif,
+               chunksize = chunksize,
+                   debug = debug,
+            exiftoolPath = exiftoolPath,
+            gifsiclePath = gifsiclePath,
+            jpegtranPath = jpegtranPath,
+             optipngPath = optipngPath,
+                    pool = None,
+                   strip = strip,
+                 timeout = timeout,
+        )
---
+++ packages/PyGuymer3/pyguymer3-0.0.8.tar.gz/pyguymer3-0.0.8/pyguymer3/image/save_array_as_image.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+
+# Define function ...
+def save_array_as_image(
+    img0,
+    fname,
+    /,
+    *,
+       chunksize = 1048576,
+              ct = "grey",
+           debug = __debug__,
+    exiftoolPath = None,
+            form = "png",
+       ftype_req = -1,
+    gifsiclePath = None,
+    jpegtranPath = None,
+     optipngPath = None,
+          pc_bot = 0.0,
+          pc_top = 0.0,
+           scale = False,
+         timeout = 60.0,
+):
+    """Save an array as an image
+
+    This function accepts a NumPy array, with optional scaling and/or colour
+    mapping, and saves it as an image. Currently only "png" and "ppm" formats
+    are available.
+
+    Parameters
+    ----------
+    img0 : numpy.ndarray
+        a 2D NumPy array of any type with shape (ny,nx)
+    fname : str
+        output file name
+    ct : str, optional
+        the colour table to apply (the default is no colour mapping, i.e.,
+        greyscale)
+    exiftoolPath : str, optional
+        the path to the "exiftool" binary (if not provided then Python will attempt to
+        find the binary itself)
+    form : str, optional
+        output image format (default "png")
+    ftype_req : int, optional
+        The PNG specification defines 5 different possible filters which are
+        numbered 0 to 4 (inclusively). Filter #0 is "no filtering". If the user
+        defines "ftype_req" as one of the identifying integers then that filter
+        will be used for the entire PNG file. If the user defines "ftype_req" as
+        "-1" (or does not define "ftype_req" at all) then adaptive filtering
+        will be used whereby an attempt is made to predict which filtering
+        method will yield the smallest compressed stream.
+    gifsiclePath : str, optional
+        the path to the "gifsicle" binary (if not provided then Python will attempt to
+        find the binary itself)
+    jpegtranPath : str, optional
+        the path to the "jpegtran" binary (if not provided then Python will attempt to
+        find the binary itself)
+    optipngPath : str, optional
+        the path to the "optipng" binary (if not provided then Python will attempt to
+        find the binary itself)
+    pc_bot : float, optional
+        the percentage to clip off the bottom of the histogram, if scaling is
+        requested (default 0.0)
+    pc_top : float, optional
+        the percentage to clip off the top of the histogram, if scaling is
+        requested (default 0.0)
+    scale : bool, optional
+        Does the input need scaling? If not, then the input array must be ≥ 0
+        and ≤ 255. (default False)
+    timeout : float, optional
+        the timeout for any requests/subprocess calls
+
+    Notes
+    -----
+    Copyright 2017 Thomas Guymer [1]_
+
+    References
+    ----------
+    .. [1] PyGuymer3, https://github.com/Guymer/PyGuymer3
+    """
+
+    # Import modules ...
+    import json
+    import os
+
+    # Import special modules ...
+    try:
+        import numpy
+    except:
+        raise Exception("\"numpy\" is not installed; run \"pip install --user numpy\"") from None
+
+    # Import sub-functions ...
+    from .optimize_image import optimize_image
+    from .save_array_as_PPM import save_array_as_PPM
+    from .save_array_as_PNG import save_array_as_PNG
+
+    # Find image size ...
+    ny, nx = img0.shape                                                         # [px], [px]
+
+    # Load colour tables ...
+    with open(f"{os.path.dirname(__file__)}/../data/json/colourTables.json", "rt", encoding = "utf-8") as fObj:
+        cts = json.load(fObj)
+
+    # Create uint8 image that will be passed to the external function ...
+    img2 = numpy.empty((ny, nx, 3), dtype = numpy.uint8)
+
+    # Check if scaling is required ...
+    if scale:
+        # Check input values ...
+        if pc_bot < 0.0:
+            raise Exception("pc_bot < 0.0") from None
+        if pc_top < 0.0:
+            raise Exception("pc_top < 0.0") from None
+        if (pc_bot + pc_top) > 100.0:
+            raise Exception("(pc_bot + pc_top) > 100.0") from None
+
+        # Create copy and find the percentiles ...
+        img1 = img0.astype(numpy.float64)
+        p_lo = numpy.percentile(img1, pc_bot)
+        p_hi = numpy.percentile(img1, 100.0 - pc_top)
+
+        # Scale the image, clip scale image, convert scaled image to correct
+        # type, clean up ...
+        img1 = 255.0 * (img1 - p_lo) / (p_hi - p_lo)
+        numpy.place(img1, img1 > 255.0, 255.0)
+        numpy.place(img1, img1 <   0.0,   0.0)
+        for ix in range(nx):
+            for iy in range(ny):
+                img2[iy, ix, :] = cts[ct][img1[iy, ix].astype(numpy.uint8)][:]
+    else:
+        # Convert image to correct type ...
+        for ix in range(nx):
+            for iy in range(ny):
+                img2[iy, ix, :] = cts[ct][img0[iy, ix].astype(numpy.uint8)][:]
+
+    # Save image ...
+    match form:
+        case "png":
+            save_array_as_PNG(
+                img2,
+                fname,
+                ftype_req = ftype_req,
+            )
+            optimize_image(
+                fname,
+                   chunksize = chunksize,
+                       debug = debug,
+                exiftoolPath = exiftoolPath,
+                gifsiclePath = gifsiclePath,
+                jpegtranPath = jpegtranPath,
+                 optipngPath = optipngPath,
+                        pool = None,
+                       strip = True,
+                     timeout = timeout,
+            )
+        case "ppm":
+            save_array_as_PPM(
+                img2,
+                fname,
+            )
+        case _:
+            # Crash ...
+            raise ValueError(f"\"form\" is an unexpected value ({repr(form)})") from None
