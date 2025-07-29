458a5bafc3ae869ee9f52d325d88759bac7563d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:45 +0000

    Add package schemathesis/4.0.0a12/schemathesis-4.0.0a12-py3-none-any.whl

---
+++ packages/schemathesis/schemathesis-4.0.0a12-py3-none-any.whl/schemathesis/__init__.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from schemathesis import errors, graphql, openapi, pytest
+from schemathesis.auths import AuthContext, auth
+from schemathesis.checks import CheckContext, CheckFunction, check
+from schemathesis.core.transport import Response
+from schemathesis.core.version import SCHEMATHESIS_VERSION
+from schemathesis.generation import GenerationMode
+from schemathesis.generation.case import Case
+from schemathesis.generation.metrics import MetricContext, MetricFunction, metric
+from schemathesis.hooks import HookContext, hook
+from schemathesis.schemas import BaseSchema
+from schemathesis.transport import SerializationContext, serializer
+
+__version__ = SCHEMATHESIS_VERSION
+
+__all__ = [
+    "__version__",
+    # Core data structures
+    "Case",
+    "Response",
+    "GenerationMode",
+    "BaseSchema",
+    # Public errors
+    "errors",
+    # Spec or usage specific namespaces
+    "openapi",
+    "graphql",
+    "pytest",
+    # Hooks
+    "hook",
+    "HookContext",
+    # Checks
+    "check",
+    "CheckContext",
+    "CheckFunction",
+    # Auth
+    "auth",
+    "AuthContext",
+    # Targeted Property-based Testing
+    "metric",
+    "MetricContext",
+    "MetricFunction",
+    # Serialization
+    "serializer",
+    "SerializationContext",
+]
