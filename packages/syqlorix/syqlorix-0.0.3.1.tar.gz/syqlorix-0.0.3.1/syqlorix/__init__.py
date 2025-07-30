2cf70ae84a70451e7680af6759fe2a42bb9c189c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:06 +0000

    Add package syqlorix/0.0.3.1/syqlorix-0.0.3.1.tar.gz

---
+++ packages/syqlorix/syqlorix-0.0.3.1.tar.gz/syqlorix-0.0.3.1/syqlorix/__init__.py
@@ -0,0 +1,19 @@
+from .page import Page
+from .devserver import start_dev_server, Route
+from .css import css
+from .components import component
+from .components_lib import SimpleAlert, ImageGallery
+from .loader import load_component
+
+__version__ = "0.0.3.1"
+
+__all__ = [
+    "Page", 
+    "start_dev_server", 
+    "css", 
+    "component", 
+    "Route",
+    "SimpleAlert",
+    "ImageGallery",
+    "load_component",
+]
---
+++ packages/syqlorix/syqlorix-0.0.3.1.tar.gz/syqlorix-0.0.3.1/syqlorix/utils.py
@@ -0,0 +1,34 @@
+import html
+import urllib.parse
+from typing import Dict, Union, List
+
+def escape_html(text: str) -> str:
+    return html.escape(text)
+
+def format_attrs(attrs: dict) -> str:
+    attr_parts = []
+    for key, value in attrs.items():
+        if key == '_class':
+            key = 'class'
+        
+        if isinstance(value, bool):
+            if value:
+                attr_parts.append(escape_html(key))
+        elif value is not None:
+            attr_parts.append(f'{escape_html(key)}="{escape_html(str(value))}"')
+    return " ".join(attr_parts)
+
+def parse_form_urlencoded(data: str) -> Dict[str, Union[str, List[str]]]:
+    """
+    Parses application/x-www-form-urlencoded string into a dictionary.
+    Values are decoded from URL encoding. If a key has multiple values,
+    it returns a list; otherwise, a single string.
+    """
+    parsed_data = urllib.parse.parse_qs(data)
+    result = {}
+    for key, values in parsed_data.items():
+        if len(values) == 1:
+            result[key] = values[0]
+        else:
+            result[key] = values
+    return result
