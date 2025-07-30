10375e40b4c7007895e175363277ee2e217b2e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:37 +0000

    Add package ispider/0.3.1/ispider-0.3.1.tar.gz

---
+++ packages/ispider/ispider-0.3.1.tar.gz/ispider-0.3.1/tests/test_run.py
@@ -0,0 +1,21 @@
+from ispider_core import ISpider
+import pandas as pd
+
+if __name__ == '__main__':
+    config_overrides = {
+        'USER_FOLDER': '/Volumes/Sandisk2TB/test_business_scraper_12',
+        'POOLS': 32,
+        'ASYNC_BLOCK_SIZE': 16,
+        'MAXIMUM_RETRIES': 1,
+        'CRAWL_METHODS': [],
+        'CODES_TO_RETRY': [430, 503, 500, 429, -1],
+        'CURL_INSECURE': True,
+        'MAX_PAGES_POR_DOMAIN': 50,
+        'ENGINES': ['httpx', 'curl']
+    }
+
+    df = pd.read_csv('t.csv')
+    doms = df['dom_tld'].tolist()
+
+    ISpider(domains=doms, stage="crawl", **config_overrides).run()
+    ISpider(domains=doms, stage="spider", **config_overrides).run()
