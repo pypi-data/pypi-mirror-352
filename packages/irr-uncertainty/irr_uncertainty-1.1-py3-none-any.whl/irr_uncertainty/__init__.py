fb3a7ed076a5f58c33f705c3ec9c255767e94d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:27 +0000

    Add package irr-uncertainty/1.1/irr_uncertainty-1.1-py3-none-any.whl

---
+++ packages/irr-uncertainty/irr_uncertainty-1.1-py3-none-any.whl/irr_uncertainty/__init__.py
@@ -0,0 +1,5 @@
+# Created by A. MATHIEU at 06/02/2025
+__all__ = ["data", "models", "utils", "pv_uncertainty_2024_article"]
+
+from .data import *
+from .models import *
---
+++ packages/irr-uncertainty/irr_uncertainty-1.1-py3-none-any.whl/irr_uncertainty/data/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["irr_data", "solar_data"]
