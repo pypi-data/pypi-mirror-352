98fc88b73d8e9067562358de511e79f4e4e6d25b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:16 +0000

    Add package splunk-add-on-ucc-framework/5.66.0/splunk_add_on_ucc_framework-5.66.0.tar.gz

---
+++ packages/splunk-add-on-ucc-framework/splunk_add_on_ucc_framework-5.66.0.tar.gz/splunk_add_on_ucc_framework-5.66.0/splunk_add_on_ucc_framework/package/appserver/static/js/build/Menu.rEOhSnnd.js
@@ -0,0 +1,2 @@
+import{k as r,a_ as o}from"./entry_page.js";import{a5 as a}from"./SearchJob.BI5Gcps7.js";var e=o();const n=r(e);var s=a();const u=r(s);export{n as D,u as M};
+//# sourceMappingURL=Menu.rEOhSnnd.js.map
