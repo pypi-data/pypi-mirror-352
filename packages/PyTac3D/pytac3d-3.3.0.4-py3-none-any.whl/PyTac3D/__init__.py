f70c007b220c3ce0b806aa10812876e7243c5980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:22 +0000

    Add package PyTac3D/3.3.0.4/pytac3d-3.3.0.4-py3-none-any.whl

---
+++ packages/PyTac3D/pytac3d-3.3.0.4-py3-none-any.whl/PyTac3D/__init__.py
@@ -0,0 +1,19 @@
+
+from .PyTac3D_Sensor import Sensor
+from .PyTac3D_Displayer import Displayer, SensorView
+from .PyTac3D_Manager import Manager
+from .PyTac3D_Analyzer import Analyzer
+from .PyTac3D_Data import DataLoader, DataRecorder
+from . import PyTac3D_Presets as Presets
+
+__all__ = ['Sensor',
+           'Displayer',
+           'SensorView',
+           'Manager',
+           'Analyzer',
+           'DataLoader',
+           'DataRecorder',
+           'Presets',
+           ]
+
+
