0ffcdd2e6d0e985216e1a7af0500f41845c9fc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:46 +0000

    Add package pyneurorg/0.1.87/pyneurorg-0.1.87-py3-none-any.whl

---
+++ packages/pyneurorg/pyneurorg-0.1.87-py3-none-any.whl/pyneurorg/organoid/organoid.py
@@ -0,0 +1,4 @@
+# Store pyneurorg metadata for logging
+syn._pyneurorg_model_name = model_name
+syn._pyneurorg_model_params = original_model_params
+syn._pyneurorg_connect_rule = connect_rule_description_str
