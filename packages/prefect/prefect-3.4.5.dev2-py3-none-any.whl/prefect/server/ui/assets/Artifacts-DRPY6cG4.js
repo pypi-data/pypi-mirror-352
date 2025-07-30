188802672411e18aa1cc2bc9bba2e0adc2ef3d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:29 +0000

    Add package prefect/3.4.5.dev2/prefect-3.4.5.dev2-py3-none-any.whl

---
+++ packages/prefect/prefect-3.4.5.dev2-py3-none-any.whl/prefect/server/ui/assets/Artifacts-DRPY6cG4.js
@@ -0,0 +1,2 @@
+import{d as o,i as r,c,o as n,j as a,k as e,m as t,aP as _,aQ as f}from"./index-BpsoK7wD.js";import{u as p}from"./usePageTitle-CNd-zINo.js";const x=o({__name:"Artifacts",setup(u){return p("Artifacts"),(l,i)=>{const s=r("p-layout-default");return n(),c(s,{class:"artifacts"},{header:a(()=>[e(t(f))]),default:a(()=>[e(t(_))]),_:1})}}});export{x as default};
+//# sourceMappingURL=Artifacts-DRPY6cG4.js.map
---
+++ packages/prefect/prefect-3.4.5.dev2-py3-none-any.whl/prefect/server/ui/assets/useCan-B8mWOkqd.js
@@ -0,0 +1,2 @@
+import{cH as n}from"./index-BpsoK7wD.js";const c=Symbol("canInjectionKey");function a(){return n(c)}export{c,a as u};
+//# sourceMappingURL=useCan-B8mWOkqd.js.map
