ab5fc43b78472c02c5dead7c6144997ec2f826bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:52 +0000

    Add package aa-fleetfinder/2.4.0/aa_fleetfinder-2.4.0-py3-none-any.whl

---
+++ packages/aa-fleetfinder/aa_fleetfinder-2.4.0-py3-none-any.whl/fleetfinder/__init__.py
@@ -0,0 +1,10 @@
+"""
+Initialize the app
+"""
+
+# Django
+from django.utils.translation import gettext_lazy as _
+
+__version__ = "2.4.0"
+__title__ = _("Fleet Finder")
+__verbose_name__ = "Fleet Finder for Alliance Auth"
