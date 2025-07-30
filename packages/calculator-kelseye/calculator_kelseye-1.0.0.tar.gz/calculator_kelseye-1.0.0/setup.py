6ad4b5a5cd12cbd271b4dfd8fa215d6a21585764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:05 +0000

    Add package calculator-kelseye/1.0.0/calculator_kelseye-1.0.0.tar.gz

---
+++ packages/calculator-kelseye/calculator_kelseye-1.0.0.tar.gz/calculator_kelseye-1.0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="calculator_kelseye",
+    description="a simple caluculator MCP",
+    long_description_content_type="text/markdown",
+    version="1.0.0",
+    author="kelseye",
+    author_email="",
+    url="",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
