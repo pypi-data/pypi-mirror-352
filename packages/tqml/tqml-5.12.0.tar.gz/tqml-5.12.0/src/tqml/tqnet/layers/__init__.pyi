b9d544ba17b3ad9067d087aa8870e76251685461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:03 +0000

    Add package tqml/5.12.0/tqml-5.12.0.tar.gz

---
+++ packages/tqml/tqml-5.12.0.tar.gz/tqml-5.12.0/src/tqml/tqnet/layers/__init__.pyi
@@ -0,0 +1,14 @@
+from .cphn import CPHN as CPHN
+from .cq import CQ as CQ
+from .dense import Dense as Dense
+from .dhn import DHN as DHN
+from .efq import EFQ as EFQ
+from .hlstm import HLSTM as HLSTM
+from .phn import PHN as PHN
+from .pqn import PQN as PQN
+from .qdi import QDI as QDI
+from .qlstm import QLSTM as QLSTM
+from .reuploading_layer import ReuploadingQuantumLayer as ReuploadingQuantumLayer
+from .vq import VQ as VQ
+
+__all__ = ['PQN', 'VQ', 'QDI', 'EFQ', 'CPHN', 'PHN', 'CQ', 'QLSTM', 'HLSTM', 'DHN', 'Dense', 'ReuploadingQuantumLayer']
