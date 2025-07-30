ce69d46b6a9571b4cd6f5ad64e48d91e8c326593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:55 +0000

    Add package hwmcp_test/0.1.0/hwmcp_test-0.1.0.tar.gz

---
+++ packages/hwmcp_test/hwmcp_test-0.1.0.tar.gz/hwmcp_test-0.1.0/src/hwmcp_test/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello, hwmcp_test!")
+
+if __name__ == "__main__":
+    main()
---
+++ packages/hwmcp_test/hwmcp_test-0.1.0.tar.gz/hwmcp_test-0.1.0/test/test_pypi.py
@@ -0,0 +1,7 @@
+import hwmcp_test
+
+def test_hwmcp_test():
+    assert hwmcp_test.add(1, 2) == 3
+
+if __name__ == "__main__":
+    test_hwmcp_test()
