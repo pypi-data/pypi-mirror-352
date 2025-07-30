69b0a522758dd5d736172b9e4c39c7886b581f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:46 +0000

    Add package trustpoint-core/0.2.3/trustpoint_core-0.2.3-py3-none-any.whl

---
+++ packages/trustpoint-core/trustpoint_core-0.2.3-py3-none-any.whl/trustpoint_core/crypto_types.py
@@ -0,0 +1,11 @@
+"""This module defines types used for TYPE_CHECKING."""
+
+from __future__ import annotations
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec, ed448, ed25519, rsa
+
+PublicKey = rsa.RSAPublicKey | ec.EllipticCurvePublicKey | ed25519.Ed25519PublicKey | ed448.Ed448PublicKey
+PrivateKey = rsa.RSAPrivateKey | ec.EllipticCurvePrivateKey | ed25519.Ed25519PrivateKey | ed448.Ed448PrivateKey
+AllowedCertSignHashAlgos = hashes.SHA224 | hashes.SHA256 | hashes.SHA384 | hashes.SHA512 | hashes.SHA3_224 | hashes.SHA3_256 | hashes.SHA3_384 | hashes.SHA3_512,
+
