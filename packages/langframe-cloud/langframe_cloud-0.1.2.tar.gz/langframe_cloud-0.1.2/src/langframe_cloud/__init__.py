b55ff2c39706ba9497da362f43c9dd7aef37bba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:01 +0000

    Add package langframe-cloud/0.1.2/langframe_cloud-0.1.2.tar.gz

---
+++ packages/langframe-cloud/langframe_cloud-0.1.2.tar.gz/langframe_cloud-0.1.2/src/langframe_cloud/__init__.py
@@ -0,0 +1,6 @@
+# Marker
+import langframe_cloud.auth_client as auth_client
+import langframe_cloud.hasura_client as hasura_client
+import langframe_cloud.protos as protos
+
+__all__ = ["hasura_client", "auth_client", "protos"]
