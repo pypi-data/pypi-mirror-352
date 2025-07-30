560a51ab7c5f3bf852603498cac218cd59019e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:59 +0000

    Add package nacos-ai-sdk-python/0.0.5/nacos_ai_sdk_python-0.0.5.tar.gz

---
+++ packages/nacos-ai-sdk-python/nacos_ai_sdk_python-0.0.5.tar.gz/nacos_ai_sdk_python-0.0.5/test/test.py
@@ -0,0 +1,15 @@
+from nacos_ai.common.ai_client_config import AIClientConfig
+from nacos_ai.common.ai_client_config_builder import AIClientConfigBuilder
+from nacos_ai.mcp.nacos_mcp_service import NacosMcpService
+
+
+async def main():
+	ai_client_config = AIClientConfigBuilder().server_address("11.161.207.61:8848").username('nacos').password('nacosopensource').build()
+	mcp_service = await NacosMcpService.create_mcp_service(ai_client_config)
+	a=await mcp_service.get_mcp_server_detail("public", "amap-mcp-server", "0.1.1")
+	print(a)
+	return await mcp_service.list_mcp_servers("public", "", 1, 10)
+
+if __name__ == '__main__':
+	import asyncio
+	print(asyncio.run(main()))
