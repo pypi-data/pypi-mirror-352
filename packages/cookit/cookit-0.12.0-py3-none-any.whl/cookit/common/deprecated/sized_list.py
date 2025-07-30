70e073c160087bcde86804a6ba003b9694f1c0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:22 +0000

    Add package cookit/0.12.0/cookit-0.12.0-py3-none-any.whl

---
+++ packages/cookit/cookit-0.12.0-py3-none-any.whl/cookit/common/deprecated/sized_list.py
@@ -0,0 +1,58 @@
+from collections.abc import Iterable
+from typing import Generic, Optional, SupportsIndex, TypeVar, Union, overload
+from typing_extensions import Self, deprecated
+
+T = TypeVar("T")
+TB = TypeVar("TB")
+
+
+@deprecated("Use `collections.deque` instead")
+class SizedList(list[T], Generic[T]):
+    def __init__(
+        self,
+        iterable: Optional[Iterable[T]] = None,
+        size: Optional[int] = None,
+    ) -> None:
+        if iterable is None:
+            super().__init__()
+        else:
+            super().__init__(iterable)
+        self.size = size
+        self._handle_overflow()
+
+    @property
+    def last(self) -> Optional[T]:
+        if self:
+            return self[-1]
+        return None
+
+    def _handle_overflow(self) -> None:
+        if self.size is None:
+            return
+        while len(self) > self.size:
+            self.pop(0)
+
+    def append(self, item: T) -> None:
+        super().append(item)
+        self._handle_overflow()
+
+    def extend(self, items: Iterable[T]) -> None:
+        super().extend(items)
+        self._handle_overflow()
+
+    def insert(self, index: SupportsIndex, item: T) -> None:
+        super().insert(index, item)
+        self._handle_overflow()
+
+    @overload
+    def __add__(self, items: Iterable[T]) -> "SizedList[T]": ...
+
+    @overload
+    def __add__(self, items: Iterable[TB]) -> "SizedList[Union[T, TB]]": ...
+
+    def __add__(self, items):
+        return SizedList((*self, *items), size=self.size)
+
+    def __iadd__(self, items: Iterable[T]) -> Self:
+        self.extend(items)
+        return self
