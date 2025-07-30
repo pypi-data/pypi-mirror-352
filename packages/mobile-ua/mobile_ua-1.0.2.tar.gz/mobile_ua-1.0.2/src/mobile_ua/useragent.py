93304cdde1285c46e940ea3c7b81e89286c37f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:08:21 +0000

    Add package mobile-ua/1.0.2/mobile_ua-1.0.2.tar.gz

---
+++ packages/mobile-ua/mobile_ua-1.0.2.tar.gz/mobile_ua-1.0.2/src/mobile_ua/useragent.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+import numpy as np
+current_path = os.path.dirname(os.path.abspath(__file__))
+ua_array = pd.read_pickle(current_path + "\\UA.pkl")
+def random_ua():
+    """
+    获取随机的ua
+    """
+    ua_ls = np.random.choice(ua_array,1)
+    return ua_ls[0]
+
+def get_ua(index=0):
+    """
+    获取指定索引的ua
+    """
+    ua_ls = np.random.choice(ua_array, index)
+    return ua_ls[0]
+
+if __name__ == '__main__':
+    ua = random_ua()
+    print(ua)
