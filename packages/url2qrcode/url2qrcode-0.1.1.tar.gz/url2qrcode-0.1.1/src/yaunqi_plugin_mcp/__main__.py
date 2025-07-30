41fbc6f02880cc88aa82d0b5aa731265a3673f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:28 +0000

    Add package url2qrcode/0.1.1/url2qrcode-0.1.1.tar.gz

---
+++ packages/url2qrcode/url2qrcode-0.1.1.tar.gz/url2qrcode-0.1.1/src/yaunqi_plugin_mcp/__main__.py
@@ -0,0 +1,26 @@
+from . import server
+# import server
+import asyncio
+import os
+
+
+def main():
+    """Main entry point for the package."""
+    asyncio.run(server.main())
+
+
+# Optionally expose other important items at package level
+main()
+# os.environ["API_KEY"] = "HyENsa3tNVoIAb0HBLJTcVSAb2fdsN7l"
+# os.environ["ASSISTANT_ID"] = "ay9y3OW4Pjkddd"
+# os.environ["TOOL_DESC"] = "chat with yuanqi agent"
+# os.environ["TOOL_NAME"] = "chat"
+# arguments = {
+#     "userID": "123",
+#     "userPrompt":"你是谁"
+# }
+# server.yuanqi_chat(arguments)
+#arguments = {
+#    "url": "https://www.baidu.com"
+#}
+#print(server.url2qrcode(arguments))
