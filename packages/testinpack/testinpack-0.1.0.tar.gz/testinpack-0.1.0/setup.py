555ca121e37323da418bb6972d0ddecb427f0537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:07:56 +0000

    Add package testinpack/0.1.0/testinpack-0.1.0.tar.gz

---
+++ packages/testinpack/testinpack-0.1.0.tar.gz/testinpack-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testinpack',
+    version='0.1.0',
+    #author='TESTNAME',
+    #author_email='you@example.com',
+    #description='A short description of your package',
+    #long_description=open('README.md').read(),
+    #long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[],  # list your dependencies here
+    #classifiers=[
+    #    'Programming Language :: Python :: 3',
+    #    'License :: OSI Approved :: MIT License',
+    #],
+    #python_requires='>=3.6',
+)
