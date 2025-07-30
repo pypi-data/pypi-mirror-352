66c9bdb2741e353399e871a3c6e8113abf6d3100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:57 +0000

    Add package sorcha/1.0.0/sorcha-1.0.0-py3-none-any.whl

---
+++ packages/sorcha/sorcha-1.0.0-py3-none-any.whl/sorcha/ephemeris/simulation_data_files.py
@@ -0,0 +1,31 @@
+import pooch
+
+
+def make_retriever(auxconfigs, directory_path: str = None) -> pooch.Pooch:
+    """Helper function that will create a Pooch object to track and retrieve files.
+
+    Parameters
+    ----------
+    directory_path : string, optional
+        The base directory to place all downloaded files. Default = None
+    registry : dictionary, optional
+        A dictionary of file names to SHA hashes. Generally we'll not use SHA=None
+        because the files we're tracking change frequently. Default = REGISTRY
+    auxconfigs: dataclass
+        Dataclass of auxiliary configuration file arguments.
+    Returns
+    -------
+    : pooch
+        The instance of a Pooch object used to track and retrieve files.
+    """
+    dir_path = pooch.os_cache("sorcha")
+    if directory_path:
+        dir_path = directory_path
+
+    return pooch.create(
+        path=dir_path,
+        base_url="",
+        urls=auxconfigs.urls,
+        registry=auxconfigs.registry,
+        retry_if_failed=25,
+    )
