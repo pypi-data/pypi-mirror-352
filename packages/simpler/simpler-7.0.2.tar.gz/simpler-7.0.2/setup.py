3cfdb08fbfe5e0a3d911bb16e0e109312645c526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:28 +0000

    Add package simpler/7.0.2/simpler-7.0.2.tar.gz

---
+++ packages/simpler/simpler-7.0.2.tar.gz/simpler-7.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from sys import version_info
+
+assert version_info.major == 3, 'Python 3 required'
+
+with open('README.md', 'r', encoding='utf-8') as fp:
+	long_description = fp.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as fp:
+	requirements = fp.read().strip().split()
+
+with open('simpler/_version.py', 'r', encoding='utf-8') as fp:
+	version = fp.read().split(' = ')[1][1:-1]
+
+setup(
+	name='simpler',
+	version=version,
+	author='Juan C. Roldán',
+	author_email='juancarlos@sevilla.es',
+	description='Makes Python simpler.',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/juancroldan/simpler',
+	license='MIT',
+	packages=find_packages(),
+	install_requires=requirements,
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3',
+	],
+)
---
+++ packages/simpler/simpler-7.0.2.tar.gz/simpler-7.0.2/simpler/terminal.py
@@ -0,0 +1,35 @@
+def getch():
+	''' Reads a single byte from the user input. '''
+	from os import name
+	if name == 'nt':  # Windows-based systems
+		from msvcrt import getch as ms_getch
+		res = ms_getch().decode('utf-8')
+	else:  # POSIX-based systems
+		from tty import setcbreak
+		from sys import stdin
+		from termios import tcgetattr, tcsetattr, TCSADRAIN
+		fd = stdin.fileno()
+		oldSettings = tcgetattr(fd)
+		try:
+			setcbreak(fd)
+			res = stdin.read(1)
+		finally:
+			tcsetattr(fd, TCSADRAIN, oldSettings)
+	return res
+
+_cprint_palette = {
+	'default': 39, 'black': 30, 'red': 31, 'green': 32, 'yellow': 33, 'blue': 34,
+	'magenta': 35, 'cyan': 36, 'light_gray': 37, 'dark_gray': 90, 'light_red': 91,
+	'light_green': 92, 'light_yellow': 93, 'light_blue': 94, 'light_magenta': 95,
+	'light_cyan': 96, 'white': 97
+}
+def cprint(*args, fg='default', bg='default', **kwargs):
+	''' Same syntax as print, with two optional parameters `fg` and `bg` to change
+	the print color. Available colors are: default, black, red, green, yellow, blue,
+	magenta, cyan, light_gray, dark_gray, light_red, light_green, light_yellow, light_blue,
+	light_magenta, light_cyan, and white. '''
+	print('\033[%dm\033[%dm%s\033[0m' % (
+		_cprint_palette[fg],
+		_cprint_palette[bg] + 10,
+		' '.join(map(str, args)),
+	), **kwargs)
