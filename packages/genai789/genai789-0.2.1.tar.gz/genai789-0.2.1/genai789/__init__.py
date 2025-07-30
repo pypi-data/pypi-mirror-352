8de6ee2adde5736badc94114e2b2d1695170382b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:32 +0000

    Add package genai789/0.2.1/genai789-0.2.1.tar.gz

---
+++ packages/genai789/genai789-0.2.1.tar.gz/genai789-0.2.1/genai789/__init__.py
@@ -0,0 +1,12 @@
+from .p1 import p1
+from .p2 import p2
+from .p3 import p3
+from .p4 import p4
+from .p5 import p5
+from .p6 import p6
+from .p7 import p7
+from .p8 import p8
+from .p9 import p9
+from .p10 import p10
+
+__all__ = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10']
