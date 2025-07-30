5a25926aa0d93e4fa64313212e36ef5470123bfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:32 +0000

    Add package pybioclip/1.3.3/pybioclip-1.3.3.tar.gz

---
+++ packages/pybioclip/pybioclip-1.3.3.tar.gz/pybioclip-1.3.3/src/bioclip/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024-present John Bradley <johnbradley2008@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from bioclip.predict import TreeOfLifeClassifier, Rank, CustomLabelsClassifier, CustomLabelsBinningClassifier
+
+__all__ = ["TreeOfLifeClassifier", "Rank", "CustomLabelsClassifier", "CustomLabelsBinningClassifier"]
