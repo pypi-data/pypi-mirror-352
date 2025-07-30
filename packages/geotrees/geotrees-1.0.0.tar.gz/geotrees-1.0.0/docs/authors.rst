ee694480cb39eb9494e3b5cb8d8cd6a18156bbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:20 +0000

    Add package geotrees/1.0.0/geotrees-1.0.0.tar.gz

---
+++ packages/geotrees/geotrees-1.0.0.tar.gz/geotrees-1.0.0/docs/authors.rst
@@ -0,0 +1,12 @@
+Credits
+=======
+
+Development Lead
+----------------
+
+* Joseph T. Siddons <josidd@noc.ac.uk> `@josidd <git.noc.ac.uk/josidd>`_
+
+Contributoring Developers
+-------------------------
+
+* Richard C. Cornes <rcornes@noc.ac.uk> `@ricorne <git.noc.ac.uk/ricorne>`_
---
+++ packages/geotrees/geotrees-1.0.0.tar.gz/geotrees-1.0.0/geotrees/__init__.py
@@ -0,0 +1,32 @@
+"""Tools for fast neighbour look-up on the Earth's surface"""
+
+from geotrees.distance_metrics import haversine
+from geotrees.great_circle import GreatCircle
+from geotrees.kdtree import KDTree
+from geotrees.neighbours import find_nearest
+from geotrees.quadtree import QuadTree
+from geotrees.record import Record, SpaceTimeRecord
+from geotrees.shape import (
+    Ellipse,
+    Rectangle,
+    SpaceTimeEllipse,
+    SpaceTimeRectangle,
+)
+
+
+__all__ = [
+    "Ellipse",
+    "GreatCircle",
+    "KDTree",
+    "QuadTree",
+    "Record",
+    "Rectangle",
+    "SpaceTimeEllipse",
+    "SpaceTimeRecord",
+    "SpaceTimeRectangle",
+    "find_nearest",
+    "haversine",
+]
+
+
+__version__ = "1.0.0"
