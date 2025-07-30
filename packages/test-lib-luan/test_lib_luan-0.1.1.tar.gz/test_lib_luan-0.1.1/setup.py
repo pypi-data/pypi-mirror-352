6c9121d1f4fd2e4e48982d75039c8eb6706267f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:52 +0000

    Add package test-lib-luan/0.1.1/test_lib_luan-0.1.1.tar.gz

---
+++ packages/test-lib-luan/test_lib_luan-0.1.1.tar.gz/test_lib_luan-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='test-lib-luan',
+    version='0.1.1',
+    packages=find_packages(),
+    author='Luan',
+    author_email='',
+    description='Đây là thư viện test',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    python_requirement='>=3.6'
+)
