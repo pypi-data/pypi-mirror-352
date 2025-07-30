8566b3964db1efce4d4a72a0e40c96237879a0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:06 +0000

    Add package torch-tensorrt/2.6.1/torch_tensorrt-2.6.1-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_34_x86_64.whl

---
+++ packages/torch-tensorrt/torch_tensorrt-2.6.1-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_34_x86_64.whl/torch_tensorrt/dynamo/lowering/__init__.py
@@ -0,0 +1,6 @@
+from ._decomposition_groups import (
+    torch_disabled_decompositions,
+    torch_enabled_decompositions,
+)
+from ._decompositions import get_decompositions  # noqa: F401
+from .passes import *
