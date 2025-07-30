e019357c680909863e51972904f7f4ed2a2ff00a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:39 +0000

    Add package django-users-auth/2.4.16/django_users_auth-2.4.16.tar.gz

---
+++ packages/django-users-auth/django_users_auth-2.4.16.tar.gz/django_users_auth-2.4.16/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-users-auth",
+    version="2.4.16",
+    packages=find_packages(include=["users", "users.*", "base", "base.*", "imagenes", "imagenes.*"]),
+    install_requires=[
+        "django>=4.2",
+    ],
+    include_package_data=True,
+    license="MIT",
+    description="Django authentication app with custom user model",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Martin",
+    author_email="vazquezmartin1240@gmail.com",
+    url="https://github.com/Vazquez1240/authentication-django",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+    ],
+)
