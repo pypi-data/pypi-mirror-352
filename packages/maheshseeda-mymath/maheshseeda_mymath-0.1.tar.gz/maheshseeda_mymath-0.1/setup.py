5805d0101bcdf76d3c29af397f86bf5c7f1fd03f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:30 +0000

    Add package maheshseeda-mymath/0.1/maheshseeda_mymath-0.1.tar.gz

---
+++ packages/maheshseeda-mymath/maheshseeda_mymath-0.1.tar.gz/maheshseeda_mymath-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='maheshseeda-mymath',  # changed to something unique
+    version='0.1',
+    packages=find_packages(),
+    description='Simple math library with add and multiply functions',
+    author='mahesh',
+)
