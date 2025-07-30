347a4758a3febbb222d871a3d58d542233cc7f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:26 +0000

    Add package ability-hand/0.1.2/ability_hand-0.1.2.tar.gz

---
+++ packages/ability-hand/ability_hand-0.1.2.tar.gz/ability_hand-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ability_hand",
+    version="0.1.2",
+    packages=find_packages(
+        include=["ah_wrapper", "ah_examples", "ah_plotting"]
+    ),
+    install_requires=["pyserial>=3.5", "matplotlib>=3.6.3,<=3.10.0"],
+    entry_points={
+        "console_scripts": [
+            "hand_wave=ah_examples.hand_wave:main",
+            "plot_motors=ah_examples.plot_motors:main",
+            "plot_touch_sensors=ah_examples.plot_touch_sensors:main",
+            "validate_hand=ah_examples.validate_hand:main",
+        ]
+    },
+    author="Justin Francis",
+    author_email="jfrancis@psyonic.io",
+    description="Python wrapper for Ability Hand API",
+    url="https://github.com/psyonicinc/ability-hand-api",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
