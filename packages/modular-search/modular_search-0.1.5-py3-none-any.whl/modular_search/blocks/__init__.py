355ec65a818e42b91d1723be78766201549810e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:41 +0000

    Add package modular-search/0.1.5/modular_search-0.1.5-py3-none-any.whl

---
+++ packages/modular-search/modular_search-0.1.5-py3-none-any.whl/modular_search/blocks/__init__.py
@@ -0,0 +1,8 @@
+from modular_search.blocks.core import UnitSearchBlock
+from modular_search.blocks.codebase import CodebaseSearchBlock, CodebaseSearchResult
+
+
+__all__ = [
+    "UnitSearchBlock",
+    "CodebaseSearchBlock", "CodebaseSearchResult"
+]
---
+++ packages/modular-search/modular_search-0.1.5-py3-none-any.whl/modular_search/engines/__init__.py
@@ -0,0 +1,31 @@
+from modular_search.engines.core import SearchEngine
+from modular_search.engines.google import GoogleSearchEngine
+from modular_search.engines.deep_google import DeepGoogleSearchEngine
+
+
+__all__ = [
+    "SearchEngine",
+    "GoogleSearchEngine",
+    "DeepGoogleSearchEngine",
+    "SearchEngineFactory",
+]
+
+class SearchEngineFactory:
+    """
+    Factory class to create instances of SearchEngine.
+    """
+
+    @staticmethod
+    def load(engine_type: str) -> SearchEngine:
+        if engine_type == "google":
+            return GoogleSearchEngine()
+        elif engine_type == "deep_google":
+            return DeepGoogleSearchEngine()
+        else:
+            raise ValueError(f"Unknown search engine type: {engine_type}")
+    
+    def __class_getitem__(cls, engine_type: str) -> SearchEngine:
+        """
+        Factory method to get an instance of a search engine based on the type.
+        """
+        return cls.load(engine_type)
