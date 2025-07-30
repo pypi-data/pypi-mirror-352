742bf988aa126ce1c7254561e0e2dea4a9151139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:52 +0000

    Add package tmep/3.0.1/tmep-3.0.1-py3-none-any.whl

---
+++ packages/tmep/tmep-3.0.1-py3-none-any.whl/tmep/format.py
@@ -0,0 +1,77 @@
+"""A collection of string format functions derived from the class “Functions()”"""
+
+from __future__ import annotations
+
+from tmep.functions import DefaultTemplateFunctions
+
+f = DefaultTemplateFunctions()
+
+alpha = f.fn_alpha
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_alpha`"""
+
+alphanum = f.fn_alphanum
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_alphanum`"""
+
+
+asciify = f.fn_asciify
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_asciify`"""
+
+
+delchars = f.fn_delchars
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_delchars`"""
+
+
+deldupchars = f.fn_deldupchars
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_deldupchars`"""
+
+
+first = f.fn_first
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_first`"""
+
+
+initial = f.fn_initial
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_initial`"""
+
+
+left = f.fn_left
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_left`"""
+
+
+lower = f.fn_lower
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_lower`"""
+
+
+nowhitespace = f.fn_nowhitespace
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_nowhitespace`"""
+
+
+num = f.fn_num
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_num`"""
+
+
+replchars = f.fn_replchars
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_replchars`"""
+
+
+right = f.fn_right
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_right`"""
+
+
+sanitize = f.fn_sanitize
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_sanitize`"""
+
+
+shorten = f.fn_shorten
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_shorten`"""
+
+
+time = f.fn_time
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_time`"""
+
+
+title = f.fn_title
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_title`"""
+
+
+upper = f.fn_upper
+""":see: :meth:`tmep.functions.DefaultTemplateFunctions.fn_upper`"""
