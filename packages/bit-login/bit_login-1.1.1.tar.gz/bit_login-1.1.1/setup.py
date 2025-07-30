15c1ce947332ca2469f297d112860b16f99a0771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:20 +0000

    Add package bit-login/1.1.1/bit_login-1.1.1.tar.gz

---
+++ packages/bit-login/bit_login-1.1.1.tar.gz/bit_login-1.1.1/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# 从 __init__.py 读取版本号
+def get_version():
+    with open("bit_login/__init__.py", "r", encoding="utf-8") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return line.split("=")[1].strip().strip('"').strip("'")
+
+setuptools.setup(
+    name="bit_login", 
+    version=get_version(),
+    author="teclab",   
+    author_email="admin@teclab.org.cn",
+    description="北京理工大学统一身份验证登录模块",
+    keywords="BIT, BITCAS, BITLogin, BITWebVPN, BITSSO, BITSSOLogin, BITSSOClient",
+    long_description=long_description,   
+    long_description_content_type="text/markdown",
+    url="https://github.com/yht0511/bit-login",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests>=2.25.0",
+        "beautifulsoup4>=4.9.0", 
+        "pycryptodome>=3.15.0",
+        "lxml>=4.6.0",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9", 
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    project_urls={
+        "Bug Reports": "https://github.com/yht0511/bit-login/issues",
+        "Source": "https://github.com/yht0511/bit-login",
+    },
+)
