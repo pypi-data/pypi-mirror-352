854f78c94c638f39807f9f9cf69f5555e904986d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:15 +0000

    Add package safeguard-toolkit/0.1.1/safeguard_toolkit-0.1.1.tar.gz

---
+++ packages/safeguard-toolkit/safeguard_toolkit-0.1.1.tar.gz/safeguard_toolkit-0.1.1/examples/eval_exec_scanner/test_eval_exec_scanner.py
@@ -0,0 +1,13 @@
+import os
+from safeguard.core.eval_exec_scanner import EvalExecScanner
+
+def test_eval_exec_scanner():
+    base_path = "./examples/eval_exec_scanner"
+    scanner = EvalExecScanner()  
+    issues = scanner.scan(base_path)
+    
+    for issue in issues:
+        print(issue)
+
+if __name__ == "__main__":
+    test_eval_exec_scanner()
---
+++ packages/safeguard-toolkit/safeguard_toolkit-0.1.1.tar.gz/safeguard_toolkit-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="safeguard-toolkit",
+    version="0.1.1",
+    description="A project for scanning secrets, configs, dependencies, and permissions",
+    author="Your Name",
+    packages=find_packages(),  
+    install_requires=[
+        "pyyaml==6.0.2",
+        "requests==2.32.3",
+        "packaging==25.0",
+        "toml==0.10.2",
+        "packaging==25.0"
+    ],
+    python_requires=">=3.12",
+)
