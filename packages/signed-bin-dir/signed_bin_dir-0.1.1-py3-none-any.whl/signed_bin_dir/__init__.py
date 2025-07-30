bad928a6448fa16b2c05deba576ebd8708c1bc8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:59 +0000

    Add package signed-bin-dir/0.1.1/signed_bin_dir-0.1.1-py3-none-any.whl

---
+++ packages/signed-bin-dir/signed_bin_dir-0.1.1-py3-none-any.whl/signed_bin_dir/__init__.py
@@ -0,0 +1,5 @@
+"""Signed Binary Directory - Secure tool signing system for PATH management."""
+
+__version__ = "0.1.0"
+__author__ = "Isaac Harrison Gutekunst"
+__email__ = "isaac@supercortex.io" 
