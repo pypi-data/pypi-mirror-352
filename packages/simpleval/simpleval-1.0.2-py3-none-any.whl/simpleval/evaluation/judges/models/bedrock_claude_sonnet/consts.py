468d9e0ea8ed8de7fe529ebb92ef935ff34bf115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:54 +0000

    Add package simpleval/1.0.2/simpleval-1.0.2-py3-none-any.whl

---
+++ packages/simpleval/simpleval-1.0.2-py3-none-any.whl/simpleval/evaluation/judges/models/bedrock_claude_sonnet/consts.py
@@ -0,0 +1,5 @@
+AWS_REGION_PLACEHOLDER = '{AWS_REGION}'
+
+SONNET35_V1_MODEL_ID = 'anthropic.claude-3-5-sonnet-20240620-v1:0'
+SONNET35_V2_MODEL_ID = f'arn:aws:bedrock:{AWS_REGION_PLACEHOLDER}::inference-profile/us.anthropic.claude-3-5-sonnet-20241022-v2:0'
+SONNET37_V1_MODEL_ID = f'arn:aws:bedrock:{AWS_REGION_PLACEHOLDER}::inference-profile/us.anthropic.claude-3-7-sonnet-20250219-v1:0'
