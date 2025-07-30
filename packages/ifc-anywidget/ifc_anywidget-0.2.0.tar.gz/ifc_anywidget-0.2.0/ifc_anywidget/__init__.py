1d2e2ba627fe55b6bc7971cbb63a2c9dc32e2d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:32 +0000

    Add package ifc-anywidget/0.2.0/ifc_anywidget-0.2.0.tar.gz

---
+++ packages/ifc-anywidget/ifc_anywidget-0.2.0.tar.gz/ifc_anywidget-0.2.0/ifc_anywidget/__init__.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import anywidget
+import traitlets
+import ifcopenshell
+
+
+__version__ = "0.2.0"
+
+
+def ifc_to_json(ifc_model: ifcopenshell.file, widget: anywidget.AnyWidget):
+    return {
+        "contents": ifc_model.to_string(),
+    }
+
+
+class IfcViewer(anywidget.AnyWidget):
+    _esm = Path(__file__).with_name("index.js")
+    _css = Path(__file__).with_name("index.css")
+    ifc_model = traitlets.Instance(ifcopenshell.file).tag(sync=True, to_json=ifc_to_json)
+    selected_guids = traitlets.List(trait=traitlets.Unicode(), allow_none=True).tag(sync=True)
