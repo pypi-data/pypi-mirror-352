a130ff84af0823d8365901a23ffd916948ffcf48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:29 +0000

    Add package mozilla-repo-urls/0.2.2/mozilla_repo_urls-0.2.2-py3-none-any.whl

---
+++ packages/mozilla-repo-urls/mozilla_repo_urls-0.2.2-py3-none-any.whl/mozilla_repo_urls/result.py
@@ -0,0 +1,48 @@
+import giturlparse
+
+_DOT_GIT_SUFFIX = ".git"
+
+
+class RepoUrlParsed(giturlparse.result.GitUrlParsed):
+    @property
+    def hgmo(self) -> bool:
+        return self.platform == "hgmo"
+
+    @property
+    def git_cinnabar(self) -> bool:
+        # https://github.com/glandium/git-cinnabar
+        return len(self.protocols) > 0 and self.protocols[0].startswith("hg::")
+
+    @property
+    def repo_name(self) -> str:
+        return self.repo_path.split("/")[-1]
+
+    @property
+    def repo_path(self) -> str:
+        repo_path = (
+            self.pathname[: -len(_DOT_GIT_SUFFIX)]
+            if self.pathname.endswith(_DOT_GIT_SUFFIX)
+            else self.pathname
+        )
+        return repo_path.strip("/")
+
+    @property
+    def repo_url(self) -> str:
+        if self.platform == "hgmo":
+            return f"https://hg.mozilla.org/{self.repo}"
+        else:
+            return f"https://github.com/{self.owner}/{self.repo}"
+
+    @property
+    def repo_type(self) -> str:
+        return "hg" if self.platform == "hgmo" and not self.git_cinnabar else "git"
+
+    @property
+    def taskcluster_role_prefix(self) -> str:
+        return f"repo:{self.host}/{self.repo_path}"
+
+    def __eq__(self, other):
+        if isinstance(other, RepoUrlParsed):
+            return self.normalized == other.normalized
+
+        return False
