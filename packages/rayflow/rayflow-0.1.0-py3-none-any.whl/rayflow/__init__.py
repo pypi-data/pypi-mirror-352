9b5887ab94be738bc32dbf8dab64d9c18df1ec19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:41 +0000

    Add package rayflow/0.1.0/rayflow-0.1.0-py3-none-any.whl

---
+++ packages/rayflow/rayflow-0.1.0-py3-none-any.whl/rayflow/__init__.py
@@ -0,0 +1,4 @@
+from .simpleagent import simpleAgent
+from .tool import Tool
+
+__version__ = "0.1.0"
---
+++ packages/rayflow/rayflow-0.1.0-py3-none-any.whl/rayflow/simpleagent.py
@@ -0,0 +1,8 @@
+class simpleAgent:
+    def __init__(self):
+        # Initialize simpleAgent
+        pass
+
+    def act(self):
+        # Add some basic functionality
+        return "Simple agent action"
