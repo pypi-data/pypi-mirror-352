0b9a118189ca510150877bfaaddfd1a97f04f6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:46 +0000

    Add package llama-index-storage-index-store-asimov/0.0.1/llama_index_storage_index_store_asimov-0.0.1.tar.gz

---
+++ packages/llama-index-storage-index-store-asimov/llama_index_storage_index_store_asimov-0.0.1.tar.gz/llama_index_storage_index_store_asimov-0.0.1/api.md
@@ -0,0 +1,5 @@
+---
+title: llama_index.storage.index_store.asimov
+---
+
+::: llama_index.storage.index_store.asimov
---
+++ packages/llama-index-storage-index-store-asimov/llama_index_storage_index_store_asimov-0.0.1.tar.gz/llama_index_storage_index_store_asimov-0.0.1/llama_index/storage/index_store/asimov/base.py
@@ -0,0 +1,6 @@
+# This is free and unencumbered software released into the public domain.
+
+from llama_index.core.storage.index_store.keyval_index_store import KVIndexStore
+
+class AsimovIndexStore(KVIndexStore):
+    """ASIMOV index store."""
