beafb28c7cf7b8f0644e7a89867a4b8482821378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:16 +0000

    Add package infrablocks.invoke_terraform/0.0.2a15/infrablocks_invoke_terraform-0.0.2a15-py3-none-any.whl

---
+++ packages/infrablocks.invoke_terraform/infrablocks_invoke_terraform-0.0.2a15-py3-none-any.whl/infrablocks/invoke_terraform/__init__.py
@@ -0,0 +1,16 @@
+from infrablocks.invoke_factory import parameter
+
+from .task_factory import (
+    Configuration,
+    PreTaskFunction,
+    TaskFactory,
+    parameters,
+)
+
+__all__ = [
+    "Configuration",
+    "PreTaskFunction",
+    "TaskFactory",
+    "parameter",
+    "parameters",
+]
