603b0babe777666ce3d07bc1a740ebe4e504bb2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:59 +0000

    Add package wetterdienst/0.109.0/wetterdienst-0.109.0.tar.gz

---
+++ packages/wetterdienst/wetterdienst-0.109.0.tar.gz/wetterdienst-0.109.0/.github/workflows/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+flavor=$1
+
+if [[ -z "${flavor}" ]]; then
+  echo "Missing option 'flavor', use e.g. 'testing' or 'docs'."
+  exit 1
+fi
+
+echo "Installing package and requirements for ${flavor}"
+
+set -e
+set -x
+
+if [ "${flavor}" = "testing" ]; then
+  uv pip install .[bufr,explorer,export,interpolation,plotting,radar,radarplus,restapi,sql]
+
+elif [ "${flavor}" = "docs" ]; then
+  uv pip install .[docs,interpolation]
+
+fi
---
+++ packages/wetterdienst/wetterdienst-0.109.0.tar.gz/wetterdienst-0.109.0/docs/library/core.md
@@ -0,0 +1,25 @@
+# Core
+
+## Core
+
+::: wetterdienst.core.timeseries
+    options:
+      heading_level: 3
+
+## Settings
+
+::: wetterdienst.settings
+    options:
+      heading_level: 3
+
+## Data models
+
+::: wetterdienst.util.geo
+    options:
+      heading_level: 3
+
+## Download
+
+::: wetterdienst.util.network
+    options:
+      heading_level: 3
