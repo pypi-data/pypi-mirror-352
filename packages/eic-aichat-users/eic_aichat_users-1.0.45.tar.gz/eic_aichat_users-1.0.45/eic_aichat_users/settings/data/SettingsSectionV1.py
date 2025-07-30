55598bdbc83967b4ba01209a6d8941fb187779cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:28 +0000

    Add package eic-aichat-users/1.0.45/eic_aichat_users-1.0.45.tar.gz

---
+++ packages/eic-aichat-users/eic_aichat_users-1.0.45.tar.gz/eic_aichat_users-1.0.45/eic_aichat_users/settings/data/SettingsSectionV1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from typing import Optional
+from pip_services4_data.data import IStringIdentifiable
+from pip_services4_components.config import ConfigParams
+
+
+class SettingsSectionV1(IStringIdentifiable):
+    def __init__(self, id: str = None, parameters: Optional[ConfigParams] = None):
+        self.id = id
+        self.parameters = ConfigParams.from_value(parameters or {})
+        self.update_time = datetime.now()
