4a1f73933d12b7917dc9d2b6dca4058721e9916f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:53 +0000

    Add package amsdal_data/0.4.10/amsdal_data-0.4.10-cp312-cp312-manylinux1_x86_64.manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_5_x86_64.whl

---
+++ packages/amsdal_data/amsdal_data-0.4.10-cp312-cp312-manylinux1_x86_64.manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_5_x86_64.whl/amsdal_data/connections/constants.py
@@ -0,0 +1,17 @@
+PRIMARY_PARTITION_KEY = 'partition_key'
+SECONDARY_PARTITION_KEY = 'range_key'
+
+METADATA_TABLE = 'Metadata'
+REFERENCE_TABLE = 'Reference'
+TRANSACTION_TABLE = 'Transaction'
+OBJECT_TABLE = 'Object'
+
+METADATA_KEY = '_metadata'
+
+SCHEMA_NAME_FIELD = 'name'
+SCHEMA_VERSION_FIELD = 'version'
+
+CLASS_OBJECT = 'ClassObject'
+CLASS_OBJECT_META = 'ClassObjectMeta'
+OBJECT_ID = 'object_id'
+OBJECT_VERSION = 'object_version'
