82e684b13aa2bb7fed0413287492d1cdec83972e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:02 +0000

    Add package verifastscore/0.1.0/verifastscore-0.1.0.tar.gz

---
+++ packages/verifastscore/verifastscore-0.1.0.tar.gz/verifastscore-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='verifastscore',
+    version='0.1.0',
+    description='Fast, end-to-end factuality evaluation for long-form LLM responses.',
+    author='Rishanth Rajendhran',
+    author_email='rishanth@umd.edu',
+    url='https://github.com/rishanthrajendhran/verifastscore',
+    license='Apache 2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'torch',
+        'transformers',
+        'spacy',
+        'tqdm',
+        'regex',
+        'requests',
+        'flash-attn',
+    ],
+    entry_points={
+        'console_scripts': [
+            'verifastscore=verifastscore.verifastscore:main',
+        ]
+    },
+    python_requires='>=3.9',
+)
