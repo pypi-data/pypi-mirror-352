9a7763deb55372c46d0f894d459a3e09bdf5fbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:13 +0000

    Add package vectice/25.2.8.0/vectice-25.2.8.0.tar.gz

---
+++ packages/vectice/vectice-25.2.8.0.tar.gz/vectice-25.2.8.0/src/vectice/autolog/__init__.py
@@ -0,0 +1,21 @@
+from vectice.autolog.autolog import (
+    add_custom_metric_function,
+    cell,
+    config,
+    get_connection,
+    iteration,
+    notebook,
+    phase_config,
+    vectice_config,
+)
+
+__all__ = [
+    "cell",
+    "notebook",
+    "config",
+    "vectice_config",
+    "phase_config",
+    "get_connection",
+    "iteration",
+    "add_custom_metric_function",
+]
