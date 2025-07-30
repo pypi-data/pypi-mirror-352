153ba23b561b0345457a76b409837b59f8c90d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:33 +0000

    Add package muoblpbindings/0.0.5/muoblpbindings-0.0.5-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/muoblpbindings/muoblpbindings-0.0.5-cp311-cp311-macosx_11_0_arm64.whl/muoblpbindings/__init__.pyi
@@ -0,0 +1,17 @@
+def equal_shares(voters: list[str],
+                 projects: list[str],
+                 cost: dict[str, float],
+                 approvers_utilities: dict[str, list[tuple[str, int]]],
+                 total_budget: float) -> list[str]:
+    """
+    Basic equal shares implementation
+    """
+
+def equal_shares_utils(voters: list[str],
+                 projects: list[str],
+                 cost: dict[str, float],
+                 approvers_utilities: dict[str, list[tuple[str, int]]],
+                 total_budget: float) -> list[str]:
+    """
+    Equal shares with utilities implementation
+    """
