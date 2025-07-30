e6daed3602fc4a67237f317239e4c8179a236f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:40 +0000

    Add package onnx-ir/0.1.1/onnx_ir-0.1.1-py3-none-any.whl

---
+++ packages/onnx-ir/onnx_ir-0.1.1-py3-none-any.whl/onnx_ir/_version_utils.py
@@ -0,0 +1,91 @@
+# Copyright (c) ONNX Project Contributors
+# SPDX-License-Identifier: Apache-2.0
+"""Version utils for testing."""
+
+from __future__ import annotations
+
+import packaging.version
+
+
+def onnx_older_than(version: str) -> bool:
+    """Returns True if the ONNX version is older than the given version."""
+    import onnx  # pylint: disable=import-outside-toplevel
+
+    return (
+        packaging.version.parse(onnx.__version__).release
+        < packaging.version.parse(version).release
+    )
+
+
+def torch_older_than(version: str) -> bool:
+    """Returns True if the torch version is older than the given version."""
+    import torch  # pylint: disable=import-outside-toplevel
+
+    return (
+        packaging.version.parse(torch.__version__).release
+        < packaging.version.parse(version).release
+    )
+
+
+def transformers_older_than(version: str) -> bool | None:
+    """Returns True if the transformers version is older than the given version."""
+    try:
+        import transformers  # pylint: disable=import-outside-toplevel
+    except ImportError:
+        return None
+
+    return (
+        packaging.version.parse(transformers.__version__).release
+        < packaging.version.parse(version).release
+    )
+
+
+def is_onnxruntime_training() -> bool:
+    """Returns True if the onnxruntime is onnxruntime-training."""
+    try:
+        from onnxruntime import training  # pylint: disable=import-outside-toplevel
+
+        assert training
+    except ImportError:
+        # onnxruntime not training
+        return False
+
+    try:
+        from onnxruntime.capi.onnxruntime_pybind11_state import (  # pylint: disable=import-outside-toplevel
+            OrtValueVector,
+        )
+    except ImportError:
+        return False
+
+    return hasattr(OrtValueVector, "push_back_batch")
+
+
+def onnxruntime_older_than(version: str) -> bool:
+    """Returns True if the onnxruntime version is older than the given version."""
+    import onnxruntime  # pylint: disable=import-outside-toplevel
+
+    return (
+        packaging.version.parse(onnxruntime.__version__).release
+        < packaging.version.parse(version).release
+    )
+
+
+def numpy_older_than(version: str) -> bool:
+    """Returns True if the numpy version is older than the given version."""
+    import numpy  # pylint: disable=import-outside-toplevel
+
+    return (
+        packaging.version.parse(numpy.__version__).release
+        < packaging.version.parse(version).release
+    )
+
+
+def has_transformers():
+    """Tells if transformers is installed."""
+    try:
+        import transformers  # pylint: disable=import-outside-toplevel
+
+        assert transformers
+        return True  # noqa
+    except ImportError:
+        return False
