73b088faf0f98292a38a3b45e32a8e318083aaf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:42 +0000

    Add package aa-fleetpings/3.6.0/aa_fleetpings-3.6.0-py3-none-any.whl

---
+++ packages/aa-fleetpings/aa_fleetpings-3.6.0-py3-none-any.whl/fleetpings/__init__.py
@@ -0,0 +1,10 @@
+"""
+AA Fleet Pings
+A couple of variables to use throughout the app
+"""
+
+# Django
+from django.utils.translation import gettext_lazy as _
+
+__version__ = "3.6.0"
+__title__ = _("Fleet Pings")
