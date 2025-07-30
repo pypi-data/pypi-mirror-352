9aff49d890f056c0e9ff58692e84545450d3e519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:12 +0000

    Add package abstract-flask/0.0.0.36/abstract_flask-0.0.0.36.tar.gz

---
+++ packages/abstract-flask/abstract_flask-0.0.0.36.tar.gz/abstract_flask-0.0.0.36/src/abstract_flask/abstract_flask.py
@@ -0,0 +1,45 @@
+import os,sys,unicodedata,hashlib,json
+from flask_cors import CORS
+from abstract_utilities import make_list,get_media_types,get_logFile
+from multiprocessing import Process
+from flask import (
+    Blueprint,
+    request,
+    jsonify,
+    send_file,
+    current_app,
+    Flask
+    
+)
+
+from .request_utils import (dump_if_json,
+                            required_keys,
+                            parse_request,
+                            parse_and_return_json,
+                            parse_and_spec_vars,
+                            get_only_kwargs
+                            )
+from .network_tools import get_user_ip
+from werkzeug.utils import secure_filename
+def jsonify_it(obj):
+    if isinstance(obj,dict):
+        status_code = obj.get("status_code")
+        return jsonify(obj),status_code
+def get_bp(name, **bp_kwargs):
+    # if they passed a filename, strip it down to the module name
+    if os.path.isfile(name):
+        basename = os.path.basename(name)
+        name = os.path.splitext(basename)[0]
+
+    bp_name = f"{name}_bp"
+    logger  = get_logFile(bp_name)
+    logger.info(f"Python path: {sys.path!r}")
+
+    # build up only the kwargs they actually gave us
+
+    bp = Blueprint(
+        bp_name,
+        __name__,
+        **bp_kwargs,
+    )
+    return bp, logger
