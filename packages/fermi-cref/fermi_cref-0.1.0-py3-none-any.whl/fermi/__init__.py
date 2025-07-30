19802f9543caacffa806ddf35451c1d6e84a0b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:14 +0000

    Add package fermi-cref/0.1.0/fermi_cref-0.1.0-py3-none-any.whl

---
+++ packages/fermi-cref/fermi_cref-0.1.0-py3-none-any.whl/fermi/__init__.py
@@ -0,0 +1,8 @@
+#from .raw_matrix_processor import RawMatrixProcessor
+from .matrix_processor import MatrixProcessorCA
+#from .comparative_advantage import ComparativeAdvantage
+from .economic_fitness_complexity import efc
+from .relatedness_metrics import RelatednessMetrics
+from .prediction_module import ECPredictor, SPS
+from .validation_metrics import ValidationMetrics
+
