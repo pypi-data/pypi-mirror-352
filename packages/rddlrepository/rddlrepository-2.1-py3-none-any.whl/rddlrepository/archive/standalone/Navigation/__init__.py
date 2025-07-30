ae800ef12bca50f685ea38e79573ede4fee84a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:28 +0000

    Add package rddlrepository/2.1/rddlrepository-2.1-py3-none-any.whl

---
+++ packages/rddlrepository/rddlrepository-2.1-py3-none-any.whl/rddlrepository/archive/standalone/Navigation/__init__.py
@@ -0,0 +1,7 @@
+info = {
+    'name': 'Navigation_Continuous',
+    'description': 'Continuous state action navigation problem with regions to be avoided.',
+    'context': '',
+    'tags': '',
+    'viz': 'None'
+}
