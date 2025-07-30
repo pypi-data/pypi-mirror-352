816aa7d7c1923902e7e87943716a943ba17fc0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:17 +0000

    Add package areq/0.0.4/areq-0.0.4-py3-none-any.whl

---
+++ packages/areq/areq-0.0.4-py3-none-any.whl/areq/__init__.py
@@ -0,0 +1,4 @@
+from .api import get, options, head, post, put, patch, delete
+from .models import AreqResponse
+
+__all__ = ["get", "options", "head", "post", "put", "patch", "delete", "AreqResponse"]
