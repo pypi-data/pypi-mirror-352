664eb9b587ea4f723a0da54e86a26ee3b21bf2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:46 +0000

    Add package otter-pensieve/0.1.0/otter_pensieve-0.1.0-py3-none-any.whl

---
+++ packages/otter-pensieve/otter_pensieve-0.1.0-py3-none-any.whl/otter_pensieve/__init__.py
@@ -0,0 +1,25 @@
+from typing_extensions import override
+from otter.plugins import AbstractOtterPlugin
+
+
+class PensieveOtterPlugin(AbstractOtterPlugin):
+    def __init__(
+        self,
+        submission_path: str,
+        submission_metadata: dict[str, object],
+        plugin_config: dict[str, object],
+    ):
+        super().__init__(submission_path, submission_metadata, plugin_config)
+
+    @override
+    def before_grading(self, config: object):
+        print(
+            r"""
+ _____  ______ _   _  _____ _____ ______ _    _ ______ 
+|  __ \|  ____| \ | |/ ____|_   _|  ____| |  | |  ____|
+| |__) | |__  |  \| | (___   | | | |__  | |  | | |__   
+|  ___/|  __| | . ` |\___ \  | | |  __| | |  | |  __|  
+| |    | |____| |\  |____) |_| |_| |____ \ \/ /| |____ 
+|_|    |______|_| \_|_____/|_____|______| \__/ |______|
+"""
+        )
