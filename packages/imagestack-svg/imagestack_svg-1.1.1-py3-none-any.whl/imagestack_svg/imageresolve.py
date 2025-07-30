af917bdf273bc691821dc9869b7080cbd7d1d3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:09 +0000

    Add package imagestack-svg/1.1.1/imagestack_svg-1.1.1-py3-none-any.whl

---
+++ packages/imagestack-svg/imagestack_svg-1.1.1-py3-none-any.whl/imagestack_svg/imageresolve.py
@@ -0,0 +1,31 @@
+try:
+    from PySide2.QtGui import QImage
+except ModuleNotFoundError:
+    from PySide6.QtGui import QImage
+
+from .imagestack import ImageStack
+from .imagecreator import ImageCreator
+
+
+class ImageStackResolveString(ImageStack):
+    async def create_bytes(self,
+                           image_creator: ImageCreator,
+                           image_format: QImage.Format = QImage.Format_RGBA64,
+                           max_size: tuple = None):
+
+        template = image_creator.jinja2_create_bytes_env.from_string(self._replace(self.svg))
+        return await super()._create_bytes(template.render(**self._last_kwargs),
+                                           image_format,
+                                           max_size)
+
+    async def create_raw_svg(self, image_creator: ImageCreator):
+        template = image_creator.jinja2_create_svg_env.from_string(self._replace(self.svg))
+        return await super()._create_raw_svg(template.render(**self._last_kwargs))
+
+    async def create_inner_svg(self, image_creator: ImageCreator):
+        template = image_creator.jinja2_create_svg_env.from_string(self._replace(self.svg))
+        return await super()._create_inner_svg(template.render(**self._last_kwargs))
+
+    def __call__(self, **kwargs):
+        self._last_kwargs = kwargs
+        return self
