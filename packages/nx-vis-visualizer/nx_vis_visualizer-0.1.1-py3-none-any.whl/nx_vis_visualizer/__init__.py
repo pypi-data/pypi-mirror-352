5b73e9f7a578cf3041bcad065384eb0ee9cf4fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:40 +0000

    Add package nx-vis-visualizer/0.1.1/nx_vis_visualizer-0.1.1-py3-none-any.whl

---
+++ packages/nx-vis-visualizer/nx_vis_visualizer-0.1.1-py3-none-any.whl/nx_vis_visualizer/__init__.py
@@ -0,0 +1,11 @@
+# src/nx_vis_visualizer/__init__.py
+
+from .core import DEFAULT_VIS_OPTIONS, nx_to_vis
+
+__version__ = "0.1.1"  # Should match pyproject.toml
+
+__all__ = [
+    "DEFAULT_VIS_OPTIONS",
+    "__version__",
+    "nx_to_vis",
+]
