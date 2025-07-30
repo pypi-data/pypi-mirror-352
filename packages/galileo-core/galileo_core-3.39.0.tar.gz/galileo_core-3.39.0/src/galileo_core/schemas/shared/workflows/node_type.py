38e52f98513e4a978f536302b87e171d64693654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:17 +0000

    Add package galileo-core/3.39.0/galileo_core-3.39.0.tar.gz

---
+++ packages/galileo-core/galileo_core-3.39.0.tar.gz/galileo_core-3.39.0/src/galileo_core/schemas/shared/workflows/node_type.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class NodeType(str, Enum):
+    chain = "chain"
+    chat = "chat"
+    llm = "llm"
+    retriever = "retriever"
+    tool = "tool"
+    agent = "agent"
+    workflow = "workflow"
+    trace = "trace"
