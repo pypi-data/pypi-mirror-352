301bc9f9f6345f9738d1afb45bd2e898eab2e821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:39 +0000

    Add package kmv/0.3.0/kmv-0.3.0.tar.gz

---
+++ packages/kmv/kmv-0.3.0.tar.gz/kmv-0.3.0/kmv/core/streams.py
@@ -0,0 +1,16 @@
+"""Defines the stream format for the viewer."""
+
+from __future__ import annotations
+
+from typing import Mapping, Tuple
+
+import mujoco
+
+
+def default_streams(model: mujoco.MjModel) -> Mapping[str, Tuple[int, ...]]:
+    """Return {stream_name: shape} for the standard viewer."""
+    return {
+        "qpos": (model.nq,),
+        "qvel": (model.nv,),
+        "sim_time": (1,),
+    }
