97db8076131a940603974f8e74f68ec4532b8976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:33 +0000

    Add package flgo/0.4.4/flgo-0.4.4.tar.gz

---
+++ packages/flgo/flgo-0.4.4.tar.gz/flgo-0.4.4/tests/test.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('../src')
+import flgo.algorithm.fedavg as fedavg
+import flgo.experiment.analyzer
+import os
+import torch.multiprocessing as mp
+
+task = './my_task'
+gen_config = {
+    'benchmark':{'name':'flgo.benchmark.mnist_classification'},
+    'partitioner':{'name':'IIDPartitioner', 'para':{'num_clients':100}}
+}
+analysis_plan = {
+    'Selector':{'task':'./my_task', 'header':['fedavg',], },
+    'Painter':{'Curve':[{'args':{'x':'communication_round', 'y':'valid_loss'}}]},
+    'Table':{'min_value':[{'x':'valid_loss'}]},
+}
+if __name__ == '__main__':
+    # generate federated task if task doesn't exist
+    if not os.path.exists(task): flgo.gen_task(gen_config, task_path=task)
+    # running fedavg on the specified task
+    runner = flgo.init(task, fedavg, {'gpu':[0,],'log_file':True, 'num_steps':5})
+    runner.run()
+    # visualize the experimental result
+    flgo.experiment.analyzer.show(analysis_plan)
