ff9b875adec41a9c51f0b99cc4181d0116852f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:12 +0000

    Add package prefect/3.4.5.dev3/prefect-3.4.5.dev3.tar.gz

---
+++ packages/prefect/prefect-3.4.5.dev3.tar.gz/prefect-3.4.5.dev3/src/prefect/server/ui/assets/404-B8cQWpHn.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,m as t,dv as r}from"./index-BpsoK7wD.js";import{u as s}from"./usePageTitle-CNd-zINo.js";const _=e({__name:"404",setup(n){return s("Page Not Found"),(c,m)=>(a(),o(t(r)))}});export{_ as default};
+//# sourceMappingURL=404-B8cQWpHn.js.map
---
+++ packages/prefect/prefect-3.4.5.dev3.tar.gz/prefect-3.4.5.dev3/src/prefect/server/ui/assets/usePrefectApi-D5UKEZ-A.js
@@ -0,0 +1,2 @@
+import{cH as r}from"./index-BpsoK7wD.js";import{p as e}from"./api-DKLA9ueI.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-D5UKEZ-A.js.map
