209250320fa20eef814aa0dd345143872ae8f093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:18 +0000

    Add package json2markdown/0.1.0/json2markdown-0.1.0.tar.gz

---
+++ packages/json2markdown/json2markdown-0.1.0.tar.gz/json2markdown-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="json2markdown",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+    description="A library to convert JSON data to Markdown documents",
+    author="Govind Banura",
+    author_email="govindbanura2310@gmail.com",
+    license="MIT",
+)
