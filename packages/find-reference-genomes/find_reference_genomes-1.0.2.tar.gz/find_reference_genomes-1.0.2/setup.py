73b10f9ebed45894344d1e2ef23bd48d544a7028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:20 +0000

    Add package find-reference-genomes/1.0.2/find_reference_genomes-1.0.2.tar.gz

---
+++ packages/find-reference-genomes/find_reference_genomes-1.0.2.tar.gz/find_reference_genomes-1.0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="find_reference_genomes",
+    version="1.0.2",
+    author="bistace",
+    author_email="bistace@genoscope.cns.fr",
+    description="find_reference_genomes is a rdbioseq tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "pytest",
+    ],
+    entry_points={
+        "console_scripts": [
+            "find_reference_genomes=find_reference_genomes.command_line:main",
+        ],
+    },
+)
