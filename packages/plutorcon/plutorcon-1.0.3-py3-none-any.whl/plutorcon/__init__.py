723e232009dcb5467739786cd9e0c997e8161e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:55 +0000

    Add package plutorcon/1.0.3/plutorcon-1.0.3-py3-none-any.whl

---
+++ packages/plutorcon/plutorcon-1.0.3-py3-none-any.whl/plutorcon/__init__.py
@@ -0,0 +1,4 @@
+from .client import PlutoniumRCON
+from .models import Player, Commands, ServerInfo, Colors, ServerStatus
+
+__all__ = ["PlutoniumRCON", "Player", "Commands", "ServerInfo", "Colors", "ServerStatus"]
---
+++ packages/plutorcon/plutorcon-1.0.3-py3-none-any.whl/plutorcon/models/commands.py
@@ -0,0 +1,8 @@
+class Commands:
+    """Enum for server commands."""
+    GET_STATUS = "getstatus"
+    STATUS = "status"
+    GET_INFO = "getinfo"
+    SAY = "say"
+    TELL = "tell"
+    KICK = "clientkick_for_reason"
