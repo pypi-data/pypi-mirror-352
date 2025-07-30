19f72cf5d75421805ba7fc7e31d013ce7076f5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:08 +0000

    Add package nominal-api/0.705.1/nominal_api-0.705.1-py3-none-any.whl

---
+++ packages/nominal-api/nominal_api-0.705.1-py3-none-any.whl/nominal_api/connect_download/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+from .._impl import (
+    connect_download_Platform as Platform,
+)
+
---
+++ packages/nominal-api/nominal_api-0.705.1-py3-none-any.whl/nominal_api/scout_channelvariables_api/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from .._impl import (
+    scout_channelvariables_api_ChannelVariable as ChannelVariable,
+    scout_channelvariables_api_ChannelVariableName as ChannelVariableName,
+    scout_channelvariables_api_ComputeNodeWithContext as ComputeNodeWithContext,
+    scout_channelvariables_api_ComputeSpec as ComputeSpec,
+    scout_channelvariables_api_ComputeSpecV1 as ComputeSpecV1,
+    scout_channelvariables_api_ComputeSpecVisitor as ComputeSpecVisitor,
+    scout_channelvariables_api_VariableLocator as VariableLocator,
+    scout_channelvariables_api_VariableLocatorVisitor as VariableLocatorVisitor,
+    scout_channelvariables_api_WorkbookChannelVariableMap as WorkbookChannelVariableMap,
+    scout_channelvariables_api_WorkbookContext as WorkbookContext,
+)
+
