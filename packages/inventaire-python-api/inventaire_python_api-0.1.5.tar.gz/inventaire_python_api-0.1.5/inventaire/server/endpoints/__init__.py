f3088479c6c2d452961e5b14851638886959e458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:26 +0000

    Add package inventaire-python-api/0.1.5/inventaire_python_api-0.1.5.tar.gz

---
+++ packages/inventaire-python-api/inventaire_python_api-0.1.5.tar.gz/inventaire_python_api-0.1.5/inventaire/server/endpoints/__init__.py
@@ -0,0 +1,9 @@
+from .endpoints import (AuthEndpoints, DataEndpoints, EntitiesEndpoints,
+                        GroupsEndpoints, ItemsEndpoints, SearchEndpoints,
+                        ShelvesEndpoints, TransactionsEndpoints, UserEndpoints,
+                        UsersEndpoints)
+
+# from .actions import(
+#         ProjectEndpoints,
+#         TestCaseEndpoints
+# )
