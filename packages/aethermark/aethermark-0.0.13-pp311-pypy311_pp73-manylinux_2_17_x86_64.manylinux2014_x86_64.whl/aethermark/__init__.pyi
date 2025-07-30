44bd4c3b6195dce36c2064346c5620e39ce28df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:44 +0000

    Add package aethermark/0.0.13/aethermark-0.0.13-pp311-pypy311_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aethermark/aethermark-0.0.13-pp311-pypy311_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aethermark/__init__.pyi
@@ -0,0 +1,26 @@
+"""
+Pybind11 example plugin
+-----------------------
+
+.. currentmodule:: aethermark
+
+.. autosummary::
+    :toctree: _generate
+
+    add
+    subtract
+"""
+
+def add(i: int, j: int) -> int:
+    """
+    Add two numbers
+
+    Some other explanation about the add function.
+    """
+
+def subtract(i: int, j: int) -> int:
+    """
+    Subtract two numbers
+
+    Some other explanation about the subtract function.
+    """
