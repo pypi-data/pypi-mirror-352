2f9845166385398f6a0f254f027e093bc02351c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:43 +0000

    Add package ob-metaflow-stubs/6.0.3.176rc3/ob_metaflow_stubs-6.0.3.176rc3-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.176rc3-py2.py3-none-any.whl/metaflow-stubs/tuple_util.pyi
@@ -0,0 +1,15 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.14.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-04T06:35:02.734474                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def namedtuple_with_defaults(typename, field_descr, defaults = ()):
+    ...
+
+foreach_frame_field_list: list
+
