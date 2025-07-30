3e0494ab33a4e8a3e2e3a45b7955059e022c5c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:02 +0000

    Add package apis-core-rdf/0.48.0/apis_core_rdf-0.48.0-py3-none-any.whl

---
+++ packages/apis-core-rdf/apis_core_rdf-0.48.0-py3-none-any.whl/apis_core/history/views.py
@@ -0,0 +1,38 @@
+from django.forms import Form
+from django.views.generic.detail import BaseDetailView, DetailView
+from django.views.generic.edit import FormView
+from django_tables2 import SingleTableMixin
+from django_tables2.tables import table_factory
+from simple_history.utils import get_history_model_for_model
+
+from apis_core.generic.helpers import first_member_match, module_paths
+from apis_core.generic.views import GenericModelMixin
+
+from .tables import HistoryGenericTable
+
+
+class HistoryView(SingleTableMixin, DetailView):
+    template_name = "history/history.html"
+
+    def setup(self, *args, **kwargs):
+        super().setup(*args, **kwargs)
+        if contenttype := kwargs.get("contenttype"):
+            self.model = get_history_model_for_model(contenttype.model_class())
+            self.queryset = self.model.objects.filter(id=kwargs.get("pk"))
+
+    def get_table_class(self):
+        table_modules = module_paths(self.model, path="tables", suffix="HistoryTable")
+        table_class = first_member_match(table_modules, HistoryGenericTable)
+        return table_factory(self.model, table_class)
+
+
+class HistoryReset(GenericModelMixin, BaseDetailView, FormView):
+    form_class = Form
+    template_name = "history/reset.html"
+
+    def form_valid(self, form):
+        self.get_object().instance.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.get_object().instance.get_history_url()
