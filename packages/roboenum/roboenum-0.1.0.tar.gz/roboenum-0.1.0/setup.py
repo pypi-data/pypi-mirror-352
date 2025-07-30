32baa9082a337b0f9bb45137f40445e9f2db0f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:36 +0000

    Add package roboenum/0.1.0/roboenum-0.1.0.tar.gz

---
+++ packages/roboenum/roboenum-0.1.0.tar.gz/roboenum-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="roboenum",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "httpx",
+        "colorama",
+    ],
+    entry_points={
+        'console_scripts': [
+            'roboenum=roboenum.cli:main',
+        ],
+    },
+    author="QTShade",
+    description="A robots.txt network enumeration and fingerprinting tool",
+    license="MIT",
+    license_files=["LICENSE*"],
+    url="https://github.com/QTShade/roboenum",
+    keywords="roboenum, enum, recon, tools, pentest",
+    python_requires='>=3.8',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
