82ad4c9af227cf4accbcdb4395d92c291d206e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:26 +0000

    Add package mipi_datamanager/1.1.7/mipi_datamanager-1.1.7-py3-none-any.whl

---
+++ packages/mipi_datamanager/mipi_datamanager-1.1.7-py3-none-any.whl/mipi_datamanager/core/mipi_setup.py
@@ -0,0 +1,11 @@
+set_search_directories = [
+    (r"\\fs02\Medical-Informatics\reporting_intake\REQS", "Reqs", False),
+    (r"\\fs02\Medical-Informatics\reporting_intake\CAPE_REQS", "Cape Reqs", True),
+]
+set_search_extensions = [
+    (".sql", True),
+    (".py", False),
+]
+set_copy_destinations = [
+    (r"\\fs02\Medical-Informatics\mipi_dev", "MiPi Development")
+]
