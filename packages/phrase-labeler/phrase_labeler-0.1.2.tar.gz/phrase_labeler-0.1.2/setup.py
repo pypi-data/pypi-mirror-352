690b4caecb972f338c435ae126d96df13b1d16e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:08:46 +0000

    Add package phrase-labeler/0.1.2/phrase_labeler-0.1.2.tar.gz

---
+++ packages/phrase-labeler/phrase_labeler-0.1.2.tar.gz/phrase_labeler-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="phrase-labeler",
+    version="0.1.2",
+    packages=find_packages(
+        include=['phrase_labeler', 'phrase_labeler.*']
+    ),
+    install_requires=[
+        "openai"       
+    ],
+    entry_points={
+        'console_scripts': [
+            'label-phrase=phrase_labeler.phrase_labeler:main',
+        ],
+    },
+    include_package_data=True,
+    description="A package to label sentence segments given predefined segment labels using OpenAI API",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Ziwei Gu",
+    author_email="ziweigu@g.harvard.edu",
+    url="https://github.com/ZiweiGu/GP-TSM", 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
