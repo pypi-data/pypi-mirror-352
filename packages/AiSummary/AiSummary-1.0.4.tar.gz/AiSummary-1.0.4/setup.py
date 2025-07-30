5143eeb817c8e4138041ebc42fe1457170a267a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:23:42 +0000

    Add package AiSummary/1.0.4/AiSummary-1.0.4.tar.gz

---
+++ packages/AiSummary/AiSummary-1.0.4.tar.gz/AiSummary-1.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AiSummary",
+    version="1.0.4",
+    author="Mohammad Taha Gorji",
+    author_email="MohammadTahaGorjiProfile@gmail.com",
+    description="Text summarization with Ai (without using API)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",
+
+)
