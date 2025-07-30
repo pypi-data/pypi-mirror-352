f1b7da21b5e1750ea749c9a50f1b968b833cd482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:07 +0000

    Add package SAPB1SL/1.0.0/sapb1sl-1.0.0.tar.gz

---
+++ packages/SAPB1SL/sapb1sl-1.0.0.tar.gz/sapb1sl-1.0.0/setup.py
@@ -0,0 +1,25 @@
+################################################
+#                   BLPG                       #   
+################################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name="SAPB1SL",
+    version="1.0.0",
+    author="Bryan Pineda Gonzalez",
+    author_email="dev@bryanlpinedag.com",
+    description="Cliente Python para el Service Layer de SAP Business One usando requests",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bryanluispg/sap_service_layer_blpg",  
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
