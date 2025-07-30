3703d0c9afc1ebf04931b6784f7d1751fdebfb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:08:28 +0000

    Add package cyclereward/0.1.3/cyclereward-0.1.3.tar.gz

---
+++ packages/cyclereward/cyclereward-0.1.3.tar.gz/cyclereward-0.1.3/cyclereward/config.py
@@ -0,0 +1,6 @@
+cyclereward_args = {
+    'blip_path': 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_large.pth',
+    'vit': 'large',  
+    'image_size': 224,
+    'mlp_dim': 768
+}
