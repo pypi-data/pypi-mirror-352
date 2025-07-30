d44dcf32720837278e2927f5e341cce01c35b8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:12 +0000

    Add package python-clu/0.12.12/python_clu-0.12.12.tar.gz

---
+++ packages/python-clu/python_clu-0.12.12.tar.gz/python_clu-0.12.12/clu/config/keymap.py
@@ -0,0 +1,465 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import clu.abstract
+import copy
+import sys
+
+from clu.config.abc import FrozenKeyMap, KeyMap, NamespaceWalker
+from clu.config.ns import concatenate_ns, unpack_ns, pack_ns
+from clu.typology import ismapping
+from clu.exporting import Exporter
+
+exporter = Exporter(path=__file__)
+export = exporter.decorator()
+
+# CONCRETE SUBCLASSES: FrozenFlat and Flat
+
+@export
+def flatwalk(mapping):
+    """ Iteratively walk a flat mapping with namespaced keys,
+        as found in a FrozenFlat or Flat instance.
+        
+        Based on https://stackoverflow.com/a/12507546/298171
+    """
+    for nskey in mapping.keys():
+        key, namespaces = unpack_ns(nskey)
+        yield namespaces + [key, mapping[nskey]]
+
+@export
+def articulate(mapping, walker=flatwalk):
+    """ Articulate a given mapping with namespaced keys into
+        one that is nested. By default, use the “flatwalk(…)”
+        walking function.
+    """
+    tree = {}
+    for *namespaces, key, value in walker(mapping):
+        d = tree
+        for namespace in namespaces:
+            try:
+                d = d[namespace]
+            except KeyError:
+                d[namespace] = {}
+                d = d[namespace]
+        d[key] = value
+    return tree
+
+@export
+class FrozenFlat(FrozenKeyMap, clu.abstract.ReprWrapper,
+                               clu.abstract.Cloneable):
+    
+    """ A concrete immutable – or frozen – KeyMap class with a flat internal topology. """
+    
+    __slots__ = 'dictionary'
+    
+    def __init__(self, dictionary=None, **updates):
+        """ Initialize a flat KeyMap instance from a target dictionary.
+            
+            The dictionary can contain normal key-value items as long as the
+            keys are strings; namespaces can be specified per-key as per the
+            output of ‘pack_ns(…)’ (q.v. function definition supra.)
+        """
+        try:
+            super().__init__(**updates)
+        except TypeError:
+            super().__init__()
+        if hasattr(dictionary, 'dictionary'):
+            dictionary = getattr(dictionary, 'dictionary')
+        elif hasattr(dictionary, 'flatten'):
+            dictionary = getattr(dictionary.flatten(), 'dictionary')
+        self.dictionary = dict(dictionary or {})
+        if updates:
+            self.dictionary.update(**updates)
+    
+    def nestify(self, cls=None, walker=flatwalk):
+        """ Articulate a flattened KeyMap instance out into one that is nested. """
+        if cls is None:
+            cls = FrozenNested
+        return cls(tree=articulate(self.dictionary, walker=walker))
+    
+    def __iter__(self):
+        yield from self.dictionary
+    
+    def __reversed__(self):
+        yield from reversed(self.dictionary.keys())
+    
+    def __len__(self):
+        return len(self.dictionary)
+    
+    def __contains__(self, nskey):
+        return nskey in self.dictionary
+    
+    def __getitem__(self, nskey):
+        return self.dictionary[nskey]
+    
+    def inner_repr(self):
+        return repr(self.dictionary)
+    
+    def clone(self, deep=False, memo=None):
+        copier = deep and copy.deepcopy or copy.copy
+        return type(self)(dictionary=copier(self.dictionary))
+    
+    def to_dict(self):
+        """ Used by `clu.config.codecs` to serialize the keymap """
+        return copy.deepcopy(self.dictionary)
+
+@export
+class Flat(FrozenFlat, KeyMap):
+    
+    """ A concrete mutable KeyMap class with a flat internal topology. """
+    
+    def freeze(self):
+        return FrozenFlat(dictionary=copy.deepcopy(self.dictionary))
+    
+    def __setitem__(self, nskey, value):
+        self.dictionary[nskey] = value
+    
+    def __delitem__(self, nskey):
+        del self.dictionary[nskey]
+
+@export
+def dictify(treeish, cls=dict):
+    """ Recursively convert a possibly nested mapping to dicts.
+        
+        Standard Python dicts will be created by default; specify
+        a custom class with the “cls” argument to use your own.
+    """
+    if ismapping(treeish):
+        return cls({ key : dictify(value, cls=cls) for key, value in treeish.items() })
+    return treeish
+
+@export
+def mapwalk(mapping, pre=None):
+    """ Iteratively walk a nested mapping.
+        
+        Based on https://stackoverflow.com/a/12507546/298171
+    """
+    pre = pre and pre[:] or []
+    if ismapping(mapping):
+        for key, value in mapping.items():
+            if ismapping(value):
+                yield from mapwalk(value, pre + [key])
+            else:
+                yield pre + [key, value]
+    else:
+        yield mapping
+
+# CONCRETE SUBCLASSES: FrozenNested and Nested
+
+@export
+class FrozenNested(NamespaceWalker, clu.abstract.ReprWrapper,
+                                    clu.abstract.Cloneable):
+    
+    """ A concrete immutable – or frozen – KeyMap class with an articulated –
+        or, if you will, a nested – internal topology.
+    """
+    
+    __slots__ = 'tree'
+    
+    def __init__(self, tree=None, **updates):
+        """ Initialize an articulated (née “nested”) KeyMap instance from a
+            target nested dictionary (or a “tree” of dicts).
+        """
+        try:
+            super().__init__(**updates)
+        except TypeError:
+            super().__init__()
+        if hasattr(tree, 'tree'):
+            tree = getattr(tree, 'tree')
+        elif hasattr(tree, 'nestify'):
+            tree = getattr(tree.nestify(), 'tree')
+        self.tree = dictify(dict(tree or {}), cls=dict)
+        if updates:
+            for nskey, value in updates.items():
+                key, namespaces = unpack_ns(nskey)
+                d = self.tree
+                for namespace in namespaces:
+                    try:
+                        d = d[namespace]
+                    except KeyError:
+                        d[namespace] = {}
+                        d = d[namespace]
+                d[key] = value
+    
+    def walk(self):
+        """ Iteratively walk the nested KeyMap’s tree of dicts. """
+        yield from mapwalk(self.tree)
+    
+    def submap(self, *namespaces, unprefixed=False):
+        """ Return a flattened dict containing only the namespaced items. """
+        if unprefixed:
+            return { key : value for key, value in self.tree.items() \
+                                  if not ismapping(value) }
+        if not namespaces:
+            return dict(self)
+        if not concatenate_ns(*namespaces) in self.namespaces():
+            return {}
+        d = self.tree
+        for namespace in namespaces:
+            d = d[namespace]
+        return { pack_ns(key, *namespaces) : value for key, value in d.items() }
+    
+    def inner_repr(self):
+        return repr(self.tree)
+    
+    def clone(self, deep=False, memo=None):
+        copier = deep and copy.deepcopy or copy.copy
+        return type(self)(tree=copier(self.tree))
+    
+    def to_dict(self):
+        """ Used by `clu.config.codecs` to serialize the keymap """
+        return copy.deepcopy(self.tree)
+    
+    def __contains__(self, nskey):
+        key, namespaces = unpack_ns(nskey)
+        if not namespaces:
+            # Test for mappings to prevent false positives:
+            return not ismapping(self.tree.get(key, {}))
+        d = self.tree
+        for namespace in namespaces:
+            try:
+                d = d[namespace]
+            except KeyError:
+                return False
+        # Test for mappings to prevent false positives:
+        return not ismapping(d.get(key, {}))
+    
+    def __getitem__(self, nskey):
+        key, namespaces = unpack_ns(nskey)
+        if not namespaces:
+            return self.tree[key]
+        d = self.tree
+        for namespace in namespaces:
+            d = d[namespace]
+        return d[key]
+
+@export
+class Nested(FrozenNested, KeyMap):
+    
+    """ A concrete mutable KeyMap class with an articulated (or nested)
+        internal topology.
+    """
+    
+    def freeze(self):
+        return FrozenNested(tree=copy.deepcopy(self.tree))
+    
+    def __setitem__(self, nskey, value):
+        key, namespaces = unpack_ns(nskey)
+        if not namespaces:
+            self.tree[key] = value
+        else:
+            d = self.tree
+            for namespace in namespaces:
+                try:
+                    d = d[namespace]
+                except KeyError:
+                    d[namespace] = {}
+                    d = d[namespace]
+            d[key] = value
+    
+    def __delitem__(self, nskey):
+        if nskey in self:
+            key, namespaces = unpack_ns(nskey)
+            if not namespaces:
+                del self.tree[key]
+            else:
+                d = self.tree
+                for namespace in namespaces:
+                    d = d[namespace]
+                del d[key]
+        else:
+            raise KeyError(nskey)
+
+# Assign the modules’ `__all__` and `__dir__` using the exporter:
+__all__, __dir__ = exporter.all_and_dir()
+
+from clu.testing.utils import inline
+
+@inline.fixture
+def nestedmaps():
+    """ Nested-dictionary fixture function """
+    return {'body':   {'declare_i': {'id': {'name': 'i', 'type': 'Identifier'},
+                                            'init': {'type': 'Literal', 'value': 2},
+                                            'type': 'VariableDeclarator'},
+                           'kind': 'var',
+                           'type': 'VariableDeclaration',
+                           'declare_j': {'id': {'name': 'j', 'type': 'Identifier'},
+                                            'init': {'type': 'Literal', 'value': 4},
+                                            'type': 'VariableDeclarator'},
+                           'kind': 'var',
+                           'type': 'VariableDeclaration',
+                           'declare_answer': {'id': {'name': 'answer', 'type': 'Identifier'},
+                                            'init': {'left': {'name': 'i',
+                                                              'type': 'Identifier'},
+                                                     'operator': '*',
+                                                     'right': {'name': 'j',
+                                                               'type': 'Identifier'},
+                                                     'type': 'BinaryExpression'},
+                                            'type': 'VariableDeclarator'},
+                           'kind': 'var',
+                           'type': 'VariableDeclaration'},
+            'type':     'Program'}
+
+@inline.fixture
+def flatdict():
+    """ Flat-dictionary fixture function """
+    out = {}
+    for mappingpath in mapwalk(nestedmaps()):
+        *namespaces, key, value = mappingpath
+        nskey = pack_ns(key, *namespaces)
+        out[nskey] = value
+    return out
+
+@inline.fixture
+def arbitrary():
+    return {
+        'yo'     : 'dogg',
+        'iheard' : 'you like dict literals'
+    }
+
+def test():
+    
+    from pprint import pprint
+    
+    @inline.precheck
+    def show_nestedmaps():
+        print("Nested maps fixture output:")
+        pprint(nestedmaps())
+    
+    @inline
+    def test_mapwalk():
+        """ Simple “mapwalk(…)” content """
+        dictderive = tuple(mapwalk(nestedmaps()))
+        return dictderive
+    
+    @inline
+    def test_mapwalk_content():
+        """ Verbose “mapwalk(…)” content check """
+        for mappingpath in mapwalk(nestedmaps()):
+            *namespaces, key, value = mappingpath
+            nskey = pack_ns(key, *namespaces)
+            print("NAMESPACES:", ", ".join(namespaces))
+            print("KEY:", key)
+            print("NSKEY:", nskey)
+            print("VALUE:", value)
+            print()
+    
+    @inline
+    def test_frozenflat_nested_eq():
+        """ FrozenFlat and Nested equivalence """
+        flat_dict = {}
+        
+        for mappingpath in mapwalk(nestedmaps()):
+            *namespaces, key, value = mappingpath
+            nskey = pack_ns(key, *namespaces)
+            flat_dict[nskey] = value
+        
+        assert flat_dict == flatdict()
+        
+        flat = FrozenFlat(flat_dict)
+        nested = Nested(flat)
+        
+        # print("FLAT:")
+        # pprint(flat)
+        # print()
+        
+        # print("NESTED:")
+        # pprint(nested)
+        # print()
+        
+        # print("NESTED FLATTEN:")
+        # pprint(nested.flatten())
+        # print()
+        
+        assert nested.flatten() == flat
+        assert nested.flatten().dictionary == flatdict()
+        assert nested == flat
+    
+    @inline
+    def test_flat_frozenflat_nested_eq():
+        """ Flat, FrozenFlat and Nested equivalence """
+        flat_dict = {}
+        
+        for mappingpath in mapwalk(nestedmaps()):
+            *namespaces, key, value = mappingpath
+            nskey = pack_ns(key, *namespaces)
+            flat_dict[nskey] = value
+        
+        assert flat_dict == flatdict()
+        
+        flat = Flat(flat_dict)
+        frozen_flat = flat.freeze()
+        assert frozen_flat == flat
+        assert frozen_flat.dictionary == flatdict()
+        
+        nested = flat.nestify()
+        flattened = nested.flatten()
+        assert flattened == flat
+        assert flattened.dictionary == flatdict()
+    
+    @inline
+    def test_frozennested_contains():
+        """ FrozenNested contains namespaced key """
+        nested = FrozenNested(tree=nestedmaps())
+        
+        for mappingpath in mapwalk(nested.tree):
+            *namespaces, key, value = mappingpath
+            nskey = pack_ns(key, *namespaces)
+            assert nskey in nested
+    
+    @inline
+    def test_nested_contains():
+        """ Nested contains namespaced key """
+        nested = Nested(tree=nestedmaps(), **arbitrary())
+        
+        for mappingpath in mapwalk(nested.tree):
+            *namespaces, key, value = mappingpath
+            nskey = pack_ns(key, *namespaces)
+            assert nskey in nested or nskey in arbitrary()
+    
+    @inline
+    def test_frozennested_flat_eq():
+        """ FrozenNested and Flat roundtrip commutativity """
+        nested = FrozenNested(tree=nestedmaps())
+        flat = Flat(nested)
+        assert flat.nestify() == nested
+        assert flat == nested
+    
+    @inline
+    def test_nested_frozennested_eq():
+        """ Nested and FrozenNested commutativity """
+        nested = Nested(tree=nestedmaps())
+        frozen_nested = nested.freeze()
+        assert frozen_nested == nested
+        
+        flat = nested.flatten()
+        renested = flat.nestify()
+        assert renested == nested
+    
+    @inline
+    def test_nested_submaps():
+        nested = Nested(tree=nestedmaps())
+        frozen_nested = nested.freeze()
+        
+        assert frozen_nested.submap() == dict(frozen_nested)
+        assert nested.submap('body') == frozen_nested.submap('body')
+        assert nested.submap('body', 'declare_i') == frozen_nested.submap('body', 'declare_i')
+        assert nested.submap('WTF', 'HAX') == {}
+        assert nested.submap(unprefixed=True) == { 'type' : 'Program' }
+        assert nested.submap() == dict(nested)
+    
+    @inline.diagnostic
+    def show_fixture_cache_stats():
+        """ Show the per-fixture-function cache stats """
+        total = len(inline.fixtures)
+        for idx, name in enumerate(inline.fixtures.keys()):
+            if idx > 0:
+                print()
+            print(f"FUNCTION CACHE INFO: {name} ({idx+1} of {total})")
+            print(inline.fixtures[name].cache_info())
+    
+    # Run all inline tests:
+    return inline.test(100)
+
+if __name__ == '__main__':
+    sys.exit(test())
---
+++ packages/python-clu/python_clu-0.12.12.tar.gz/python_clu-0.12.12/clu/config/ns.py
@@ -0,0 +1,319 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from itertools import chain, zip_longest
+
+import sys
+
+from clu.constants.consts import ENVIRONS_SEP, NAMESPACE_SEP, NoDefault
+from clu.predicates import tuplize
+from clu.exporting import Exporter
+
+exporter = Exporter(path=__file__)
+export = exporter.decorator()
+
+# NAMESPACE-MANIPULATION FUNCTION API:
+
+@export
+def concatenate_ns(*namespaces):
+    """ Return the given namespace(s), concatenated with the
+        namespace separator.
+    """
+    return NAMESPACE_SEP.join(namespaces)
+
+@export
+def prefix_for(*namespaces):
+    """ Return the prefix string for the given namespace(s) """
+    ns = concatenate_ns(*namespaces)
+    return ns and f"{ns}{NAMESPACE_SEP}" or ns
+
+@export
+def strip_ns(nskey):
+    """ Strip all namespace-related prefixing from a namespaced key """
+    return nskey.rpartition(NAMESPACE_SEP)[-1]
+
+@export
+def split_ns(namespaced):
+    """ Split a namespaced string into its components """
+    return str(namespaced).split(NAMESPACE_SEP)
+
+@export
+def startswith_ns(putative, prefix):
+    """ Boolean predicate to compare a pair of namespace iterables,
+        returning True if the first starts with the second.
+        
+        Do not confuse this with the helper function “compare_ns(…)”,
+        defined below, which returns False if the namespace iterables
+        in question aren’t exactly alike.
+    """
+    putative_ns = concatenate_ns(*putative)
+    prefix_ns = concatenate_ns(*prefix)
+    return putative_ns.startswith(prefix_ns)
+
+@export
+def validate_ns(*namespaces):
+    """ Raise a ValueError if any of the given namespaces are invalid. """
+    for namespace in namespaces:
+        if NAMESPACE_SEP in namespace:
+            raise ValueError(f"namespace contains separator: “{namespace}”")
+        if not namespace.isidentifier():
+            raise ValueError(f"invalid namespace: “{namespace}”")
+    return True
+
+@export
+def unpack_ns(nskey):
+    """ Unpack a namespaced key into a set of namespaces and a key name.
+        
+        To wit: if the namespaced key is “yo:dogg:i-heard”, calling “unpack_ns(…)”
+        on it will return the tuple ('i-heard', ('yo', 'dogg'));
+        
+        If the key is not namespaced (like e.g. “wat”) the “unpack_ns(…)”
+        call will return the tuple ('wat', tuple()).
+    """
+    *namespaces, key = split_ns(nskey)
+    return key, namespaces
+
+@export
+def pack_ns(key, *namespaces):
+    """ Pack a key and a set of (optional) namespaces into a namespaced key.
+        
+        To wit: if called as “pack_ns('i-heard, 'yo', 'dogg')” the return
+        value will be the string "yo:dogg:i-heard".
+        
+        If no namespaces are provided (like e.g. “pack_ns('wat')”)
+        the return value will be the string "wat".
+    """
+    if not namespaces:
+        return key
+    return NAMESPACE_SEP.join(chain(namespaces, tuplize(key, expand=False)))
+
+@export
+def get_ns(nskey):
+    """ Get the namespace portion of a namespaced key as a packed string. """
+    return nskey.rpartition(NAMESPACE_SEP)[0]
+
+@export
+def get_ns_and_key(nskey):
+    """ Get the namespace and key portion of a namespaced key, as a packed
+        string and a bare key, respectively.
+    """
+    rpartition = nskey.rpartition(NAMESPACE_SEP)
+    return rpartition[0], rpartition[-1]
+
+@export
+def compare_ns(iterone, itertwo):
+    """ Boolean predicate to compare a pair of namespace iterables, value-by-value """
+    for one, two in zip_longest(iterone,
+                                itertwo,
+                                fillvalue=NoDefault):
+        if one != two:
+            return False
+    return True
+
+# ENVIRONMENT-VARIABLE MANIPULATION API:
+
+@export
+def concatenate_env(*namespaces):
+    """ Concatenate and UPPERCASE namespaces, per environment variables. """
+    return ENVIRONS_SEP.join(namespace.upper() for namespace in namespaces)
+
+@export
+def prefix_env(appname, *namespaces):
+    """ Determine the environment-variable prefix based on a given
+        set of namespaces and the provided “appname” value. Like e.g.,
+        for an appname of “YoDogg” and a namespace value of “iheard”,
+        the environment variable prefix would work out such that
+        a variable with a key value of “youlike” would look like this:
+            
+            YODOGG_IHEARD_YOULIKE
+                                 
+            ^^^^^^ ^^^^^^ ^^^^^^^
+               |      |      |
+               |      |      +––––– mapping key (uppercased)
+               |      +–––––––––––– namespaces (uppercased, one value)
+               +––––––––––––––––––– app name (uppercased)
+    """
+    if not appname and not namespaces:
+        return ''
+    if not appname:
+        return concatenate_env(*namespaces) + ENVIRONS_SEP
+    if not namespaces:
+        return appname.upper() + ENVIRONS_SEP
+    return appname.upper() + ENVIRONS_SEP + concatenate_env(*namespaces) + ENVIRONS_SEP
+
+@export
+def pack_env(appname, key, *namespaces):
+    """ Transform a mapping key, along with optional “namespaces”
+        values and the provided “appname” value, into an environment-
+        variable name. Like e.g., for an appname of “YoDogg” and
+        a namespace value of “iheard”, the environment variable
+        prefix would work out such that a variable with a key value
+        of “youlike” would look like this:
+            
+            YODOGG_IHEARD_YOULIKE
+                                 
+            ^^^^^^ ^^^^^^ ^^^^^^^
+               |      |      |
+               |      |      +––––– mapping key (uppercased)
+               |      +–––––––––––– namespaces (uppercased, one value)
+               +––––––––––––––––––– app name (uppercased)
+    """
+    prefix = prefix_env(appname, *namespaces)
+    return f"{prefix}{key.upper()}"
+
+@export
+def unpack_env(envkey):
+    """ Unpack the appname, possible namespaces, and the key from an environment
+        variable key name.
+    """
+    appname, *namespaces, key = envkey.casefold().split(ENVIRONS_SEP)
+    return appname, key, namespaces
+
+@export
+def nskey_from_env(envkey):
+    """ Repack an environment-variable key name as a packed namespace key. """
+    appname, *namespaces, key = envkey.casefold().split(ENVIRONS_SEP)
+    return appname, pack_ns(key, *namespaces)
+
+@export
+def nskey_to_env(appname, nskey):
+    """ Repack a packed namespace key, with a given appname, as an environment
+        variable key name.
+    """
+    key, namespaces = unpack_ns(nskey)
+    return pack_env(appname, key, *namespaces)
+
+# Assign the modules’ `__all__` and `__dir__` using the exporter:
+__all__, __dir__ = exporter.all_and_dir()
+
+def test():
+    
+    from clu.testing.utils import inline
+    from clu.config.keymap import Flat, Nested
+    from clu.config.keymap import nestedmaps, flatdict, arbitrary
+    from clu.fs.filesystem import which
+    from pprint import pprint
+    import os, re, textwrap
+    
+    flat = Flat(flatdict())
+    nested = Nested(nestedmaps())
+    
+    @inline
+    def test_flat_validate_ns():
+        """ Validate a Flat keymap’s namespaces """
+        for namespace in flat.namespaces():
+            assert validate_ns(*split_ns(namespace))
+    
+    @inline
+    def test_frozenflat_validate_ns():
+        """ Validate a FrozenFlat keymap’s namespaces """
+        for namespace in flat.freeze().namespaces():
+            assert validate_ns(*split_ns(namespace))
+    
+    @inline
+    def test_nested_validate_ns():
+        """ Validate a Nested keymap’s namespaces """
+        for namespace in nested.namespaces():
+            assert validate_ns(*split_ns(namespace))
+    
+    @inline
+    def test_frozennested_validate_ns():
+        """ Validate a FrozenNested keymap’s namespaces """
+        for namespace in nested.freeze().namespaces():
+            assert validate_ns(*split_ns(namespace))
+    
+    @inline
+    def test_validate_ns_errors():
+        """ Check “validate_ns(…)” error conditions """
+        try:
+            validate_ns("yo dogg")
+        except ValueError as exc:
+            assert "invalid namespace" in str(exc)
+        
+        try:
+            validate_ns("yo:dogg")
+        except ValueError as exc:
+            assert "namespace contains separator" in str(exc)
+    
+    namespaces = ('yo', 'dogg')
+    key = 'iheard'
+    nskey = "yo:dogg:iheard"
+    value = "you like this sort of thing"
+    
+    @inline
+    def test_concatenate_ns():
+        assert concatenate_ns(*namespaces) == "yo:dogg"
+    
+    @inline
+    def test_prefix_for():
+        assert prefix_for(*namespaces) == "yo:dogg:"
+    
+    @inline
+    def test_strip_ns():
+        assert strip_ns(nskey) == "iheard"
+    
+    @inline
+    def test_split_ns():
+        assert tuple(split_ns(nskey)) == ('yo', 'dogg', 'iheard')
+        assert split_ns(nskey) == ['yo', 'dogg', 'iheard']
+    
+    @inline
+    def test_startswith_ns():
+        assert startswith_ns(namespaces, ('yo', 'dogg'))
+    
+    @inline
+    def test_unpack_ns():
+        assert unpack_ns(nskey) == (key, list(namespaces))
+        assert unpack_ns(nskey) == ('iheard', ['yo', 'dogg'])
+    
+    @inline
+    def test_pack_ns():
+        assert pack_ns(key, *namespaces) == nskey
+        assert pack_ns(key, *namespaces) == "yo:dogg:iheard"
+    
+    @inline
+    def test_get_ns():
+        assert get_ns(nskey) == "yo:dogg"
+    
+    @inline
+    def test_get_ns_and_key():
+        assert get_ns_and_key(nskey) == ("yo:dogg", 'iheard')
+    
+    @inline
+    def test_compare_ns():
+        assert compare_ns(namespaces, ('yo', 'dogg'))
+    
+    appname = 'testing'
+    envkey = "TESTING_YO_DOGG_IHEARD"
+    
+    @inline
+    def test_concatenate_env():
+        assert concatenate_env(*namespaces) == "YO_DOGG"
+        assert concatenate_env(*chain(tuplize(appname), namespaces)) == "TESTING_YO_DOGG"
+        assert concatenate_env(*chain(tuplize(appname), namespaces, tuplize(key))) == "TESTING_YO_DOGG_IHEARD"
+    
+    @inline
+    def test_prefix_env():
+        assert prefix_env(appname, *namespaces) == "TESTING_YO_DOGG_"
+    
+    @inline
+    def test_pack_env():
+        assert pack_env(appname, key, *namespaces) == "TESTING_YO_DOGG_IHEARD"
+    
+    @inline
+    def test_unpack_env():
+        assert unpack_env(envkey) == (appname, key, list(namespaces))
+        assert unpack_env(envkey) == ('testing', 'iheard', ['yo', 'dogg'])
+    
+    @inline
+    def test_nskey_from_env():
+        assert nskey_from_env(envkey) == (appname, pack_ns(key, *namespaces))
+        assert nskey_from_env(envkey) == ('testing', "yo:dogg:iheard")
+    
+    @inline
+    def test_nskey_to_env():
+        assert nskey_to_env(appname, nskey) == "TESTING_YO_DOGG_IHEARD"
+    
+    return inline.test(100)
+
+if __name__ == '__main__':
+    sys.exit(test())
