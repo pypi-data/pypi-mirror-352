1ea2c9383991b9c0d220f0c8a7fbeb2a8e90a47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:36 +0000

    Add package dsw-models/4.19.0/dsw_models-4.19.0.tar.gz

---
+++ packages/dsw-models/dsw_models-4.19.0.tar.gz/dsw_models-4.19.0/dsw/models/build_info.py
@@ -0,0 +1,17 @@
+# Generated file
+# - do not overwrite
+# - do not include in git
+from collections import namedtuple
+
+BuildInfo = namedtuple(
+    'BuildInfo',
+    ['version', 'built_at', 'sha', 'branch', 'tag'],
+)
+
+BUILD_INFO = BuildInfo(
+    version='v4.19.0~0cdb9b4',
+    built_at='2025-06-03 07:41:07Z',
+    sha='0cdb9b4e5cc117b0d2465282b274750d80927ace',
+    branch='HEAD',
+    tag='v4.19.0',
+)
