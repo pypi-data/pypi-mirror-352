d8514d732636c66410579f716e39137215951f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:35 +0000

    Add package nonebot-plugin-zzzpanel/0.1.3/nonebot_plugin_zzzpanel-0.1.3.tar.gz

---
+++ packages/nonebot-plugin-zzzpanel/nonebot_plugin_zzzpanel-0.1.3.tar.gz/nonebot_plugin_zzzpanel-0.1.3/nonebot_plugin_zzzpanel/nonebot_plugin_localstore/config.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class Config(BaseModel):
+    localstore_use_cwd: bool = True
+    localstore_cache_dir: Optional[Path] = None
+    localstore_config_dir: Optional[Path] = None
+    localstore_data_dir: Optional[Path] = None
+
+    localstore_plugin_cache_dir: dict[str, Path] = Field(default_factory=dict)
+    localstore_plugin_config_dir: dict[str, Path] = Field(default_factory=dict)
+    localstore_plugin_data_dir: dict[str, Path] = Field(default_factory=dict)
