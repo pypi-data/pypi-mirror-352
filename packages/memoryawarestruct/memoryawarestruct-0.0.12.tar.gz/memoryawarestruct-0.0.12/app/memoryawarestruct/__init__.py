253d1e2de7dba361ed857d840a408182b0665be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:26 +0000

    Add package memoryawarestruct/0.0.12/memoryawarestruct-0.0.12.tar.gz

---
+++ packages/memoryawarestruct/memoryawarestruct-0.0.12.tar.gz/memoryawarestruct-0.0.12/app/memoryawarestruct/__init__.py
@@ -0,0 +1,6 @@
+import os, sys
+path, filename = os.path.split(__file__)
+sys.path.insert(0, path)
+from .utils.typedata import *
+from .utils.middleware import ProtectedDict, ReadOnlyDictView, ProtectedAttribute, metaClass, replace_special_chars, smart_list_to_dict
+from .core.memory import memory, create_secure_memory, SecureMemoryContext
