8f9b30895b1808aa7bab7a6cd3a426ea0af4172c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:55 +0000

    Add package AkvoFormPrint/0.1.0b1/akvoformprint-0.1.0b1-py3-none-any.whl

---
+++ packages/AkvoFormPrint/akvoformprint-0.1.0b1-py3-none-any.whl/AkvoFormPrint/stylers/base_styler.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from AkvoFormPrint.models import FormModel
+
+
+class BaseStyler(ABC):
+    @abstractmethod
+    def render_html(self, form_model: FormModel) -> str:
+        pass
+
+    @abstractmethod
+    def render_pdf(self, form_model: FormModel) -> bytes:
+        pass
