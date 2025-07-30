7c88ebb81fe2168873fdd5c4846f0ea0d14bda5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:58 +0000

    Add package fundamental-data/0.1.6/fundamental_data-0.1.6.tar.gz

---
+++ packages/fundamental-data/fundamental_data-0.1.6.tar.gz/fundamental_data-0.1.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='fundamental_data',
+    version='0.1.6',
+    packages=find_packages(),
+    install_requires=[
+        "ipykernel>=6.29.5",
+        "matplotlib>=3.10.1",
+        "openpyxl>=3.1.5",
+        "pandas>=2.2.3",
+        "requests>=2.32.3",
+        "seaborn>=0.13.2",
+        "yfinance>=0.2.55",
+        "twine>=6.1.0",
+        "plotly>=5.20.0",
+    ],
+    url='https://github.com/mashroor10/fundamental_data/tree/main',
+    license='MIT',
+    description='A Python package for retrieving SEC fundamental data',
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author="Mashroor Rahman",
+)
