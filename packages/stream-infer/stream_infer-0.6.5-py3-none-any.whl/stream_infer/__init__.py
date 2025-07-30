5af112cff39bbe7dbcc88c591b114272c59fe5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:02 +0000

    Add package stream-infer/0.6.5/stream_infer-0.6.5-py3-none-any.whl

---
+++ packages/stream-infer/stream_infer-0.6.5-py3-none-any.whl/stream_infer/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.5.0"
+
+from .inference import Inference
+from .player import Player
+
+try:
+    from .streamlit_ import StreamlitApp
+except ImportError:
+
+    class StreamlitApp:
+        def __init__(self, *args, **kwargs):
+            raise ImportError(
+                "StreamlitApp needs streamlit package support. Please use 'pip install \"stream-infer[server]\"' to install the server version."
+            )
