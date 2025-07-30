20c1d593ff865412a272a0886910735898f61648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:56 +0000

    Add package interviewlib/0.1.0/interviewlib-0.1.0.tar.gz

---
+++ packages/interviewlib/interviewlib-0.1.0.tar.gz/interviewlib-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='interviewlib',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Python interview questions and solutions library',
+    author='Mustakim Shaikh',
+    author_email='mustakim.shaikh.prof@gmail.com',
+    url='https://github.com/yourusername/interviewlib',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",],
+    python_requires='>=3.6',
+)
