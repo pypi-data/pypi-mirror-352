d1ed6d233274e200c0914af77003d5da4930ea0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:16 +0000

    Add package sivalibrary/0.1/sivalibrary-0.1.tar.gz

---
+++ packages/sivalibrary/sivalibrary-0.1.tar.gz/sivalibrary-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(
+    name='sivalibrary',
+    version='0.1',
+    description="Even or Odd identifies",
+    packages=find_packages(),
+    classifiers=[],
+
+)
