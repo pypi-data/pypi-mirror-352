ca264acf5b866559fdf5fa5a06c54b2616b42ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:22:33 +0000

    Add package hypha-artifact/0.0.2/hypha_artifact-0.0.2-py3-none-any.whl

---
+++ packages/hypha-artifact/hypha_artifact-0.0.2-py3-none-any.whl/hypha_artifact/__init__.py
@@ -0,0 +1,7 @@
+"""
+HyphaArtifact module for interacting with Hypha artifacts.
+"""
+
+from .hypha_artifact import HyphaArtifact, ArtifactHttpFile, FileMode, OnError, JsonType
+
+__all__ = ["HyphaArtifact", "ArtifactHttpFile", "FileMode", "OnError", "JsonType"]
