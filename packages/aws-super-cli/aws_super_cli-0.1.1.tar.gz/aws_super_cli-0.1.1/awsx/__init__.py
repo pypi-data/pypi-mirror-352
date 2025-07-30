c59a072b8ea3727b0cb3e21e8deb882f7a763d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:30 +0000

    Add package aws-super-cli/0.1.1/aws_super_cli-0.1.1.tar.gz

---
+++ packages/aws-super-cli/aws_super_cli-0.1.1.tar.gz/aws_super_cli-0.1.1/awsx/__init__.py
@@ -0,0 +1,5 @@
+"""AWS Super CLI - Multi-account AWS resource discovery tool"""
+
+__version__ = "0.1.0"
+__author__ = "Marcelo Acosta"
+__description__ = "See your AWS resources across all accounts in one command" 
