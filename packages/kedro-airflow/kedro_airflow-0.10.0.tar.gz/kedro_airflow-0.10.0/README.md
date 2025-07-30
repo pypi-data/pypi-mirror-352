04594dd27ac917c2382ba8cef32a94936bc39882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:15 +0000

    Add package kedro-airflow/0.10.0/kedro_airflow-0.10.0.tar.gz

---
+++ packages/kedro-airflow/kedro_airflow-0.10.0.tar.gz/kedro_airflow-0.10.0/README.md
@@ -0,0 +1,179 @@
+# Kedro-Airflow
+
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Python Version](https://img.shields.io/badge/python-3.9%20%7C%203.10%20%7C%203.11%20%7C%203.12-blue.svg)](https://pypi.org/project/kedro-airflow/)
+[![PyPI Version](https://badge.fury.io/py/kedro-airflow.svg)](https://pypi.org/project/kedro-airflow/)
+[![Code Style: Black](https://img.shields.io/badge/code%20style-black-black.svg)](https://github.com/ambv/black)
+
+[Apache Airflow](https://github.com/apache/airflow) is a tool for orchestrating complex workflows and data processing pipelines. The Kedro-Airflow plugin can be used for:
+- Rapid pipeline creation in the prototyping phase. You can write Python functions in Kedro without worrying about schedulers, daemons, services or having to recreate the Airflow DAG file.
+- Automatic dependency resolution in Kedro. This allows you to bypass Airflow's need to specify the order of your tasks.
+- Distributing Kedro tasks across many workers. You can also enable monitoring and scheduling of the tasks' runtimes.
+
+## Installation
+
+`kedro-airflow` is a Python plugin. To install it:
+
+```bash
+pip install kedro-airflow
+```
+
+## Usage
+
+You can use `kedro-airflow` to deploy a Kedro pipeline as an Airflow DAG by following these steps:
+
+### Step 1: Generate the DAG file
+
+At the root directory of the Kedro project, run:
+
+```bash
+kedro airflow create
+```
+
+This command will generate an Airflow DAG file located in the `airflow_dags/` directory in your project.
+You can pass a `--pipeline` flag to generate the DAG file for a specific Kedro pipeline and an `--env` flag to generate the DAG file for a specific Kedro environment.
+Passing `--all` will convert all registered Kedro pipelines to Airflow DAGs.
+
+### Step 2: Copy the DAG file to the Airflow DAGs folder.
+
+For more information about the DAGs folder, please visit [Airflow documentation](https://airflow.apache.org/docs/stable/concepts.html#dags).
+The Airflow DAG configuration can be customized by editing this file.
+
+### Step 3: Package and install the Kedro pipeline in the Airflow executor's environment
+
+After generating and deploying the DAG file, you will then need to package and install the Kedro pipeline into the Airflow executor's environment.
+Please visit the guide to [Apache Airflow deployment](https://docs.kedro.org/en/stable/deployment/airflow.html) for more details.
+
+### FAQ
+
+#### What if my DAG file is in a different directory to my project folder?
+
+By default, the generated DAG file is configured to live in the same directory as your project as per this [template](https://github.com/kedro-org/kedro-plugins/blob/main/kedro-airflow/kedro_airflow/airflow_dag_template.j2#L44). If your DAG file is located in a different directory to your project, you will need to tweak this  manually after running the `kedro airflow create` command.
+
+#### What if I want to use a different Jinja2 template?
+
+You can use the additional command line argument `--jinja-file` (alias `-j`) to provide an alternative path to a Jinja2 template. Note that these files have to accept the same variables as those used in the [default Jinja2 template](https://github.com/kedro-org/kedro-plugins/blob/main/kedro-airflow/kedro_airflow/airflow_dag_template.j2).
+
+```bash
+kedro airflow create --jinja-file=./custom/template.j2
+```
+
+#### How can I pass arguments to the Airflow DAGs dynamically?
+
+`kedro-airflow` picks up configuration from `airflow.yml` in `conf/base` or `conf/local` of your Kedro project.
+Or it could be in a folder starting with `airflow`.
+The [parameters](https://docs.kedro.org/en/stable/configuration/parameters.html) are read by Kedro.
+Arguments can be specified globally, or per pipeline:
+
+```yaml
+# Global parameters
+default:
+    start_date: [2023, 1, 1]
+    max_active_runs: 3
+    # https://airflow.apache.org/docs/stable/scheduler.html#dag-runs
+    schedule_interval: "@once"
+    catchup: false
+    # Default settings applied to all tasks
+    owner: "airflow"
+    depends_on_past: false
+    email_on_failure: false
+    email_on_retry: false
+    retries: 1
+    retry_delay: 5
+
+# Arguments specific to the pipeline (overrides the parameters above)
+data_science:
+    owner: "airflow-ds"
+```
+
+Arguments can also be passed via `--params` in the command line:
+
+```bash
+kedro airflow create --params "schedule_interval='@weekly'"
+```
+
+These variables are passed to the Jinja2 template that creates an Airflow DAG from your pipeline.
+
+### What if I want to use a configuration pattern other than `airflow*` and `airflow**`?
+
+In order to configure the config loader, update the `settings.py` file in your Kedro project.
+For instance, if you would like to use the name `scheduler`, then change the file as follows:
+
+```python
+CONFIG_LOADER_ARGS = {"config_patterns": {"airflow": ["scheduler*", "scheduler/**"]}}
+```
+
+Follow Kedro's [official documentation](https://docs.kedro.org/en/stable/configuration/advanced_configuration.html#how-to-do-templating-with-the-omegaconfigloader), to see how to add templating, custom resolvers etc.
+
+#### What if I want to pass different arguments?
+
+In order to pass arguments other than those specified in the default template, simply pass a custom template (see: _"What if I want to use a different Jinja2 template?"_)
+
+The syntax for arguments is:
+```
+{{ argument_name }}
+```
+
+In order to make arguments optional, one can use:
+```
+{{ argument_name | default("default_value") }}
+```
+
+For examples, please have a look at the default template (`airflow_dag_template.j2`).
+
+### What if I want to use a configuration file other than `airflow.yml`?
+
+The default configuration pattern is `["airflow*", "airflow/**"]`.
+In order to configure the `OmegaConfigLoader`, update the `settings.py` file in your Kedro project as follows:
+
+```python
+from kedro.config import OmegaConfigLoader
+
+CONFIG_LOADER_CLASS = OmegaConfigLoader
+CONFIG_LOADER_ARGS = {
+    # other args
+    "config_patterns": {  # configure the pattern for configuration files
+        "airflow": ["airflow*", "airflow/**"]
+    }
+}
+```
+
+Follow Kedro's official documentation, to see how to add templating, custom resolvers etc. (https://docs.kedro.org/en/stable/configuration/advanced_configuration.html#how-to-do-templating-with-the-omegaconfigloader)[https://docs.kedro.org/en/stable/configuration/advanced_configuration.html#how-to-do-templating-with-the-omegaconfigloader]
+
+#### How can I use Airflow runtime parameters?
+
+It is possible to pass parameters when triggering an Airflow DAG from the user interface.
+In order to use this feature, create a custom template using the [Params syntax](https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/params.html).
+See ["What if I want to use a different Jinja2 template?"](#what-if-i-want-to-use-a-different-jinja2-template) for instructions on using custom templates.
+
+#### What if I want to use a different Airflow Operator?
+
+Which Airflow Operator to use depends on the environment your project is running in.
+You can set the operator to use by providing a custom template.
+See ["What if I want to use a different Jinja2 template?"](#what-if-i-want-to-use-a-different-jinja2-template) for instructions on using custom templates.
+The [rich offering](https://airflow.apache.org/docs/apache-airflow-providers/operators-and-hooks-ref/index.html) of operators means that the `kedro-airflow` plugin is providing templates for specific operators.
+The default template provided by `kedro-airflow` uses the `BaseOperator`.
+
+### Can I group nodes together?
+
+When running Kedro nodes using Airflow, MemoryDatasets are often not shared across operators.
+This will cause the DAG run to fail.
+
+MemoryDatasets may be used to provide logical separation between nodes in Kedro, without the overhead of needing to write to disk (and in the case of distributed running needing multiple executors).
+
+Nodes that are connected through MemoryDatasets are grouped together via the `--group-in-memory` flag.
+This preserves the option to have logical separation in Kedro, with little computational overhead.
+
+It is possible to use [task groups](https://docs.astronomer.io/learn/task-groups) by changing the template.
+See ["What if I want to use a different Jinja2 template?"](#what-if-i-want-to-use-a-different-jinja2-template) for instructions on using custom templates.
+
+## Can I contribute?
+
+Yes! Want to help build Kedro-Airflow? Check out our guide to [contributing](https://github.com/kedro-org/kedro-plugins/blob/main/kedro-airflow/CONTRIBUTING.md).
+
+## What licence do you use?
+
+Kedro-Airflow is licensed under the [Apache 2.0](https://github.com/kedro-org/kedro-plugins/blob/main/LICENSE.md) License.
+
+## Python version support policy
+* The [Kedro-Airflow](https://github.com/kedro-org/kedro-plugins/tree/main/kedro-airflow) supports all Python versions that are actively maintained by the CPython core team. When a [Python version reaches end of life](https://devguide.python.org/versions/#versions), support for that version is dropped from `kedro-airflow`. This is not considered a breaking change.
---
+++ packages/kedro-airflow/kedro_airflow-0.10.0.tar.gz/kedro_airflow-0.10.0/tests/test_plugin.py
@@ -0,0 +1,418 @@
+from __future__ import annotations
+
+import shutil
+from pathlib import Path
+from typing import Any
+
+import pytest
+import yaml
+
+from kedro_airflow.plugin import commands
+
+
+@pytest.mark.parametrize(
+    "pipeline_name,command,expected_airflow_dag",
+    [
+        # Test normal execution
+        (
+            "__default__",
+            ["airflow", "create"],
+            'tasks["node0"] >> tasks["node1"]',
+        ),
+        # Test execution with alternate pipeline name
+        (
+            "ds",
+            ["airflow", "create", "--pipeline", "ds"],
+            'tasks["node0"] >> tasks["node1"]',
+        ),
+        # Test with grouping
+        # All the datasets are MemoryDataset(), so we have only one joined node without dependencies
+        (
+            "__default__",
+            ["airflow", "create", "--group-by", "memory"],
+            'task_id="node0-node1-node2-node3-node4",',
+        ),
+        # As the example pipeline is not namespaced, each node is treated as a separate task.
+        (
+            "__default__",
+            ["airflow", "create", "--group-by", "namespace"],
+            'task_id="node0",',
+        ),
+    ],
+)
+def test_create_airflow_dag(
+    pipeline_name, command, expected_airflow_dag, cli_runner, metadata
+):
+    """Check the generation and validity of a simple Airflow DAG."""
+    dag_name = "fake_project"
+    dag_file = (
+        metadata.project_path
+        / "airflow_dags"
+        / (
+            f"{dag_name}_dag.py"
+            if pipeline_name == "__default__"
+            else f"{dag_name}_{pipeline_name}_dag.py"
+        )
+    )
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+
+    with dag_file.open(encoding="utf-8") as f:
+        dag_code = [line.strip() for line in f.read().splitlines()]
+
+    assert expected_airflow_dag in dag_code
+    dag_file.unlink()
+
+
+def _create_kedro_airflow_yml(file_name: Path, content: dict[str, Any]):
+    file_name.parent.mkdir(parents=True, exist_ok=True)
+    with file_name.open("w") as fp:
+        yaml.dump(content, fp)
+
+
+def test_airflow_config_params(cli_runner, metadata):
+    """Check if config variables are picked up"""
+    dag_name = "fake_project"
+    template_name = "airflow_params.j2"
+    content = "{{ owner | default('hello')}}"
+
+    _create_kedro_airflow_jinja_template(Path.cwd(), template_name, content)
+
+    # default
+    default_content = "hello"
+    command = ["airflow", "create", "-j", template_name]
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+    assert dag_file.read_text() == default_content
+    dag_file.unlink()
+
+
+def test_airflow_config_params_cli(cli_runner, metadata):
+    """Check if config variables are picked up"""
+    dag_name = "fake_project"
+    template_name = "airflow_params.j2"
+    content = "{{ owner | default('hello')}}"
+
+    _create_kedro_airflow_jinja_template(Path.cwd(), template_name, content)
+
+    # "--params"
+    expected_content = "testme"
+    command = ["airflow", "create", "--params", "owner=testme", "-j", template_name]
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+    assert dag_file.read_text() == expected_content
+    dag_file.unlink()
+
+
+def test_airflow_config_params_from_config(cli_runner, metadata):
+    """Check if config variables are picked up"""
+    dag_name = "fake_project"
+    template_name = "airflow_params.j2"
+    content = "{{ owner | default('hello')}}"
+
+    _create_kedro_airflow_jinja_template(Path.cwd(), template_name, content)
+
+    # airflow.yml
+    expected_content = "someone else"
+    file_name = Path.cwd() / "conf" / "base" / "airflow.yml"
+    _create_kedro_airflow_yml(file_name, {"default": {"owner": expected_content}})
+    command = ["airflow", "create", "-j", template_name]
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+    assert dag_file.read_text() == expected_content
+    file_name.unlink()
+    dag_file.unlink()
+
+    # ../airflow.yml
+    expected_content = "yet someone else"
+    file_name = Path.cwd() / "conf" / "base" / "airflow" / "default.yml"
+    _create_kedro_airflow_yml(file_name, {"default": {"owner": expected_content}})
+    command = ["airflow", "create", "-j", template_name]
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+    assert dag_file.read_text() == expected_content
+    file_name.unlink()
+
+
+def test_airflow_config_params_from_config_non_default(cli_runner, metadata):
+    """Check if config variables are picked up"""
+    dag_name = "fake_project"
+    template_name = "airflow_params.j2"
+    content = "{{ owner | default('hello')}}"
+    default_content = "hello"
+
+    _create_kedro_airflow_jinja_template(Path.cwd(), template_name, content)
+
+    # random.yml
+    expected_content = "yet someone else again"
+    file_name = Path.cwd() / "conf" / "base" / "random.yml"
+    _create_kedro_airflow_yml(file_name, {"default": {"owner": expected_content}})
+    command = ["airflow", "create", "-j", template_name]
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+    assert dag_file.read_text() == default_content
+    dag_file.unlink()
+
+    # scheduler.yml
+    file_name = Path.cwd() / "conf" / "base" / "scheduler.yml"
+    _create_kedro_airflow_yml(file_name, {"default": {"owner": expected_content}})
+    command = ["airflow", "create", "-j", template_name]
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+    assert dag_file.read_text() == expected_content
+    dag_file.unlink()
+    file_name.unlink()
+
+
+def test_airflow_config_params_env(cli_runner, metadata):
+    """Check if config variables are picked up"""
+    dag_name = "fake_project"
+    template_name = "airflow_params.j2"
+    content = "{{ owner | default('hello')}}"
+
+    _create_kedro_airflow_jinja_template(Path.cwd(), template_name, content)
+
+    # env
+    expected_content = "again someone else"
+    file_name = Path.cwd() / "conf" / "local" / "airflow.yml"
+    _create_kedro_airflow_yml(file_name, {"default": {"owner": expected_content}})
+    command = ["airflow", "create", "-j", template_name, "-e", "local"]
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+    assert dag_file.read_text() == expected_content
+    dag_file.unlink()
+
+
+def test_airflow_config_params_custom_pipeline(cli_runner, metadata):
+    """Check if config variables are picked up"""
+    dag_name = "fake_project"
+    template_name = "airflow_params.j2"
+    content = "{{ owner | default('hello')}}"
+
+    _create_kedro_airflow_jinja_template(Path.cwd(), template_name, content)
+
+    # custom pipeline name
+    expected_content = "finally someone else"
+    file_name = Path.cwd() / "conf" / "base" / "airflow.yml"
+    _create_kedro_airflow_yml(
+        file_name, {"default": {"owner": "foobar"}, "ds": {"owner": expected_content}}
+    )
+    command = ["airflow", "create", "-j", template_name, "-p", "ds"]
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_ds_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+    assert dag_file.read_text() == expected_content
+    dag_file.unlink()
+
+
+def _create_kedro_airflow_jinja_template(path: Path, name: str, content: str):
+    (path / name).write_text(content)
+
+
+def test_custom_template_exists(cli_runner, metadata):
+    """Test execution with different dir and filename for Jinja2 Template"""
+    dag_name = "fake_project"
+    template_name = "custom_template.j2"
+    command = ["airflow", "create", "-j", template_name]
+    content = "print('my custom dag')"
+    # because there are no jinja variables
+    expected_content = content
+
+    _create_kedro_airflow_jinja_template(Path.cwd(), template_name, content)
+
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+    assert dag_file.read_text() == expected_content
+
+
+def test_custom_template_nonexistent(cli_runner, metadata):
+    """Test execution with different dir and filename for Jinja2 Template"""
+    template_name = "non_existent_custom_template.j2"
+    command = ["airflow", "create", "-j", template_name]
+    result = cli_runner.invoke(commands, command, obj=metadata)
+    assert result.exit_code == 2
+    assert (
+        f"Error: Invalid value for '-j' / '--jinja-file': File '{template_name}' does not exist."
+        in result.stdout
+    )
+
+
+def _kedro_create_env(project_root: Path, env: str):
+    (project_root / "conf" / env).mkdir(parents=True)
+
+
+def _kedro_remove_env(project_root: Path, env: str):
+    shutil.rmtree(project_root / "conf" / env)
+
+
+def test_create_airflow_dag_env_parameter_exists(cli_runner, metadata):
+    """Test the `env` parameter"""
+    dag_name = "fake_project"
+    command = ["airflow", "create", "--env", "remote"]
+
+    _kedro_create_env(Path.cwd(), "remote")
+
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_remote_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+
+    expected_airflow_dag = 'tasks["node0"] >> tasks["node1"]'
+    with dag_file.open(encoding="utf-8") as f:
+        dag_code = [line.strip() for line in f.read().splitlines()]
+    assert expected_airflow_dag in dag_code
+
+    _kedro_remove_env(Path.cwd(), "remote")
+
+
+@pytest.mark.parametrize(
+    "tags, expected_airflow_dags, unexpected_airflow_dags",
+    [
+        # Test one tag
+        (
+            ["--tags", "tag0"],
+            ['tasks["node0"] >> tasks["node2"]'],
+            ['tasks["node0"] >> tasks["node1"]'],
+        ),
+        # Test few tags with whitespaces
+        (
+            ["--tags", "tag0 , tag1"],
+            ['tasks["node0"] >> tasks["node2"]', 'tasks["node0"] >> tasks["node3"]'],
+            ['tasks["node0"] >> tasks["node1"]', 'tasks["node0"] >> tasks["node4"]'],
+        ),
+    ],
+)
+def test_create_airflow_dag_tags_parameter_exists(
+    tags, expected_airflow_dags, unexpected_airflow_dags, cli_runner, metadata
+):
+    """Test the `tags` parameter"""
+    dag_name = "fake_project"
+    command = ["airflow", "create", "--env", "remote"] + tags
+
+    _kedro_create_env(Path.cwd(), "remote")
+
+    dag_file = Path.cwd() / "airflow_dags" / f"{dag_name}_remote_dag.py"
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+    assert dag_file.exists()
+
+    with dag_file.open(encoding="utf-8") as f:
+        dag_code = [line.strip() for line in f.read().splitlines()]
+    for expected_dag in expected_airflow_dags:
+        assert expected_dag in dag_code
+    for unexpected_dag in unexpected_airflow_dags:
+        assert unexpected_dag not in dag_code
+
+    _kedro_remove_env(Path.cwd(), "remote")
+
+
+def test_create_airflow_dag_nonexistent_pipeline(cli_runner, metadata):
+    """Test executing with a non-existing pipeline"""
+    command = ["airflow", "create", "--pipeline", "de"]
+    result = cli_runner.invoke(commands, command, obj=metadata)
+    assert result.exit_code == 1
+    assert (
+        "kedro.framework.cli.utils.KedroCliError: Pipeline de not found."
+        in result.stdout
+    )
+
+
+def test_create_airflow_all_dags(cli_runner, metadata):
+    command = ["airflow", "create", "--all"]
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0, (result.exit_code, result.stdout)
+
+    for dag_name, pipeline_name in [
+        ("fake_project", "__default__"),
+        ("fake_project", "ds"),
+    ]:
+        dag_file = (
+            metadata.project_path
+            / "airflow_dags"
+            / (
+                f"{dag_name}_dag.py"
+                if pipeline_name == "__default__"
+                else f"{dag_name}_{pipeline_name}_dag.py"
+            )
+        )
+        assert dag_file.exists()
+
+        expected_airflow_dag = 'tasks["node0"] >> tasks["node1"]'
+        with dag_file.open(encoding="utf-8") as f:
+            dag_code = [line.strip() for line in f.read().splitlines()]
+        assert expected_airflow_dag in dag_code
+        dag_file.unlink()
+
+
+def test_create_airflow_all_and_pipeline(cli_runner, metadata):
+    command = ["airflow", "create", "--all", "-p", "ds"]
+    result = cli_runner.invoke(commands, command, obj=metadata)
+    assert result.exit_code == 2
+    assert (
+        "Error: Invalid value: The `--all` and `--pipeline` option are mutually exclusive."
+        in result.stdout
+    )
+
+
+def test_create_airflow_conf_source(cli_runner, metadata):
+    command = ["airflow", "create", "--conf-source", "conf"]
+    result = cli_runner.invoke(commands, command, obj=metadata)
+    assert result.exit_code == 0
+    dag_file = metadata.project_path / "airflow_dags" / "fake_project_dag.py"
+
+    assert dag_file.exists()
+
+    expected_airflow_dag = 'conf_source = "conf" or Path.cwd() / "conf"'
+    with dag_file.open(encoding="utf-8") as f:
+        dag_code = [line.strip() for line in f.read().splitlines()]
+        assert expected_airflow_dag in dag_code
+    dag_file.unlink()
+
+
+@pytest.mark.parametrize("value", ["memory", "MEMORY", "namespace", "NaMeSpAcE"])
+def test_group_by_valid_commands(cli_runner, metadata, value):
+    command = ["airflow", "create", "--group-by", value]
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 0
+
+
+def test_group_by_invalid_value(cli_runner, metadata):
+    command = ["airflow", "create", "--group-by", "asdasdasd"]
+    result = cli_runner.invoke(commands, command, obj=metadata)
+
+    assert result.exit_code == 2
+    assert (
+        "Error: Invalid value for '-g' / '--group-by': 'asdasdasd' is not one of 'memory', 'namespace'."
+        in result.stdout
+    )
