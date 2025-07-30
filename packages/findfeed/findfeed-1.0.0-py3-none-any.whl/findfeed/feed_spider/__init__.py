6c4ae3ba299326389af0fd2d18e7ff59c9398ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:08:33 +0000

    Add package findfeed/1.0.0/findfeed-1.0.0-py3-none-any.whl

---
+++ packages/findfeed/findfeed-1.0.0-py3-none-any.whl/findfeed/feed_spider/__init__.py
@@ -0,0 +1,5 @@
+from findfeed.feed_spider.feed_info import FeedInfo
+from findfeed.feed_spider.site_meta import SiteMeta
+from findfeed.feed_spider.spider import FeedsearchSpider
+
+__all__ = ["FeedsearchSpider", "FeedInfo", "SiteMeta"]
