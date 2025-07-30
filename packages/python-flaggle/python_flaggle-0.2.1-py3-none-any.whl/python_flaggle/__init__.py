5e6fb18dbde5213614a8c762606a13fc6d974855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:47 +0000

    Add package python-flaggle/0.2.1/python_flaggle-0.2.1-py3-none-any.whl

---
+++ packages/python-flaggle/python_flaggle-0.2.1-py3-none-any.whl/python_flaggle/__init__.py
@@ -0,0 +1,7 @@
+from python_flaggle.flaggle import Flaggle
+from python_flaggle.flag import Flag, FlagOperation, FlagType
+
+__all__ = ["FlagType", "FlagOperation", "Flag", "Flaggle"]
+__version__ = "0.2.1"
+__author__ = "Asaph Diniz"
+__email__ = "contato@asaph.dev.br"
