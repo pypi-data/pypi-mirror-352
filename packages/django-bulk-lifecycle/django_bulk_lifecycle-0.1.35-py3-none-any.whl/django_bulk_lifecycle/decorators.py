8b1da1ed775769e7d5c0d8389c91017c9b3793d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:39 +0000

    Add package django-bulk-lifecycle/0.1.35/django_bulk_lifecycle-0.1.35-py3-none-any.whl

---
+++ packages/django-bulk-lifecycle/django_bulk_lifecycle-0.1.35-py3-none-any.whl/django_bulk_lifecycle/decorators.py
@@ -0,0 +1,10 @@
+def hook(event, *, model, condition=None, priority=0):
+    """
+    Decorator to annotate a method with multiple lifecycle hook registrations.
+    """
+    def decorator(fn):
+        if not hasattr(fn, "lifecycle_hooks"):
+            fn.lifecycle_hooks = []
+        fn.lifecycle_hooks.append((model, event, condition, priority))
+        return fn
+    return decorator
