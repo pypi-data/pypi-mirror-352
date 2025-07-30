24a8e271ff022087d2c98ca8b32a27fe131b4c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:11 +0000

    Add package django-users-auth/2.4.15/django_users_auth-2.4.15-py3-none-any.whl

---
+++ packages/django-users-auth/django_users_auth-2.4.15-py3-none-any.whl/users/settings.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+
+DEFAULTS = {
+    "SECRET_KEY": "default-secret-key",
+    "TOKEN_ALGORITHM": "HS256",
+    "PUBLIC_KEY": None,
+    "OIDC_OP_BASE_URL": ""
+}
+
+class UsersAuthSettings:
+    def __init__(self, defaults=None):
+        self.defaults = defaults or DEFAULTS
+
+    def __getattr__(self, attr):
+        if hasattr(settings, "DJANGO_USERS_AUTH_TOKEN"):
+            return settings.USERS_AUTH.get(attr, self.defaults.get(attr))
+        return self.defaults.get(attr)
+
+users_auth_settings = UsersAuthSettings()
+
+
