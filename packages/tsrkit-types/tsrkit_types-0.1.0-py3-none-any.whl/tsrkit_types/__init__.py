5a65154e88fb8cd917e9e78869a460e20335d361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:54 +0000

    Add package tsrkit-types/0.1.0/tsrkit_types-0.1.0-py3-none-any.whl

---
+++ packages/tsrkit-types/tsrkit_types-0.1.0-py3-none-any.whl/tsrkit_types/__init__.py
@@ -0,0 +1,76 @@
+# from jam.types.base.integers import (
+#     U8,
+#     U16,
+#     U32,
+#     U64,
+#     U128,
+#     U256,
+#     Int,
+# )
+# from jam.types.base.string import String
+# from jam.types.base.null import Null, Nullable
+# from jam.types.base.dictionary import Dictionary, decodable_dictionary
+# from jam.types.base.boolean import Boolean
+# from jam.types.base.bit import Bit
+# from jam.types.base.composite import Choice, Option, decodable_choice, decodable_option
+# from jam.types.base.sequences.array import Array, decodable_array
+# from jam.types.base.sequences.vector import Vector, decodable_vector
+# from jam.types.base.bytes.bytes import Bytes
+# # from jam.types.base.sequences.bytes import BitArray, decodable_bit_array, Byte
+# # from jam.types.base.bytes.byte_array import (
+# #     ByteArray8,
+# #     ByteArray16,
+# #     ByteArray32,
+# #     ByteArray64,
+# #     ByteArray96,
+# #     ByteArray128,
+# #     ByteArray144,
+# #     ByteArray256,
+# #     ByteArray784,
+# # )
+#
+# __all__ = [
+#     # Integer types
+#     "Int",
+#     "U8",
+#     "U16",
+#     "U32",
+#     "U64",
+#     "U128",
+#     "U256",
+#     # Choice and Null types
+#     "Choice",
+#     "Option",
+#     "Null",
+#     "Nullable",
+#     # Dictionary type
+#     "Dictionary",
+#     # Boolean and Bit types
+#     "Boolean",
+#     "Bit",
+#     # String type
+#     "String",
+#     # Sequence types
+#     # "Array",
+#     # "Vector",
+#     # # Byte types
+#     # "ByteArray8",
+#     # "ByteArray16",
+#     # "ByteArray32",
+#     # "ByteArray64",
+#     # "ByteArray96",
+#     # "ByteArray128",
+#     # "ByteArray144",
+#     # "ByteArray256",
+#     # "ByteArray784",
+#     # "BitArray",
+#     # "Byte",
+#     "Bytes",
+#     # Decodable types
+#     "decodable_array",
+#     "decodable_bit_array",
+#     "decodable_vector",
+#     "decodable_dictionary",
+#     "decodable_choice",
+#     "decodable_option",
+# ]
