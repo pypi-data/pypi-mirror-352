2d2548b6cc9f7902c37410489ba1d2926015c6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:08:08 +0000

    Add package korlibrarycheater/0.1.2/korlibrarycheater-0.1.2.tar.gz

---
+++ packages/korlibrarycheater/korlibrarycheater-0.1.2.tar.gz/korlibrarycheater-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="korlibrarycheater",  # PyPI에 중복 없는 이름
+    version="0.1.2",
+    packages=find_packages(),
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'cheatlib=korlibrarycheater.KorLibraryCheater:sk_library_cheater',  # sk_library_cheater 함수로 변경
+        ],
+    },
+    author="ppenn0411",
+    description="파이썬 라이브러리 탐색 도구",
+    url="https://github.com/ppenn0411/KorLibraryCheater",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+) 
