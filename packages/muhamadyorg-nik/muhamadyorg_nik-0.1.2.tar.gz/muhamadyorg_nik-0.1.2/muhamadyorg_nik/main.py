fea7360a0cdea16308e4e36b6050fafd4e969959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:54 +0000

    Add package muhamadyorg-nik/0.1.2/muhamadyorg_nik-0.1.2.tar.gz

---
+++ packages/muhamadyorg-nik/muhamadyorg_nik-0.1.2.tar.gz/muhamadyorg_nik-0.1.2/muhamadyorg_nik/main.py
@@ -0,0 +1,14 @@
+import pyfiglet
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Figlet-style ASCII banner generator")
+    parser.add_argument("text", nargs="*", help="Text to convert into ASCII art")
+    args = parser.parse_args()
+
+    if args.text:
+        text = " ".join(args.text)
+    else:
+        text = input("nik : ")
+
+    print(pyfiglet.figlet_format(text))
