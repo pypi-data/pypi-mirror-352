3a58a6d6cf3f1de2ecddedf7266612f7d0657285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:00 +0000

    Add package muoblpbindings/0.0.10/muoblpbindings-0.0.10.tar.gz

---
+++ packages/muoblpbindings/muoblpbindings-0.0.10.tar.gz/muoblpbindings-0.0.10/src/mes_add1.hpp
@@ -0,0 +1,11 @@
+#include <vector> // Required for std::vector
+#include <string> // Required for std::string
+#include <unordered_map> // Required for std::unordered_map
+#include <utility> // Required for std::pair
+
+std::vector<std::string> equal_shares_add1(
+    const std::vector<std::string>& voters,
+    const std::vector<std::string>& projects,
+    const std::unordered_map<std::string, double>& cost,
+    std::unordered_map<std::string, std::vector<std::pair<std::string, double>>> approvers_utilities,
+    double total_budget);
