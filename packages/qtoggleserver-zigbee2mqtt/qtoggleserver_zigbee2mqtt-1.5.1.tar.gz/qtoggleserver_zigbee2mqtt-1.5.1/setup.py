0191b750984b9d78f7f831c4375b50fe9c51ac25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:44 +0000

    Add package qtoggleserver-zigbee2mqtt/1.5.1/qtoggleserver_zigbee2mqtt-1.5.1.tar.gz

---
+++ packages/qtoggleserver-zigbee2mqtt/qtoggleserver_zigbee2mqtt-1.5.1.tar.gz/qtoggleserver_zigbee2mqtt-1.5.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name='qtoggleserver-zigbee2mqtt',
+    version='1.5.1',
+    description='qToggleServer integration with Zigbee2MQTT',
+    author='Calin Crisan',
+    author_email='ccrisan@gmail.com',
+    license='Apache 2.0',
+
+    packages=find_namespace_packages(),
+
+    install_requires=[
+        'aiomqtt>=2.0',
+    ]
+)
