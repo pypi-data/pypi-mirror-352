1b085914930e8bfb944b94f21be1cff609e6c810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:58 +0000

    Add package maleo-identity/0.0.54/maleo_identity-0.0.54-py3-none-any.whl

---
+++ packages/maleo-identity/maleo_identity-0.0.54-py3-none-any.whl/maleo_identity/models/tables/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from .organization import OrganizationsTable
+from .organization_role import OrganizationRolesTable
+from .user import UsersTable
+from .user_profile import UserProfilesTable
+from .user_system_role import UserSystemRolesTable
+from .user_organization import UserOrganizationsTable
+from .user_organization_role import UserOrganizationRolesTable
+
+class MaleoIdentityTables:
+    Organization = OrganizationsTable
+    OrganizationRole = OrganizationRolesTable
+    User = UsersTable
+    UserProfile = UserProfilesTable
+    UserSystemRole = UserSystemRolesTable
+    UserOrganization = UserOrganizationsTable
+    UserOrganizationRole = UserOrganizationRolesTable
