568a824f4aa35fa7648d5fbf8a7f5e2f6499c7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:59 +0000

    Add package comfyui-frontend-package/1.22.0/comfyui_frontend_package-1.22.0.tar.gz

---
+++ packages/comfyui-frontend-package/comfyui_frontend_package-1.22.0.tar.gz/comfyui_frontend_package-1.22.0/comfyui_frontend_package/static/assets/DesktopStartView-D5w_aSLl.js
@@ -0,0 +1,22 @@
+import { defineComponent, openBlock, createBlock, withCtx, createVNode, unref } from "./vendor-vue-B7YUw5vA.js";
+import { script$29 as script } from "./vendor-primevue-CBB09Bln.js";
+import { _sfc_main as _sfc_main$1 } from "./BaseViewTemplate-T3J2Na2f.js";
+import "./index-Co5rVbXs.js";
+import "./vendor-vue-i18n-CdFxvEOa.js";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "DesktopStartView",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(_sfc_main$1, { dark: "" }, {
+        default: withCtx(() => [
+          createVNode(unref(script), { class: "m-8 w-48 h-48" })
+        ]),
+        _: 1
+      });
+    };
+  }
+});
+export {
+  _sfc_main as default
+};
+//# sourceMappingURL=DesktopStartView-D5w_aSLl.js.map
