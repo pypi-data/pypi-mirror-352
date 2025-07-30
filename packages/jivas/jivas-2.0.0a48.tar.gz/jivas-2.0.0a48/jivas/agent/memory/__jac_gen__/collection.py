51e918dab0396cc5f4ee496ed60911a47a1115a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:18 +0000

    Add package jivas/2.0.0a48/jivas-2.0.0a48.tar.gz

---
+++ packages/jivas/jivas-2.0.0a48.tar.gz/jivas-2.0.0a48/jivas/agent/memory/__jac_gen__/collection.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from jaclang import *
+import typing
+if typing.TYPE_CHECKING:
+    from jivas.agent.core.graph_node import GraphNode
+else:
+    GraphNode, = jac_import('jivas.agent.core.graph_node', items={'GraphNode': None})
+
+class Collection(GraphNode, Node):
+    name: str = field('')
+    data: dict = field(gen=lambda: {})
+
+    def get_name(self) -> str:
+        return self.name
+
+    def set_name(self, name: str) -> None:
+        self.name = name
+
+    def get_data_item(self, label: str) -> any:
+        return self.data.get(label, None)
+
+    def set_data_item(self, label: str, value: any) -> None:
+        self.data[label] = value
