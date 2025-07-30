7dc383432ba5f7971ac930998f084c9505888d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:08:31 +0000

    Add package diffindiff/2.0.6/diffindiff-2.0.6.tar.gz

---
+++ packages/diffindiff/diffindiff-2.0.6.tar.gz/diffindiff-2.0.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+def read_README():
+    with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+        return f.read()
+    
+setup(
+    name='diffindiff',
+    version='2.0.6',
+    description='diffindiff: Python library for convenient Difference-in-Differences Analyses',
+    packages=find_packages(include=["diffindiff", "diffindiff.tests"]),
+    include_package_data=True,
+    long_description=read_README(),
+    long_description_content_type='text/markdown',
+    author='Thomas Wieland',
+    author_email='geowieland@googlemail.com',
+    license_files=["LICENSE"],
+    package_data={
+        'diffindiff': ['tests/data/*'],
+    },
+    install_requires=[
+        'numpy',
+        'pandas',
+        'statsmodels',
+        'matplotlib',
+        'datetime',
+        'scikit-learn',
+        'xgboost',
+        'lightgbm'
+    ],
+    test_suite='tests',
+)
