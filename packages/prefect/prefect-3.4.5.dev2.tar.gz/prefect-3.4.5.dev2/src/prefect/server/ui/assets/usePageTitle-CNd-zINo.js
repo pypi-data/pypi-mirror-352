7c99285f4587085dd9702b65210b34caa5058a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:29 +0000

    Add package prefect/3.4.5.dev2/prefect-3.4.5.dev2.tar.gz

---
+++ packages/prefect/prefect-3.4.5.dev2.tar.gz/prefect-3.4.5.dev2/src/prefect/server/ui/assets/usePageTitle-CNd-zINo.js
@@ -0,0 +1,2 @@
+import{h as a,m as c,bw as n}from"./index-BpsoK7wD.js";function i(...t){const r=[...t,"Prefect Server"],o=a(()=>r.map(e=>c(e)).filter(e=>e!==null).join(" • "));n(()=>document.title=o.value)}export{i as u};
+//# sourceMappingURL=usePageTitle-CNd-zINo.js.map
