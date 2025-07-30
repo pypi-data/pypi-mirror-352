b9e9993bd1fec1ce7aa606379926c643d6c51edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:01 +0000

    Add package listless/0.3.0/listless-0.3.0-py3-none-any.whl

---
+++ packages/listless/listless-0.3.0-py3-none-any.whl/listless/__main__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""pkg entry ~ `python -m listless`"""
+
+from __future__ import annotations
+
+import sys
+
+from listless.__about__ import __pkgroot__, __title__, __version__
+
+
+def main() -> None:
+    import json
+
+    sys.stdout.write(
+        json.dumps(
+            {"package": __title__, "version": __version__, "pkgroot": __pkgroot__},
+            indent=2,
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
