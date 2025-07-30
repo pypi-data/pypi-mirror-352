b25d6cbd395f7d0bd8dc463f638dd9a2e0bd25f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:50 +0000

    Add package package-events-bus/0.1.26/package_events_bus-0.1.26.tar.gz

---
+++ packages/package-events-bus/package_events_bus-0.1.26.tar.gz/package_events_bus-0.1.26/events_bus/core/contracts/handler.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+from .. import BaseEvent
+
+
+class BaseHandler(ABC):
+    """
+    Abstract base class for event handlers.
+    This class defines the interface for handling events.
+    Subclasses should implement the `handle` method to process the event.
+    Attributes:
+        event_type (type[BaseEvent]):
+        The type of event that this handler can process.
+    """
+    @abstractmethod
+    def handle(self, event: BaseEvent) -> None:
+        pass
+
+
+class SyncHandler(BaseHandler):
+    """
+    Synchronous event handler.
+    This class implements the `handle` method to process events synchronously.
+    """
+
+    def handle(self, event: BaseEvent) -> None:
+        """Handle the event synchronously.
+        Args:
+            event (BaseEvent): The event data to handle.
+        """
+        pass
+
+
+class AsyncHandler(BaseHandler):
+    """
+    Asynchronous event handler.
+    This class implements the `handle` method to process events asynchronously.
+    """
+
+    async def handle(self, event: BaseEvent) -> None:
+        """Handle the event asynchronously.
+        Args:
+            event (BaseEvent): The event event to handle.
+        """
+        pass
+        # Implement asynchronous event handling logic here
+        # For example, you can process the event data and perform actions
+        # based on the event type and data.
---
+++ packages/package-events-bus/package_events_bus-0.1.26.tar.gz/package_events_bus-0.1.26/events_bus/core/runtime/event_register.py
@@ -0,0 +1,58 @@
+from .. import BaseHandler
+
+
+class EventHandlerRegistry:
+    """Registry for event handlers.
+    This class is responsible for registering event handlers and
+    providing access to them.
+    """
+    _registry_queues: dict[str, BaseHandler] = {}
+    _registry_handlers: dict[str, BaseHandler] = {}
+
+    @classmethod
+    def register_by_queue(cls, queue_url: str, handler: BaseHandler):
+        """Register an event handler associated with a specific queue URL.
+        Args:
+            queue_url (str): The URL of the queue to which the handler is
+            associated.
+            handler (BaseHandler): The event handler to register.
+        """
+        cls._registry_queues[queue_url] = handler
+
+    @classmethod
+    def register_handler(cls, event_name: str, handler: BaseHandler):
+        """Register an event handler without a specific queue URL.
+        Args:
+            handler (BaseHandler): The event handler to register.
+        """
+        if not isinstance(handler, BaseHandler):
+            raise TypeError(f"Expected a BaseHandler, got {type(handler)}")
+        cls._registry_handlers[event_name] = handler
+
+    @classmethod
+    def register_multiple_handlers(cls, handlers: dict[str, BaseHandler]):
+        """Register multiple event handlers at once.
+        Args:
+            handlers (dict[str, BaseHandler]): A dictionary mapping event
+            names to their associated event handlers.
+        """
+        for event_name, handler in handlers.items():
+            cls.register_handler(event_name, handler)
+
+    @classmethod
+    def get_handlers_with_queues(cls) -> dict[str, BaseHandler]:
+        """Get all registered event handlers.
+        Returns:
+            dict[str, BaseHandler]: A dictionary mapping queue URLs to
+            their associated event handlers.
+        """
+        return cls._registry_queues
+
+    @classmethod
+    def get_handlers_by_event(cls) -> dict[str, BaseHandler]:
+        """Get all registered event handlers.
+        Returns:
+            dict[str, BaseHandler]: A dictionary mapping event names to
+            their associated event handlers.
+        """
+        return cls._registry_handlers
