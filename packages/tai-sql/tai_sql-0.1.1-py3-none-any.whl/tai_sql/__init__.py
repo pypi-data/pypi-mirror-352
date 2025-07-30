f503feeaac8993152ac515e7e28ea59e8fbfc059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:39 +0000

    Add package tai-sql/0.1.1/tai_sql-0.1.1-py3-none-any.whl

---
+++ packages/tai-sql/tai_sql-0.1.1-py3-none-any.whl/tai_sql/__init__.py
@@ -0,0 +1,26 @@
+"""
+Declarative models for SQLAlchemy.
+This module provides the base classes and utilities to define
+models using SQLAlchemy's declarative system.
+"""
+from __future__ import annotations
+
+# Importar la instancia global
+from .manager import db
+from .core import datasource, generate, env, connection_string, params
+from .orm import Table, column, relation
+from sqlalchemy.types import BigInteger as bigint
+
+# Exportar los elementos principales
+__all__ = [
+    'db',
+    'datasource', 
+    'generate',
+    'env',
+    'connection_string',
+    'params',
+    'Table',
+    'column',
+    'relation',
+    'bigint'
+]
---
+++ packages/tai-sql/tai_sql-0.1.1-py3-none-any.whl/tai_sql/orm/mappers/__init__.py
@@ -0,0 +1,9 @@
+from .table import Table
+from .columns import column
+from .relations import relation
+
+__all__ = [
+    'Table',
+    'column',
+    'relation'
+]
