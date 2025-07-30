aed95dae068cc69c1b06f20baf29602662043a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:34 +0000

    Add package lib-anim/1.0.0/lib_anim-1.0.0-py3-none-any.whl

---
+++ packages/lib-anim/lib_anim-1.0.0-py3-none-any.whl/anim/plane/__init__.py
@@ -0,0 +1,23 @@
+'''
+animate API
+'''
+
+from .canva import canva
+from .grid import grid
+
+# ─── Items
+
+from .items.group import group, composite
+from .items.rectangle import rectangle
+from .items.ellipse import ellipse
+from .items.circle import circle
+from .items.line import line
+from .items.path import path
+from .items.polygon import polygon
+from .items.image import image
+from .items.text import text
+
+# ─── Composites
+
+from .items.arrow import arrow
+from .items.colorbar import colorbar
